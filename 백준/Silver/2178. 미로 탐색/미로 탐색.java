776a13cc753d820650dda66b9e3c761c03af66db
최수연 <tndus5383@gmail.com>
2024-10-27 04:57:20 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 15592 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,60 +1,68 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int M;
-    static int[][] miro;
-    static int[] dx = {-1, 1, 0, 0}; // x 방향배열-상하
-    static int[] dy = {0, 0, -1, 1}; // y 방향배열-좌우
-    static int answer = 0;
-    static boolean[][] check;
+    static int N, M; // 세로,가로
+    static int[][] graph;
+    static boolean[][] visited;
+    // 동,서,남,북
+    static int[] dx = {0, 0, 1, -1}; // 수직 이동
+    static int[] dy = {1, -1, 0, 0}; // 수평 이동
 
-    static void bfs(int x, int y) {
-        Queue<int[]> Q = new LinkedList<>();
-        Q.add(new int[] {x, y});
-        check[x][y] = true;
-        while (!Q.isEmpty()) {
-            int[] now = Q.poll();
-            int nowx = now[0];
-            int nowy = now[1];
+    static class Node {
+        int x, y, distance;
 
-            for (int i = 0; i < 4; i++) {
-                int nextx = nowx + dx[i];
-                int nexty = nowy + dy[i];
-                if (nextx < 0 || nexty < 0 || nextx > N || nexty > M)
-                    continue;
-                if (check[nextx][nexty] || miro[nextx][nexty] == 0)
-                    continue;
-                
-                Q.add(new int[] {nextx, nexty});
-                check[nextx][nexty] = true;
-                miro[nextx][nexty] = miro[nowx][nowy]+1;
-            }
+        Node(int x, int y, int distance) {
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    static boolean canGo(int x, int y) {
+        return 0 <= x && x <= N - 1 && 0 <= y && y <= M - 1;
+    }
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        // StringBuffer sb = new StringBuffer();
+    static int BFS(int x, int y) {
+        ArrayDeque<Node> queue = new ArrayDeque<>();
+        visited[x][y] = true;
+        queue.offer(new Node(x, y, 1));
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        miro = new int[N + 1][M + 1];
-        check = new boolean[N + 1][M + 1];
-        for (int i = 1; i < N + 1; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < str.length(); j++) {
-                miro[i][j + 1] = Integer.parseInt(str.substring(j, j + 1));
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            if (current.x == N - 1 && current.y == M - 1) {
+                return current.distance;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int newX = current.x + dx[i];
+                int newY = current.y + dy[i];
+                if (canGo(newX, newY) && graph[newX][newY] == 1 && !visited[newX][newY]) {
+                    visited[newX][newY] = true;
+                    queue.offer(new Node(newX, newY, current.distance + 1));
+                }
             }
         }
-        bfs(1, 1);
-        System.out.println(miro[N][M]);
+        return -1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        graph = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String[] split = br.readLine().split("");
+            for (int j = 0; j < M; j++) {
+                graph[i][j] = Integer.parseInt(split[j]);
+            }
+        }
+
+        System.out.println(BFS(0, 0));
     }
 }
