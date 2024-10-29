2ed18deaa7ac7c9385d1b3b59d8cd8e26877c33b
최수연 <tndus5383@gmail.com>
2024-10-30 00:00:35 +0900

    [level 2] Title: 타겟 넘버, Time: 4.73 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,27 @@
+import java.util.*;
+
 class Solution {
+    static int n;
     static int answer;
+    static ArrayList<Integer> result = new ArrayList<>();
     
-    static void DFS(int depth, int sum, int[] numbers, int target) {
-        if (depth == numbers.length) {
-            if (sum == target) answer++;
+    static void DFS(int L, int sum, int[] numbers, int target) {
+        if (L == n) {
+            if (sum == target) {
+                answer++;
+            }
             return;
         }
-       
-        DFS(depth+1, sum+numbers[depth], numbers, target);
-        DFS(depth+1, sum-numbers[depth], numbers, target);
+        
+        DFS(L+1, sum+numbers[L], numbers, target);
+        DFS(L+1, sum-numbers[L], numbers, target);
     }
     
     public int solution(int[] numbers, int target) {
         answer = 0;
-        
+        n = numbers.length;
+
         DFS(0, 0, numbers, target);
-    
         return answer;
     }
 }
