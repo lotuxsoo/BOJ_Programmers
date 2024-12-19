8d60b89e3f050a76391c5292cb0c9063fb50d686
최수연 <tndus5383@gmail.com>
2024-12-19 14:56:45 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.06 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static int gcd(int a, int b) {
+         if (b > a) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        
+        while (b != 0) {
+            int mod = a % b;
+            a = b;
+            b = mod;
+        }
+        return a;
+    }
+    
+    public static int lcm(int a, int b) {
+        int gcd = gcd(a,b);
+        return Math.abs(a*b) / gcd;
+    }
+    
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        answer[0] = gcd(n,m);
+        answer[1] = lcm(n,m);
+        
+        return answer;
+    }
+}
