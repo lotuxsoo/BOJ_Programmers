c4e0411eef87c95dfee165507fc763ed90501140
최수연 <tndus5383@gmail.com>
2024-12-24 19:46:56 +0900

    [level 2] Title: 타겟 넘버, Time: 19.44 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,27 +1,39 @@
 import java.util.*;
 
 class Solution {
-    static int n;
-    static int answer;
-    static ArrayList<Integer> result = new ArrayList<>();
     
-    static void DFS(int L, int sum, int[] numbers, int target) {
-        if (L == n) {
-            if (sum == target) {
-                answer++;
-            }
+    static int n;
+    static ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+    static int ans = 0;
+    
+    static void DFS(int x, int sum, int target) {
+        if (x == n) {
+            if (sum == target) ans++;
             return;
         }
         
-        DFS(L+1, sum+numbers[L], numbers, target);
-        DFS(L+1, sum-numbers[L], numbers, target);
+        for (int next : adjList.get(x)) {
+            DFS(x+1, sum+next, target);
+        }
     }
     
     public int solution(int[] numbers, int target) {
-        answer = 0;
+        int answer = 0;
+        
+        for (int i=0; i<numbers.length; i++) {
+            adjList.add(new ArrayList<>());
+        }
+        for (int i=0; i<numbers.length; i++) {
+            adjList.get(i).add(numbers[i]);
+            adjList.get(i).add(numbers[i]*-1);
+        }
+        
         n = numbers.length;
-
-        DFS(0, 0, numbers, target);
+        
+        DFS(0, 0, target);
+        
+        answer = ans;
+        
         return answer;
     }
 }
