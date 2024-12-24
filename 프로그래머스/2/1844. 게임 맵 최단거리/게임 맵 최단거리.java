5b4c3f78e15a22090d8103364466300a68fffff3
최수연 <tndus5383@gmail.com>
2024-12-24 14:42:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.21 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,54 +1,68 @@
 import java.util.*;
 
 class Solution {
-    // 동,서,남,북
-    static int[] dx = {0,0,1,-1}; // 수직 이동
-    static int[] dy = {1,-1,0,0}; // 수평 이동
-    static int n,m; // 세로,가로
-    static boolean[][] visited;
-    
-    // 좌표와 거리를 저장하는 Node 클래스
     static class Node {
-        int x, y, distance;
-        Node(int x, int y, int distance) {
+        int x,y;
+        Node(int x, int y) {
             this.x = x;
             this.y = y;
-            this.distance = distance;
+        }
+    }
+    
+    static int n,m;
+    static Queue<Node> que = new ArrayDeque<>();
+    static int[][] dist; // 최단거리를 저장할 배열 생성
+    static int[][] grid; // 원본배열 copy
+    
+    static void BFS() {
+        que.offer(new Node(0,0));
+        dist[0][0] = 1;
+        
+        while (!que.isEmpty()) {
+            Node now = que.poll();
+            int x = now.x, y = now.y;
+            
+            int[] dx = new int[]{-1,1,0,0};
+            int[] dy = new int[]{0,0,-1,1};
+            for (int i=0; i<4; i++) {
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+                if (canGo(newX,newY)) {
+                    if (dist[newX][newY] == -1) {
+                        dist[newX][newY] = dist[x][y] + 1;
+                        que.offer(new Node(newX,newY));
+                    }
+                }
+            }
         }
     }
     
     static boolean canGo(int x, int y) {
-        return 0 <= x && x <= n-1 && 0 <= y && y <= m-1;
-    }
-    
-    static int BFS(int x, int y, int[][] maps) {
-        ArrayDeque<Node> queue = new ArrayDeque<>();
-        queue.offer(new Node(x,y,1));
-        visited[x][y] = true;
-
-        while (!queue.isEmpty()) {
-            Node current = queue.poll();
-            
-            if (current.x==n-1 && current.y==m-1) return current.distance;
-            
-            for (int i=0; i<4; i++) {
-                int newX = current.x + dx[i];
-                int newY = current.y + dy[i];
-                
-                if (canGo(newX, newY) && maps[newX][newY] == 1 && !visited[newX][newY]) {
-                    visited[newX][newY] = true;
-                    queue.offer(new Node(newX,newY, current.distance+1));
-                }
-            }
-        }
-        return -1;
+        if (!(0 <= x && x < n && 0 <= y && y < m)) return false;
+        if (grid[x][y] == 0) return false;
+        return true;
     }
     
     public int solution(int[][] maps) {
-        n = maps.length; // 세로 길이
-        m = maps[0].length; // 가로 길이
-        visited = new boolean[n][m];
+        int answer = 0;
         
-        return BFS(0,0,maps);
+        n = maps.length;
+        m = maps[0].length;
+        
+        grid = new int[n][m];
+        for (int i=0; i<n; i++) {
+            grid[i] = Arrays.copyOf(maps[i], maps[i].length);
+        }
+        
+        dist = new int[n][m];
+        for (int i=0; i<n; i++) {
+            Arrays.fill(dist[i], -1);
+        }
+
+        BFS();
+        
+        answer = dist[n-1][m-1];
+        
+        return answer;
     }
 }
