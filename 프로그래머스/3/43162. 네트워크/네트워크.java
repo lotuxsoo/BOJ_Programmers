a379e2b09ac2a8fac7efb8b49d923b1c8809dea4
최수연 <tndus5383@gmail.com>
2024-12-25 13:06:56 +0900

    [level 3] Title: 네트워크, Time: 0.34 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -5,24 +5,26 @@ class Solution {
     static boolean[] visited;
     
     static void DFS(int x, int[][] computers) {
+        
         for (int i=0; i<computers[x].length; i++) {
-            if (!visited[i] && computers[x][i]==1) {
+            if (i == x) continue;
+            if (!visited[i] && computers[x][i] == 1) {
                 visited[i] = true;
                 DFS(i, computers);
-            }    
-        }
+            }
+        }   
     }
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
-
+        
         visited = new boolean[n];
         
         for (int i=0; i<n; i++) {
             if (!visited[i]) {
-                visited[i] = true;
-                DFS(i, computers);
                 answer++;
+                visited[i] = true;
+                DFS(i, computers);  
             }
         }
         
