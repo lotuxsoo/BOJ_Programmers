08ed9f100426c4e795c24f3b0d34bf9157d2e3c8
최수연 <tndus5383@gmail.com>
2024-12-30 01:16:51 +0900

    [level 3] Title: 네트워크, Time: 0.68 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,32 +1,46 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
+    static int[] parent;
     
-    static boolean[] visited;
+    static int find(int x) {
+        if (x != parent[x]) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
     
-    static void DFS(int x, int[][] computers) {
-        
-        for (int i=0; i<computers[x].length; i++) {
-            if (i == x) continue;
-            if (!visited[i] && computers[x][i] == 1) {
-                visited[i] = true;
-                DFS(i, computers);
-            }
-        }   
+    static void union(int x, int y) {
+        int r1 = find(x);
+        int r2 = find(y);
+        if (r1 != r2) {
+            parent[r1] = r2;
+        }
     }
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
         
-        visited = new boolean[n];
-        
+        parent = new int[n];
         for (int i=0; i<n; i++) {
-            if (!visited[i]) {
-                answer++;
-                visited[i] = true;
-                DFS(i, computers);  
+            parent[i] = i;
+        }
+        
+        for (int i=0; i<computers.length; i++) {
+            for (int j=0; j<computers[i].length; j++) {
+                if (i!=j && computers[i][j] == 1) {
+                    union(i, j);
+                }
             }
         }
+        
+         // 네트워크 개수 계산
+        Set<Integer> networks = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            networks.add(find(i)); // 모든 노드의 루트를 찾아 Set에 저장
+        }
+        answer = networks.size();
         
         return answer;
     }
