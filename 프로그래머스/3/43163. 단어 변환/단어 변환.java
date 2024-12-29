78e351812043f2ba991191ca6057302a606e7d4b
최수연 <tndus5383@gmail.com>
2024-12-30 08:55:08 +0900

    [level 3] Title: 단어 변환, Time: 0.35 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,54 +1,58 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
-    public int solution(String begin, String target, String[] words) {
-        // target이 words에 없는 경우 바로 0 반환
-        if (!Arrays.asList(words).contains(target)) {
-            return 0;
+    
+    static boolean cango(String from, String to) {
+        int cnt = 0;
+        for (int i=0; i<from.length(); i++) {
+            if (from.charAt(i) != to.charAt(i)) cnt++;
         }
-
-        // BFS를 위한 큐와 방문 배열 초기화
-        Queue<String> queue = new LinkedList<>();
+        return cnt == 1;
+    }
+    
+    static int BFS(String begin, String target, String[] words) {
+        int ans = 0;
+        Queue<String> que = new ArrayDeque<>();
         boolean[] visited = new boolean[words.length];
-        queue.offer(begin);
-
-        int step = 0;
-
-        while (!queue.isEmpty()) {
-            int size = queue.size(); // 현재 레벨의 노드 개수
-            step++; // 새로운 레벨로 진입했으므로 단계 증가
-
-            // 현재 레벨의 모든 노드를 처리
-            for (int i = 0; i < size; i++) {
-                String current = queue.poll();
-
-                // 변환 가능한 단어를 찾아 큐에 추가
-                for (int j = 0; j < words.length; j++) {
-                    if (!visited[j] && canTransform(current, words[j])) {
-                        if (words[j].equals(target)) {
-                            return step; // target에 도달하면 변환 단계 반환
-                        }
-                        queue.offer(words[j]);
-                        visited[j] = true; // 방문 체크
+        que.add(begin);
+        
+        while (!que.isEmpty()) {
+            for (int i=0; i<que.size(); i++) { // 현재 들어있는 큐만큼만 반복
+                String now = que.poll();
+                
+                if (now.equals(target)) {
+                    return ans;
+                }
+                
+                for (int j=0; j<words.length; j++) {
+                    if (!visited[j] && cango(now, words[j])) {
+                        que.add(words[j]);
+                        visited[j] = true;
                     }
                 }
             }
+            ans++;
         }
-
-        return 0; // 변환 불가능한 경우
+            
+        return ans;
     }
-
-    // 두 단어가 정확히 한 글자만 다른지 확인하는 함수
-    private boolean canTransform(String word1, String word2) {
-        int diff = 0;
-        for (int i = 0; i < word1.length(); i++) {
-            if (word1.charAt(i) != word2.charAt(i)) {
-                diff++;
-                if (diff > 1) {
-                    return false; // 두 글자 이상 다르면 변환 불가
-                }
+    
+    
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        
+        boolean flag = false;
+        for (String word : words) {
+            if (word.equals(target)) {
+                flag = true;
+                break;
             }
         }
-        return diff == 1; // 정확히 한 글자만 다르면 true
+        if (!flag) return 0;
+        
+        answer = BFS(begin, target, words);
+        
+        return answer;
     }
 }
