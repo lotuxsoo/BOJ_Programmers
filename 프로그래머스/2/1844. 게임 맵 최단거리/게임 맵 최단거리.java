d5b44feb709127202efd482c3977b0c437f3a640
최수연 <tndus5383@gmail.com>
2024-12-30 09:23:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.51 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,62 +1,58 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
-    static class Point {
-        int x, y;
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    static int n,m;
-    static int[][] copys;
-    static int[][] distance;
-    static Queue<Point> que = new ArrayDeque<>();
+    static int n;
+    static int m;
     
-    static void BFS() {
-        distance[0][0] = 1;
-        que.add(new Point(0,0));
+    static boolean cango(int x, int y, int[][] maps) {
+        if (!(0 <= x && x < n && 0 <= y && y < m)) return false;
+        if (maps[x][y] == 0) return false;
+        return true;
+    }
+    
+    static int BFS(int[][] maps) {
+        Queue<int[]> que = new ArrayDeque<>();
+        n = maps.length;
+        m = maps[0].length;
+        boolean[][] visited = new boolean[n][m];
+        
+        que.add(new int[]{0,0});
+        visited[0][0] = true;
+        int ans = 1;
         
         while (!que.isEmpty()) {
-            Point p = que.poll();
-            int x = p.x, y = p.y;
             
-            for (int i=0; i<4; i++) {
+            int size = que.size();
+            for (int t=0; t<size; t++) {
+                int[] now = que.poll();
+                
+                if (now[0]==n-1 && now[1]==m-1) {
+                    return ans;
+                }
+                
                 int[] dx = {-1,1,0,0};
                 int[] dy = {0,0,-1,1};
-                int newX = x + dx[i];
-                int newY = y + dy[i];
-                if (canGo(newX, newY)) {
-                    distance[newX][newY] = distance[x][y] + 1;
-                    que.add(new Point(newX,newY));
+
+                for (int i=0; i<4; i++) {
+                    int nx = now[0] + dx[i];
+                    int ny = now[1] + dy[i];
+                    if (cango(nx,ny,maps) && !visited[nx][ny]) {
+                        que.add(new int[]{nx,ny});
+                        visited[nx][ny] = true;
+                    }
                 }
             }
+            ans++;
         }
-    }
-    
-    static boolean canGo(int x, int y) {
-        if (!(0 <= x && x < n && 0 <= y && y < m)) return false;
-        if (distance[x][y] != -1 || copys[x][y] == 0) return false;
-        return true;
+
+        return -1;
     }
     
     public int solution(int[][] maps) {
         int answer = 0;
-        n = maps.length; // 세로
-        m = maps[0].length; // 가로
-        copys = new int[n][m];
-        for (int i=0; i<n; i++) {
-            copys[i] = Arrays.copyOf(maps[i], maps[i].length);
-        }
-        distance = new int[n][m];
-        for (int i=0; i<n; i++) {
-            Arrays.fill(distance[i], -1);
-        }
         
-        BFS();
-        
-        if (distance[n-1][m-1] == -1) return -1;
-        else answer = distance[n-1][m-1];
+        answer = BFS(maps);
         
         return answer;
     }
