efbd0fd2f14fe6b54ac212f8221069322e09afa2
최수연 <tndus5383@gmail.com>
2024-12-30 10:08:50 +0900

    [level 3] Title: 네트워크, Time: 2.41 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -11,9 +11,9 @@ class Solution {
         return parent[x];
     }
     
-    static void union(int x, int y) {
-        int r1 = find(x);
-        int r2 = find(y);
+    static void union(int a, int b) {
+        int r1 = parent[a];
+        int r2 = parent[b];
         if (r1 != r2) {
             parent[r1] = r2;
         }
@@ -27,20 +27,22 @@ class Solution {
             parent[i] = i;
         }
         
-        for (int i=0; i<computers.length; i++) {
-            for (int j=0; j<computers[i].length; j++) {
-                if (i!=j && computers[i][j] == 1) {
-                    union(i, j);
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (computers[i][j] == 1) {
+                    if (find(i) != find(j)) {
+                        union(i, j);
+                    }
                 }
             }
         }
         
-         // 네트워크 개수 계산
-        Set<Integer> networks = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            networks.add(find(i)); // 모든 노드의 루트를 찾아 Set에 저장
+        Set<Integer> set = new HashSet<>();
+        for (int i=0; i<n; i++) {
+            set.add(find(i));
         }
-        answer = networks.size();
+        
+        answer = set.size();
         
         return answer;
     }
