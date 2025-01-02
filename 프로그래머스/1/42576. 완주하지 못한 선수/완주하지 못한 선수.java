83eda15c22c375db2007adc7a5a8ccbfc2fa8f3a
최수연 <tndus5383@gmail.com>
2025-01-02 20:12:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.76 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,22 +4,22 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        Map<String,Integer> parMap = new HashMap<>();
-        for (String s : participant) {
-            parMap.put(s, parMap.getOrDefault(s,0)+1);
-        }  
-        
-        for (String s : completion) {
-            parMap.put(s, parMap.get(s)-1);
+        Map<String,Integer> map = new HashMap<>();
+
+        for (int i=0; i<participant.length; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i],0)+1);
         }
-        
-        for (String key : parMap.keySet()) {
-            if (parMap.get(key) > 0) {
-                answer = key;
+        for (int i=0; i<completion.length; i++) {
+            map.put(completion[i], map.get(completion[i])-1);
+        }
+
+        for (String s : participant) {
+            if (map.get(s) > 0) {
+                answer = s;
                 break;
             }
         }
-
+        
         return answer;
     }
 }
