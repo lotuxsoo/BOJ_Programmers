0264e60931993bd6dcd73b17075286878f5bd0d1
최수연 <tndus5383@gmail.com>
2025-01-03 14:59:57 +0900

    [level 2] Title: 올바른 괄호, Time: 5.83 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -4,24 +4,20 @@ class Solution {
     boolean solution(String s) {
         boolean answer = true;
         
-        Stack<Character> stack = new Stack<>();
+        int openCount = 0;
         
-        char[] ch = s.toCharArray();
-        for(int i=0; i<ch.length; i++) {
-            if (ch[i] == '(') {
-                stack.push('(');
-            } else if (ch[i] == ')' && stack.size()>0) {
-                if (stack.peek() == '(') {
-                    stack.pop();
-                } else {
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                openCount++;
+            } else {
+                openCount--;
+                if (openCount < 0) {
                     return false;
                 }
-            } else {
-                return false;
-            }
+            }   
         }
         
-        if (stack.size() > 0) return false;
+        if (openCount > 0) return false;
 
         return answer;
     }
