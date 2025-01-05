638576ca26cc05508a324d9d8b3347251e8a6eb0
최수연 <tndus5383@gmail.com>
2025-01-05 14:48:37 +0900

    [level 2] Title: 피로도, Time: 4.97 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,17 +1,18 @@
 import java.util.*;
 
 class Solution {
-    
-    static int MAX_VAL = Integer.MIN_VALUE;
+    static int k;
+    static int[][] dungeons;
     static boolean[] visited;
+    static int MAX_VAL = Integer.MIN_VALUE;
     
-    static void permute(int cnt, int k, int[][] dungeons) {
+    static void backtrack(int cnt, int k) {
         MAX_VAL = Math.max(MAX_VAL, cnt);
-
+        
         for (int i=0; i<dungeons.length; i++) {
             if (!visited[i] && k >= dungeons[i][0]) {
                 visited[i] = true;
-                permute(cnt+1, k - dungeons[i][1], dungeons);
+                backtrack(cnt+1, k - dungeons[i][1]);
                 visited[i] = false;
             }
         }
@@ -19,11 +20,13 @@ class Solution {
     
     public int solution(int k, int[][] dungeons) {
         int answer = -1;
+        this.k = k;
+        this.dungeons = dungeons;
         
         visited = new boolean[dungeons.length];
         
-        permute(0, k, dungeons);
-        
+        backtrack(0, k);
+
         answer = MAX_VAL;
         
         return answer;
