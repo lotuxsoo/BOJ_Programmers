e997e4ccc79de2c1e6ac727daaad614146b38d32
최수연 <tndus5383@gmail.com>
2025-01-05 12:10:02 +0900

    [level 2] Title: 카펫, Time: 0.13 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,35 @@
+import java.util.*;
+
 class Solution {
+    
+    static int[] getPair(int total, int brown, int yellow) {
+        List<int[]> pairs = new ArrayList<>();    
+        
+        for (int i=1; i<=Math.sqrt(total); i++) {
+            if (total % i == 0) {
+                pairs.add(new int[]{i, total/i});
+            }
+        }
+        
+        for (int[] pair : pairs) {
+            int row = Math.min(pair[0], pair[1]); // 세로
+            int col = Math.max(pair[0], pair[1]); // 가로
+            
+            if ((col-2)*(row-2) == yellow && col*2+(row-2)*2 == brown) {
+                return new int[]{col, row};
+            }
+        }
+        
+        return new int[]{0,0};
+    }
+    
     public int[] solution(int brown, int yellow) {
         int[] answer = {};
         
         int total = brown + yellow;
+        int[] pair = getPair(total, brown, yellow);
         
-        // 조합 구하기 
-        for (int i = 1; i <= Math.sqrt(total); i++) {
-            if (total % i == 0) {
-                int height = i;
-                int width = total / i;
-                if ((height-2) * (width-2) == yellow) {
-                    answer = new int[]{width,height};
-                }
-            }
-        }
+        answer = pair;
         
         return answer;
     }
