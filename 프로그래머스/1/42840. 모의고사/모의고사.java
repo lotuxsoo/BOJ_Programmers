a886c764d1ee729264f4a405e68d7025b1fcf46a
최수연 <tndus5383@gmail.com>
2025-01-05 10:14:36 +0900

    [level 1] Title: 모의고사, Time: 1.04 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,33 +1,48 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] answers) {
         int[] answer = {};
         
-        int[] scores = new int[3]; // 점수 배열
+        int[][] students = {
+            {1,2,3,4,5},
+            {2,1,2,3,2,4,2,5},
+            {3,3,1,1,2,2,4,4,5,5}
+        };
         
-        int[] pat1 = {1,2,3,4,5};
-        int[] pat2 = {2,1,2,3,2,4,2,5};
-        int[] pat3 = {3,3,1,1,2,2,4,4,5,5};
+        int[] result = new int[3];
         
         for (int i=0; i<answers.length; i++) {
-            int ans = answers[i];
             
-            if (ans == pat1[i % pat1.length]) scores[0] += 1;
-            if (ans == pat2[i % pat2.length]) scores[1] += 1;
-            if (ans == pat3[i % pat3.length]) scores[2] += 1;
+            for (int j=0; j<students.length; j++) { // 3번
+                int[] student = students[j];
+            
+                if (student[i % student.length] == answers[i]) {
+                    result[j]++;
+                }   
+            }
         }
         
-        int maxScore = Math.max(scores[0], Math.max(scores[1],scores[2]));
-        List<Integer> result = new ArrayList<>();
-        for (int i=0; i<3; i++) {
-            if (scores[i] == maxScore) {
-                result.add(i+1);
+        int max = Integer.MIN_VALUE;
+        for (int x : result) {
+            if (x > max) {
+                max = x;
             }
-        }        
+        }
         
-        answer = result.stream().mapToInt(Integer::intValue).toArray();
-
+        int count = 0;
+        for (int x : result) {
+            if (x == max) {
+                count++;
+            }
+        }
+        
+        answer = new int[count];
+        int index = 0;
+        for (int i=0; i<result.length; i++) {
+            if (result[i] == max) {
+                answer[index++] = i+1;
+            }
+        }
+        
         return answer;
     }
 }
