d5a04734c40b58d8b97b50bbd8a093d0b5af57b1
최수연 <tndus5383@gmail.com>
2025-01-05 14:38:16 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 11.33 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,28 +1,31 @@
 import java.util.*;
 
 class Solution {
+    static int[][] wires;
     static int n;
     static int[][] adj;
     static boolean[] visited;
-    static int MIN_DIFF = Integer.MAX_VALUE;
     
-    static int DFS(int node) {
-        int count = 1; // 현재 노드 포함
-        visited[node] = true;
+    static int DFS(int x) { // DFS(1,1)
+        int cnt = 1;
+        visited[x] = true;
         
-        for (int i = 1; i <= n; i++) {
-            if (adj[node][i] == 1 && !visited[i]) {
-                count += DFS(i); // 연결된 노드의 개수 누적
+        for (int i=1; i<=n; i++) {
+            if (!visited[i] && adj[x][i] == 1) {
+                visited[i] = true;
+                cnt += DFS(i); // DFS(2), DFS(3), DFS(4)
             }
         }
-        return count;
+        
+        return cnt;
     }
     
-    static void buildAdjMatrix(int skipEdge, int[][] wires) {
-        adj = new int[n + 1][n + 1]; // 인접 행렬 초기화
-        for (int i = 0; i < wires.length; i++) {
-            if (i == skipEdge) continue; // 끊은 전선 제외
-            
+    static void backtrack(int x) {
+        // 인접리스트 초기화
+        adj = new int[n+1][n+1];
+        
+        for (int i=0; i<wires.length; i++) {
+            if (i == x) continue;
             int a = wires[i][0];
             int b = wires[i][1];
             adj[a][b] = 1;
@@ -31,23 +34,24 @@ class Solution {
     }
     
     public int solution(int n, int[][] wires) {
+        int answer = -1;
         this.n = n;
-
-        for (int i = 0; i < wires.length; i++) {
-            // 인접 행렬 생성
-            buildAdjMatrix(i, wires);
+        this.wires = wires;
+        int MIN_VAL = Integer.MAX_VALUE;
+        
+        // 전선들 중 하나를 고르는 백트래킹
+        for (int i=0; i<wires.length; i++) {
+            backtrack(i);
             
-            // 방문 상태 초기화
-            visited = new boolean[n + 1];
+            visited = new boolean[n+1];
+            int cnt = DFS(1); // 정점 1부터 탐색
             
-            // 한쪽 네트워크 크기 계산
-            int networkSize = DFS(1); // 항상 1번 노드에서 탐색 시작
-            int otherNetworkSize = n - networkSize; // 나머지 네트워크 크기 계산
-            
-            // 두 네트워크 크기 차이의 최솟값 갱신
-            MIN_DIFF = Math.min(MIN_DIFF, Math.abs(networkSize - otherNetworkSize));
+            int remain = n - cnt;
+            MIN_VAL = Math.min(MIN_VAL, Math.abs(cnt - remain));
         }
         
-        return MIN_DIFF;
+        answer = MIN_VAL;
+        
+        return answer;
     }
 }
