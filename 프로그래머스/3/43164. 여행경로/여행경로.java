747e3c4022c0971ce408946e72f508e44cda2be7
최수연 <tndus5383@gmail.com>
2025-01-07 13:13:21 +0900

    [level 3] Title: 여행경로, Time: 69.78 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,51 +1,33 @@
 import java.util.*;
 
 class Solution {
-    static class Ticket {
-        String start;
-        String end;
-        Ticket(String start, String end) {
-            this.start = start;
-            this.end = end;
-        }
-    }
     
+    static List<String> ansList = new ArrayList<>();
     static boolean[] visited;
-    static ArrayList<String> ansList = new ArrayList<>();
     
-    static void DFS(int depth, String start, StringBuilder sb, ArrayList<Ticket> ticketList) {
-        if (depth == ticketList.size()) {
+    static void DFS(int depth, String start, StringBuilder sb, String[][] tickets) {
+        if (depth == tickets.length) {
             ansList.add(sb.toString());
             return;
         }
         
-        for (int i=0; i<ticketList.size(); i++) {
-            if (!visited[i] && ticketList.get(i).start.equals(start)) {
+        for (int i=0; i<tickets.length; i++) {
+            if (!visited[i] && tickets[i][0].equals(start)) {
                 visited[i] = true;
-                StringBuilder nsb = new StringBuilder(sb);
-                nsb.append(ticketList.get(i).end).append(" ");
-                DFS(depth+1, ticketList.get(i).end, nsb, ticketList);
+                StringBuilder nsb = new StringBuilder(sb).append(tickets[i][1]).append(" ");
+                DFS(depth+1, tickets[i][1], nsb, tickets);
                 visited[i] = false;
             }
         }
     }
-
+    
     public String[] solution(String[][] tickets) {
         String[] answer = {};
         
-        ArrayList<Ticket> ticketList = new ArrayList<>();
-        for (String[] ticket : tickets) {
-            ticketList.add(new Ticket(ticket[0],ticket[1]));
-        }
+        visited = new boolean[tickets.length]; // 티겟 개수만큼
         
-        // 티켓 도착지 이름순 정렬 
-        Collections.sort(ticketList, (o1,o2) -> o1.end.compareTo(o2.end));
+        DFS(0, "ICN", new StringBuilder("ICN "), tickets);
         
-        visited = new boolean[ticketList.size()];
-        
-        DFS(0, "ICN", new StringBuilder("ICN").append(" "), ticketList);
-        
-        // 정답 문자열 배열 이름순 정렬
         Collections.sort(ansList);
         
         answer = ansList.get(0).split(" ");
