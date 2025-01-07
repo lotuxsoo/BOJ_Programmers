bfabbcd3cdbe77ada70f81b031ffb453c069e9aa
최수연 <tndus5383@gmail.com>
2025-01-07 12:59:07 +0900

    [level 3] Title: 단어 변환, Time: 0.42 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,46 +1,46 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
+    static String[] words;
+    static boolean[] visited;
     
-    static boolean cango(String from, String to) {
+    static boolean canAdd(String to, String from) {
+        if (from.length() != to.length()) return false;
+        
         int cnt = 0;
         for (int i=0; i<from.length(); i++) {
             if (from.charAt(i) != to.charAt(i)) cnt++;
         }
+
         return cnt == 1;
     }
     
-    static int BFS(String begin, String target, String[] words) {
-        int ans = 0;
-        Queue<String> que = new ArrayDeque<>();
-        boolean[] visited = new boolean[words.length];
-        que.add(begin);
+    static int BFS(String begin, String target) {
+        int cnt = 0;
+        Queue<String[]> queue = new LinkedList<>();
+        queue.add(new String[]{begin,"0"});
         
-        while (!que.isEmpty()) {
-            for (int i=0; i<que.size(); i++) { // 현재 들어있는 큐만큼만 반복
-                String now = que.poll();
-                
-                if (now.equals(target)) {
-                    return ans;
-                }
-                
-                for (int j=0; j<words.length; j++) {
-                    if (!visited[j] && cango(now, words[j])) {
-                        que.add(words[j]);
-                        visited[j] = true;
-                    }
+        while (!queue.isEmpty()) {
+            String[] current = queue.poll();
+            String now = current[0];
+            int level = Integer.parseInt(current[1]);
+
+            if (now.equals(target)) return level;
+            
+            for (int i=0; i<words.length; i++) {
+                if (!visited[i] && canAdd(words[i], now)) {
+                    visited[i] = true;
+                    queue.add(new String[]{words[i], String.valueOf(level + 1)});
                 }
             }
-            ans++;
         }
-            
-        return ans;
+        return 0;
     }
     
     
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
+        this.words = words;
         
         boolean flag = false;
         for (String word : words) {
@@ -51,7 +51,9 @@ class Solution {
         }
         if (!flag) return 0;
         
-        answer = BFS(begin, target, words);
+        visited = new boolean[words.length];
+            
+        answer = BFS(begin, target);
         
         return answer;
     }
