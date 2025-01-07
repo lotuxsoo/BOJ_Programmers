09850286b47b8915a75657566051caae3dede853
최수연 <tndus5383@gmail.com>
2025-01-07 18:49:24 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,72 +2,69 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class Main {
-    static int n;
-    static int[][] grid;
+    static int N;
+    static int[][] map;
     static boolean[][] visited;
-    static int num;
+    static ArrayList<Integer> answerList = new ArrayList<>();
 
-    static int DFS(int x, int y) {
-        int[] dx = new int[]{-1, 1, 0, 0};
-        int[] dy = new int[]{0, 0, -1, 1};
-
-        for (int i = 0; i < 4; i++) {
-            int newX = x + dx[i];
-            int newY = y + dy[i];
-            if (canGo(newX, newY, grid[x][y])) {
-                visited[newX][newY] = true;
-                num++;
-                DFS(newX, newY);
-            }
-        }
-
-        return num;
-    }
-
-    static boolean canGo(int x, int y, int value) {
-        if (!(0 <= x && x < n && 0 <= y && y < n)) {
+    static boolean cango(int x, int y) {
+        if (!(0 <= x && x < N && 0 <= y && y < N)) {
             return false;
         }
         if (visited[x][y]) {
             return false;
         }
-        if (grid[x][y] != value) {
+        if (map[x][y] == 0) {
             return false;
         }
         return true;
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        grid = new int[n][n];
-        visited = new boolean[n][n];
+    static int DFS(int x, int y) {
+        int cnt = 1;
+        visited[x][y] = true;
 
-        for (int i = 0; i < n; i++) {
-            String[] split = br.readLine().split("");
-            for (int j = 0; j < n; j++) {
-                grid[i][j] = Integer.parseInt(split[j]);
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (cango(nx, ny)) {
+                cnt += DFS(nx, ny);
             }
         }
+        return cnt;
+    }
 
-        int numOfDfs = 0;
-        ArrayList<Integer> list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            String[] splits = br.readLine().split("");
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(splits[j]);
+            }
+        }
+        visited = new boolean[N][N];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] != 0 && !visited[i][j]) {
-                    visited[i][j] = true;
-                    numOfDfs++;
-                    num = 1;
-                    list.add(DFS(i, j));
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    int cnt = DFS(i, j);
+                    answerList.add(cnt);
                 }
             }
         }
 
-        System.out.println(numOfDfs);
-
-        list.stream().sorted().forEach(System.out::println);
+        System.out.println(answerList.size());
+        Collections.sort(answerList);
+        for (int answer : answerList) {
+            System.out.println(answer);
+        }
     }
 }
