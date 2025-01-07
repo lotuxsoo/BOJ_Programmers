9c5806ff24cc43ffc0e10fab843a0f3f093188db
최수연 <tndus5383@gmail.com>
2025-01-07 19:47:39 +0900

    [level 3] Title: 여행경로, Time: 133.96 ms, Memory: 99.8 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,36 +1,53 @@
 import java.util.*;
 
 class Solution {
+    static class Ticket {
+        String start;
+        String end;
+        Ticket(String start, String end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
     
-    static List<String> ansList = new ArrayList<>();
+    static ArrayList<String> answerList = new ArrayList<>();
+    static ArrayList<Ticket> ticketList;
     static boolean[] visited;
     
-    static void DFS(int depth, String start, StringBuilder sb, String[][] tickets) {
-        if (depth == tickets.length) {
-            ansList.add(sb.toString());
+    static void DFS(int cnt, String start, StringBuilder sb) {
+        if (cnt == ticketList.size()) {
+            answerList.add(sb.toString());
             return;
         }
         
-        for (int i=0; i<tickets.length; i++) {
-            if (!visited[i] && tickets[i][0].equals(start)) {
+        for (int i=0; i<ticketList.size(); i++) {
+            if (!visited[i] && ticketList.get(i).start.equals(start)) {
                 visited[i] = true;
-                StringBuilder nsb = new StringBuilder(sb).append(tickets[i][1]).append(" ");
-                DFS(depth+1, tickets[i][1], nsb, tickets);
+                StringBuilder nsb = new StringBuilder(sb).append(ticketList.get(i).end).append(" ");
+                DFS(cnt+1, ticketList.get(i).end, nsb);
                 visited[i] = false;
             }
         }
-    }
+    } 
     
     public String[] solution(String[][] tickets) {
         String[] answer = {};
         
-        visited = new boolean[tickets.length]; // 티겟 개수만큼
+        visited = new boolean[tickets.length];
         
-        DFS(0, "ICN", new StringBuilder("ICN "), tickets);
+        ticketList = new ArrayList<>();
         
-        Collections.sort(ansList);
+        for (String[] ticket : tickets) {
+            ticketList.add(new Ticket(ticket[0], ticket[1]));
+        }
         
-        answer = ansList.get(0).split(" ");
+        Collections.sort(ticketList, (o1,o2) -> o1.end.compareTo(o2.end));
+        
+        DFS(0, "ICN", new StringBuilder("ICN "));
+        
+        Collections.sort(answerList);
+        
+        answer = answerList.get(0).split(" ");
         
         return answer;
     }
