22bedef98faa3e07c100cd6fbea4badf3985a9ba
최수연 <tndus5383@gmail.com>
2025-01-08 15:28:27 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1136 ms, Memory: 66120 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -10,23 +10,34 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[] sum = new int[N+1];
-        int[] arr = new int[N+1];
+        int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        for (int i = 1; i <= N; i++) {
-            sum[i] = sum[i-1] + arr[i];
-        }
-
+        int[][] arr2 = new int[M][2];
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            System.out.println(sum[y] - sum[x - 1]);
+            arr2[i][0] = Integer.parseInt(st.nextToken()) - 1;
+            arr2[i][1] = Integer.parseInt(st.nextToken()) - 1;
         }
 
+        // 부분합 구해놓기
+        int[] sum = new int[N];
+        sum[0] = arr[0];
+        for (int i = 1; i < N; i++) {
+            sum[i] = sum[i - 1] + arr[i];
+        }
+
+        // 구간합: S[j] - S[i-1]
+        for (int[] arr1 : arr2) {
+            int x;
+            if (arr1[0] == 0) {
+                x = sum[arr1[1]];
+            } else {
+                x = sum[arr1[1]] - sum[arr1[0] - 1];
+            }
+            System.out.println(x);
+        }
     }
 }
