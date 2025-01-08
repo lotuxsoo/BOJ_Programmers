96a6db1b68405802fcd78239aa4ffd9766b0c765
최수연 <tndus5383@gmail.com>
2025-01-08 20:47:38 +0900

    [Silver IV] Title: 주몽, Time: 188 ms, Memory: 17448 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,20 +10,29 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        int[] arr = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        Arrays.sort(arr);
         int answer = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (arr[i] + arr[j] == M) {
-                    answer++;
-                }
+        int left = 1;
+        int right = N;
+
+        while (left < right) { // 무조건 2개 선택
+            if (arr[left] + arr[right] == M) {
+                answer++;
+                left++;
+                right--;
+            } else if (arr[left] + arr[right] > M) {
+                right--;
+            } else if (arr[left] + arr[right] < M) {
+                left++;
             }
         }
+
         System.out.println(answer);
     }
 }
