58341ff334ad0118f726dd749ddf6f821a977120
최수연 <tndus5383@gmail.com>
2025-01-13 00:36:22 +0900

    [Gold IV] Title: 오큰수, Time: 828 ms, Memory: 152180 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.StringTokenizer;
 
@@ -10,17 +11,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N + 1];
+        int[] A = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
         }
 
         Deque<Integer> stack = new ArrayDeque<>();
-        int[] result = new int[N + 1];
+        int[] result = new int[N];
 
-        for (int i = 1; i <= N; i++) {
-            while (!stack.isEmpty() && A[stack.peek()] < A[i]) { // 오큰수 찾음
+        for (int i = 0; i < N; i++) {
+            // 스택안에 있는거는 아직 result가 확정되지 않은 숫자
+            while (!stack.isEmpty() && A[stack.peek()] < A[i]) {
                 int index = stack.pop();
                 result[index] = A[i];
             }
@@ -33,8 +35,8 @@ public class Main {
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            sb.append(result[i]).append(" ");
+        for (int i : result) {
+            sb.append(i).append(" ");
         }
         System.out.println(sb.toString());
     }
