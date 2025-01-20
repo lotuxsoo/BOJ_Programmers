8be6e5fc4797de5db31a592e2bb7201d0ab34f3b
최수연 <tndus5383@gmail.com>
2025-01-20 13:38:28 +0900

    [level 2] Title: 짝지어 제거하기, Time: 57.58 ms, Memory: 66.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = 0;
+        
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        if (stack.isEmpty()) return 1;
+        
+        return answer;
+    }
+}
