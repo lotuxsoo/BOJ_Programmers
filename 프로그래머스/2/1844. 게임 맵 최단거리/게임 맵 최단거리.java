4eef9aca47fc839aed6c096e86e2954559a52ffb
최수연 <tndus5383@gmail.com>
2025-01-23 01:49:01 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.60 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,54 +1,55 @@
 import java.util.*;
 
 class Solution {
-    static int[][] maps;
-    static int n,m;
-    static int[][] distance;
-    
-    static boolean cango(int x, int y) {
-        if (!(0 <= x && x < n && 0 <= y && y < m)) return false;
-        if (distance[x][y] != -1) return false;
-        if (maps[x][y] == 0) return false;
-        return true;
-    }
-    
-    static int BFS(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x,y});
-        distance[x][y] = 1;
+    static int n;
+    static int m;
+ 
+    static boolean BFS(int[][] maps, int[][] dist) {
+        boolean found = false;
         
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0,0});
+        dist[0][0] = 1;
+
         while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            if (now[0] == n-1 && now[1] == m-1) {
-                return distance[n-1][m-1];
+            int[] cur = queue.poll();
+            if (cur[0] == n-1 && cur[1] == m-1) {
+                found = true;
+                break;
             }
+            
             int[] dx = {-1,1,0,0};
             int[] dy = {0,0,-1,1};
             for (int i=0; i<4; i++) {
-                int nx = now[0] + dx[i];
-                int ny = now[1] + dy[i];
-                if (cango(nx, ny)) {
-                    queue.add(new int[]{nx, ny});
-                    distance[nx][ny] = distance[now[0]][now[1]] + 1;
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                
+                if ((0 <= nx && nx < n && 0 <= ny && ny < m) && maps[nx][ny] == 1) {
+                    if (dist[nx][ny] > dist[cur[0]][cur[1]] + 1) {
+                        dist[nx][ny] = dist[cur[0]][cur[1]] + 1;
+                        queue.add(new int[]{nx, ny});
+                    }
                 }
-            }
+            }  
         }
         
-        return -1;
+        return found;
     }
     
     public int solution(int[][] maps) {
         int answer = 0;
-        this.maps = maps;
+
         n = maps.length;
         m = maps[0].length;
         
-        distance = new int[n][m];
+        int[][] dist = new int[n][m];
         for (int i=0; i<n; i++) {
-            Arrays.fill(distance[i],-1);
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
         }
-            
-        answer = BFS(0,0);
+        
+        if (!BFS(maps, dist)) return -1;
+        
+        answer = dist[n-1][m-1];
         
         return answer;
     }
