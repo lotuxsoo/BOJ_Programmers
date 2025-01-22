a4a4aa9a967874a06942e9ce0fb6aa58b42199fb
최수연 <tndus5383@gmail.com>
2025-01-22 16:39:30 +0900

    [level 3] Title: 섬 연결하기, Time: 0.82 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,63 +1,47 @@
 import java.util.*;
 
 class Solution {
-    static class Node implements Comparable<Node> {
-        int x,y,cost;
-        Node(int x,int y,int cost) {
-            this.x = x;
-            this.y = y;
-            this.cost = cost;
-        }
-        @Override
-        public int compareTo(Node o) { // 거리 최소 순대로
-            return this.cost - o.cost;
-        }
-    }
+    static int[] parent;
     
     static int find(int x) {
-        if (x != parent[x]) {
-            parent[x] = find(parent[x]);
-        }
+        if (x == parent[x]) return x;
+        parent[x] = find(parent[x]);
         return parent[x];
     }
-    
+
     static void union(int x, int y) {
         int root1 = find(x);
         int root2 = find(y);
-        if (root1 != root2) {
-            parent[root2] = root1;
-        }
+        parent[root1] = root2;
     }
     
-    static int[] parent;
+    static boolean isFinish() {
+        int x = parent[0];
+        for (int i=1; i<parent.length; i++) {
+            if (x != parent[i]) return false;
+            x = parent[i];
+        }
+        return true;
+    }
     
     public int solution(int n, int[][] costs) {
         int answer = 0;
         
-        ArrayList<Node> nodes = new ArrayList<>();
-        for (int[] arr : costs) {
-            int a = arr[0];
-            int b = arr[1];
-            int cost = arr[2];
-            nodes.add(new Node(a,b,cost));
-        }
-        Collections.sort(nodes);
+        Arrays.sort(costs, (o1,o2) -> Integer.compare(o1[2],o2[2]));
         
-        // 노드 초기화
         parent = new int[n];
         for (int i=0; i<n; i++) {
             parent[i] = i;
         }
         
-        int cnt = 0;
-        
-        for (Node node : nodes) {
-            if (find(node.x) != find(node.y)) {
-                union(node.x, node.y);
-                answer += node.cost;
-                cnt++;
-                if (cnt == n-1) break;
+        for (int[] edge : costs) {
+            // 경로 연결
+            if (isFinish()) break;
+            if (find(edge[0]) != find(edge[1])) {
+                union(edge[0], edge[1]);
+                answer += edge[2];
             }
+
         }
         
         return answer;
