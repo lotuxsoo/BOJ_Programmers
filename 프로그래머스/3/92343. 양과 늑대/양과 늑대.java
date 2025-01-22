becc6017f6fc2bf23324f5b43f77037c6fb80992
최수연 <tndus5383@gmail.com>
2025-01-22 13:05:59 +0900

    [level 3] Title: 양과 늑대, Time: 55.44 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -11,61 +11,55 @@ class Solution {
         }
     }
     
-    static ArrayList<Integer>[] A;
+    static int N;
+    static ArrayList<Integer>[] graph;
     static int MAX_VAL = Integer.MIN_VALUE;
     
     static void BFS(int[] info) {
-    Queue<Status> queue = new LinkedList<>();
-    Set<String> visited = new HashSet<>();
-
-    // 초기 상태: 루트 노드(0)부터 탐색 시작
-    queue.add(new Status(1, 0, new HashSet<>(Set.of(0))));
-
-    while (!queue.isEmpty()) {
-        Status cur = queue.poll();
-        int zero = cur.zero;
-        int one = cur.one;
-        Set<Integer> nodes = cur.nodes;
-
-        // 상태 갱신: 현재 0의 최대값
-        MAX_VAL = Math.max(MAX_VAL, zero);
-
-        // 상태를 문자열로 변환하여 방문 여부 확인
-        String state = zero + "," + one + "," + nodes.toString();
-        if (visited.contains(state)) continue;
-        visited.add(state);
-
-        // 현재 노드 집합에서 자식 노드 탐색
-        for (int node : nodes) {
-            for (int child : A[node]) {
-                if (nodes.contains(child)) continue; // 이미 방문한 노드 제외
-
-                Set<Integer> newNodes = new HashSet<>(nodes);
-                newNodes.add(child); // 자식 노드 추가
-
-                if (info[child] == 1 && zero > one + 1) {
-                    // 늑대를 추가로 선택할 수 있는 경우
-                    queue.add(new Status(zero, one + 1, newNodes));
-                } else if (info[child] == 0) {
-                    // 양을 추가로 선택할 수 있는 경우
-                    queue.add(new Status(zero + 1, one, newNodes));
+        Queue<Status> queue = new LinkedList<>();
+        queue.add(new Status(1,0,new HashSet<>(Set.of(0))));
+        Set<String> visited = new HashSet<>();
+            
+        while (!queue.isEmpty()) {
+            Status cur = queue.poll();
+            int zero = cur.zero, one = cur.one;
+            MAX_VAL = Math.max(MAX_VAL, zero);
+            
+            Set<Integer> nodes = cur.nodes;
+            // String s = zero+" "+one+" "+nodes.toString();
+            // if (visited.contains(s)) continue;
+            // else visited.add(s);
+            
+            for (int node : nodes) {
+                for (int next : graph[node]) { // 다음 노드와 연결된 노드들
+                    if (nodes.contains(next)) continue;
+                    if ((info[next] == 1) && zero > one+1) {
+                        Set<Integer> newNodes = new HashSet<>(nodes);
+                        newNodes.add(next);
+                        queue.add(new Status(zero,one+1,newNodes));
+                    } else if (info[next] == 0) {
+                        Set<Integer> newNodes = new HashSet<>(nodes);
+                        newNodes.add(next);
+                        queue.add(new Status(zero+1,one,newNodes));
+                    }
                 }
             }
         }
     }
-}
     
     public int solution(int[] info, int[][] edges) {
         int answer = 0;
-        
-        A = new ArrayList[info.length];
-        for (int i=0; i<info.length; i++) {
-            A[i] = new ArrayList<>();
+        N = info.length; // 노드의 수
+        graph = new ArrayList[N]; // 에지 저장
+        for (int i=0; i<N; i++) {
+            graph[i] = new ArrayList<>();
         }
         
         for (int[] edge : edges) {
-            A[edge[0]].add(edge[1]);
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
         }
+        
         
         BFS(info);
         
