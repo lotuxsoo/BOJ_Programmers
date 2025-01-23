2164a679378a6f1db391c74ad9b39b87260ccc7b
최수연 <tndus5383@gmail.com>
2025-01-23 14:43:43 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.68 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,56 +1,47 @@
 import java.util.*;
 
 class Solution {
-    static int n;
-    static int m;
- 
-    static boolean BFS(int[][] maps, int[][] dist) {
-        boolean found = false;
-        
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0,0});
-        dist[0][0] = 1;
-
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            if (cur[0] == n-1 && cur[1] == m-1) {
-                found = true;
-                break;
-            }
-            
-            int[] dx = {-1,1,0,0};
-            int[] dy = {0,0,-1,1};
-            for (int i=0; i<4; i++) {
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
-                
-                if ((0 <= nx && nx < n && 0 <= ny && ny < m) && maps[nx][ny] == 1) {
-                    if (dist[nx][ny] > dist[cur[0]][cur[1]] + 1) {
-                        dist[nx][ny] = dist[cur[0]][cur[1]] + 1;
-                        queue.add(new int[]{nx, ny});
-                    }
-                }
-            }  
+    static class Node {
+        int x, y;
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
-        
-        return found;
     }
     
     public int solution(int[][] maps) {
         int answer = 0;
-
-        n = maps.length;
-        m = maps[0].length;
+        
+        int n = maps.length;
+        int m = maps[0].length;
         
         int[][] dist = new int[n][m];
-        for (int i=0; i<n; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        
+        ArrayDeque<Node> queue = new ArrayDeque<>();
+        queue.add(new Node(0,0));
+        dist[0][0] = 1;
+        
+        while (!queue.isEmpty()) {
+            Node now = queue.poll();
+            
+            int[] dx = {-1,1,0,0};
+            int[] dy = {0,0,-1,1};
+            for (int i=0; i<4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
+                
+                if (!(0 <= nx && nx < n && 0 <= ny && ny < m)) continue;
+                if (maps[nx][ny] == 0) continue;
+                
+                if (dist[nx][ny] == 0) {
+                    dist[nx][ny] = dist[now.x][now.y] + 1;
+                    queue.add(new Node(nx,ny));
+                }   
+            }
         }
         
-        if (!BFS(maps, dist)) return -1;
-        
-        answer = dist[n-1][m-1];
-        
+        answer = dist[n-1][m-1] == 0 ? -1 : dist[n-1][m-1];
+ 
         return answer;
     }
 }
