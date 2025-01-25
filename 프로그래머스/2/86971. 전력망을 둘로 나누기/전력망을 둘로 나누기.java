d23906731464509e16ff2e030fec6c6732669779
최수연 <tndus5383@gmail.com>
2025-01-26 00:07:30 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.15 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,14 +2,13 @@ import java.util.*;
 
 class Solution {
     static ArrayList<Integer>[] T;
-    static boolean[] visited;
     
-    static int DFS(int x, int cnt) {
+    static int DFS(int x, int cnt, boolean[] visited) {
         visited[x] = true;
         
         for (int next : T[x]) {
             if (!visited[next]) {
-                cnt = DFS(next, cnt+1);
+                cnt = DFS(next, cnt+1, visited);
             }
         }
         
@@ -20,25 +19,33 @@ class Solution {
         int answer = -1;
         int MIN_VAL = Integer.MAX_VALUE;
         
-        for (int k=0; k<wires.length; k++) {
-            T = new ArrayList[n+1];
-            for (int i=0; i<n+1; i++) {
-                T[i] = new ArrayList<>();
-            }
-            
-            visited = new boolean[n+1];
-            
-            for (int i=0; i<wires.length; i++) {
-                if (i == k) continue;
-                T[wires[i][0]].add(wires[i][1]);
-                T[wires[i][1]].add(wires[i][0]);
-            }
-            int subset = DFS(1, 1);
-            MIN_VAL = Math.min(MIN_VAL, Math.abs((n - subset) - subset));
+        // ArrayList 배열 초기화
+        T = new ArrayList[n+1];
+        for (int i=0; i<n+1; i++) {
+            T[i] = new ArrayList<>();
         }
-
+        
+        // 인접리스트 생성
+        for (int i=0; i<wires.length; i++) {
+            T[wires[i][0]].add(wires[i][1]);
+            T[wires[i][1]].add(wires[i][0]);
+        }
+        
+        // 간선 끊기
+        for (int i=0; i<wires.length; i++) {
+            T[wires[i][0]].remove(Integer.valueOf(wires[i][1]));
+            T[wires[i][1]].remove(Integer.valueOf(wires[i][0]));
+            
+            boolean[] visited = new boolean[n+1];
+            int subset = DFS(1, 1, visited);
+            MIN_VAL = Math.min(MIN_VAL, Math.abs((n - subset) - subset));
+            
+            T[wires[i][0]].add(wires[i][1]);
+            T[wires[i][1]].add(wires[i][0]);
+        }
+        
         answer = MIN_VAL;
-       
+        
         return answer;
     }
 }
