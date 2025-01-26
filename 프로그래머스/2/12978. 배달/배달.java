f14954733a58b88aeb242df8ff3ca7b33c0396d7
최수연 <tndus5383@gmail.com>
2025-01-27 02:06:37 +0900

    [level 2] Title: 배달, Time: 5.61 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -9,44 +9,43 @@ class Solution {
         }
     }
     
-    static int INF = Integer.MAX_VALUE;
-    
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-        
+
         ArrayList<Node>[] A = new ArrayList[N+1]; // 1~N 사용
         for (int i=0; i<N+1; i++) {
             A[i] = new ArrayList<>();
         }
         
+        // 인접리스트 양방향
         for (int i=0; i<road.length; i++) {
             int s = road[i][0];
             int e = road[i][1];
             int v = road[i][2];
-            A[s].add(new Node(e,v));
-            A[e].add(new Node(s,v));
+            A[s].add(new Node(e, v));
+            A[e].add(new Node(s, v));
         }
         
+        // 거리 배열 초기화
         int[] dist = new int[N+1];
-        Arrays.fill(dist, INF);
+        Arrays.fill(dist, Integer.MAX_VALUE);
         dist[1] = 0;
         
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1,o2) -> Integer.compare(o1.v, o2.v));
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.v, b.v));
         pq.add(new Node(1, 0));
         
+        // 다익스트라 탐색
         while (!pq.isEmpty()) {
             Node cur = pq.poll();
             
             for (Node next : A[cur.e]) {
-                if (next.v > K) continue;
-                
                 if (dist[next.e] > cur.v + next.v) {
                     dist[next.e] = cur.v + next.v;
                     pq.add(new Node(next.e, dist[next.e]));
                 }
             }
         }
-    
+        
         for (int i=1; i<N+1; i++) {
             if (dist[i] <= K) answer++;
         }
