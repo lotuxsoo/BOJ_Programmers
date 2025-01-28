b747178ccec26f5a2654e76deed473ea6a416203
최수연 <tndus5383@gmail.com>
2025-01-28 20:10:42 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 11.01 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -11,45 +11,47 @@ class Solution {
         }
     }
     
-    static int BFS(int[][] board) {
-        int N = board.length;
-        int M = board[0].length;
-        int[][][] visited = new int[N][M][4];
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<M; j++) {
-                Arrays.fill(visited[i][j], Integer.MAX_VALUE);
-            }
-        }
-        
-        // 상(0), 하(1), 좌(2), 우(3)
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        
+    static int N;
+    static int[][][] dist;
+    
+    static int BFS(int[][] board) { 
         Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(0, 0, -1, 0)); // 초기: 방향없음(-1)
-
+        queue.add(new Node(0, 0, -1, 0)); // 초기: -1(방향없음)
+        
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
+        
         int minCost = Integer.MAX_VALUE;
-
+        
         while (!queue.isEmpty()) {
             Node cur = queue.poll();
             int x = cur.x, y = cur.y, dir = cur.dir, cost = cur.cost;
             
             if (x == N-1 && y == N-1) {
+                // 이 좌표에 도달한 모든 방향의 비용 중 최솟값을 선택
                 minCost = Math.min(minCost, cost);
                 continue;
             }
             
+            // 0:상 1:하 2:좌 3:우
             for (int i=0; i<4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if (!(0 <= nx && nx < N && 0 <= ny && ny < M)) continue;
+                if (!(0 <= nx && nx < N && 0 <= ny && ny < N)) continue;
                 if (board[nx][ny] == 1) continue;
                 
-                int newCost = (dir == i || dir == -1) ? cost + 100 : cost + 600;
-                
-                if (newCost < visited[nx][ny][i]) {
-                    visited[nx][ny][i] = newCost;
-                    queue.add(new Node(nx, ny, i, newCost));
+                if (dir == i || dir == -1) {
+                    int newCost = cost + 100;
+                    if (dist[nx][ny][i] > newCost) {
+                        dist[nx][ny][i] = newCost;
+                        queue.add(new Node(nx, ny, i, dist[nx][ny][i]));
+                    }
+                } else {
+                    int newCost = cost + 600;
+                    if (dist[nx][ny][i] > newCost) {
+                        dist[nx][ny][i] = newCost;
+                        queue.add(new Node(nx, ny, i, dist[nx][ny][i]));
+                    }
                 }
             }
         }
@@ -60,8 +62,16 @@ class Solution {
     public int solution(int[][] board) {
         int answer = 0;
         
-        answer = BFS(board);
+        N = board.length;
+        dist = new int[N][N][4];
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                Arrays.fill(dist[i][j], Integer.MAX_VALUE);   
+            }
+        }
         
+        answer = BFS(board);
+
         return answer;
     }
 }
