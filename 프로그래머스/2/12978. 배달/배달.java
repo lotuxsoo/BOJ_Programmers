7a3ca462465447cc861100c27505de2d90ab7183
최수연 <tndus5383@gmail.com>
2025-01-28 21:29:47 +0900

    [level 2] Title: 배달, Time: 10.74 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,54 +2,59 @@ import java.util.*;
 
 class Solution {
     static class Node {
-        int e, v;
-        Node(int e, int v) {
-            this.e = e;
-            this.v = v;
+        int end, cost;
+        Node(int end, int cost) {
+            this.end = end;
+            this.cost = cost;
         }
     }
     
-    public int solution(int N, int[][] road, int K) {
-        int answer = 0;
-
-        ArrayList<Node>[] A = new ArrayList[N+1]; // 1~N 사용
-        for (int i=0; i<N+1; i++) {
-            A[i] = new ArrayList<>();
-        }
-        
-        // 인접리스트 양방향
-        for (int i=0; i<road.length; i++) {
-            int s = road[i][0];
-            int e = road[i][1];
-            int v = road[i][2];
-            A[s].add(new Node(e, v));
-            A[e].add(new Node(s, v));
-        }
-        
-        // 거리 배열 초기화
+    static ArrayList<Node>[] A;
+    
+    static int BFS(int N, int K) {
         int[] dist = new int[N+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[1] = 0;
         
-        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.v, b.v));
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.cost,b.cost));
         pq.add(new Node(1, 0));
         
-        // 다익스트라 탐색
         while (!pq.isEmpty()) {
             Node cur = pq.poll();
             
-            for (Node next : A[cur.e]) {
-                if (dist[next.e] > cur.v + next.v) {
-                    dist[next.e] = cur.v + next.v;
-                    pq.add(new Node(next.e, dist[next.e]));
+            for (Node next : A[cur.end]) {
+                if (dist[next.end] > cur.cost + next.cost) {
+                    dist[next.end] = cur.cost + next.cost;
+                    pq.add(new Node(next.end, dist[next.end]));
                 }
+            } 
+        }
+        
+        int cnt = 0;
+        for (int i=1; i<N+1; i++) {
+            if (dist[i] <= K) {
+                cnt++;
             }
         }
         
-        for (int i=1; i<N+1; i++) {
-            if (dist[i] <= K) answer++;
+        return cnt;
+    }
+    
+    public int solution(int N, int[][] road, int K) {
+        int answer = 0;
+        
+        A = new ArrayList[N+1]; // 마을 번호: 1~N
+        for (int i=0; i<N+1; i++) {
+            A[i] = new ArrayList<>();
         }
-
+        
+        for (int[] row : road) {
+            A[row[0]].add(new Node(row[1], row[2]));
+            A[row[1]].add(new Node(row[0], row[2]));
+        }
+        
+        answer = BFS(N, K);
+        
         return answer;
     }
 }
