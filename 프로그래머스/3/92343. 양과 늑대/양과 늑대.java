69ab38c071322ba5f9403526ce3d3f36522cd752
최수연 <tndus5383@gmail.com>
2025-01-28 16:21:23 +0900

    [level 3] Title: 양과 늑대, Time: 36.41 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -2,66 +2,50 @@ import java.util.*;
 
 class Solution {
     static class Node {
-        int sheep, wolf;
+        int idx, sheep, wolf;
         Set<Integer> nodes;
-        Node(int sheep, int wolf, Set<Integer> nodes) {
+        Node(int idx, int sheep, int wolf, Set<Integer> nodes) {
+            this.idx = idx;
             this.sheep = sheep;
             this.wolf = wolf;
             this.nodes = nodes;
-        }
-    }
-    static int n;
-    static ArrayList<Integer>[] graph;
-    static int MAX_VAL = Integer.MIN_VALUE;
-    
-    static void BFS(int[] info) {
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(1, 0, new HashSet<>(Set.of(0))));
-        
-        while (!queue.isEmpty()) {
-            Node cur = queue.poll();
-            int sheep = cur.sheep, wolf = cur.wolf;
-            Set<Integer> nodes = cur.nodes;
-            
-            MAX_VAL = Math.max(MAX_VAL, sheep);
-            
-            for (int node : nodes) {
-                for (int next : graph[node]) {
-                    if (nodes.contains(next)) continue;
-                    
-                    if (info[next] == 0) {
-                        Set<Integer> newSet = new HashSet<>(nodes);
-                        newSet.add(next);
-                        queue.add(new Node(sheep+1, wolf, newSet));
-                   } else if ((info[next] == 1) && (wolf+1 < sheep)){
-                        Set<Integer> newSet = new HashSet<>(nodes);
-                        newSet.add(next);
-                        queue.add(new Node(sheep, wolf+1, newSet));
-                   }
-                }
-            }
         }
     }
     
     public int solution(int[] info, int[][] edges) {
         int answer = 0;
         
-        n = info.length; // 노드 개수
-        graph = new ArrayList[n]; // 0:루트(양)
+        int n = info.length;
+        ArrayList<Integer>[] A = new ArrayList[n];
         for (int i=0; i<n; i++) {
-            graph[i] = new ArrayList<>();
+            A[i] = new ArrayList<>();
+        }
+        for (int[] edge : edges) {
+            A[edge[0]].add(edge[1]);
         }
         
-        for (int i=0; i<n-1; i++) {
-            int a = edges[i][0];
-            int b = edges[i][1];
-            graph[a].add(b);
-            graph[b].add(a);
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(0, 1, 0, new HashSet<>(Set.of(0))));
+        
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            answer = Math.max(answer, cur.sheep);
+            
+            for (int node : cur.nodes) {
+                for (int next : A[node]) {
+                    if (cur.nodes.contains(next)) continue;
+                    
+                    int nextSheep = (info[next] == 0) ? cur.sheep + 1 : cur.sheep;
+                    int nextWolf = (info[next] == 1) ? cur.wolf + 1 : cur.wolf;
+
+                    if (nextSheep > nextWolf) {
+                        Set<Integer> newSet = new HashSet<>(cur.nodes);
+                        newSet.add(next);
+                        queue.add(new Node(node, nextSheep, nextWolf, newSet));
+                    }
+                }
+            } 
         }
-        
-        BFS(info);
-        
-        answer = MAX_VAL;
         
         return answer;
     }
