180a9c940462414380d4a0d87700006e7c9663e4
최수연 <tndus5383@gmail.com>
2025-01-30 20:24:31 +0900

    [Silver III] Title: N과 M (7), Time: 644 ms, Memory: 142888 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb;
+
+    static void backtrack(int cnt, ArrayList<Integer> list) {
+        if (cnt == M) {
+            for (int i : list) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            list.add(arr[i]);
+            backtrack(cnt + 1, list);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        sb = new StringBuilder();
+
+        backtrack(0, new ArrayList<>());
+
+        System.out.println(sb.toString());
+    }
+}
