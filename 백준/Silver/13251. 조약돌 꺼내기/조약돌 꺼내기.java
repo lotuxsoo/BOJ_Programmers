a3afa6bbc3d2a3c8c75d6e12048b85a3daaa0e66
최수연 <tndus5383@gmail.com>
2025-01-30 22:20:59 +0900

    [Silver III] Title: 조약돌 꺼내기, Time: 104 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.java
+++ 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,24 +9,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int M = Integer.parseInt(br.readLine());
-        int[] A = new int[M + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int total = 0;
-        for (int i = 1; i <= M; i++) {
+        int[] A = new int[M];
+        for (int i = 0; i < M; i++) {
             A[i] = Integer.parseInt(st.nextToken());
-            total += A[i];
         }
+        int sum = Arrays.stream(A).sum();
         int K = Integer.parseInt(br.readLine());
 
         double answer = 0.0;
-        for (int i = 1; i <= M; i++) {
+        for (int j = 0; j < M; j++) {
             double prob = 1.0;
-            for (int k = 0; k < K; k++) {
-                prob *= (double) (A[i] - k) / (total - k);
+            for (int i = 0; i < K; i++) {
+                prob *= (double) (A[j] - i) / (sum - i);
             }
             answer += prob;
         }
-
         System.out.println(answer);
     }
 }
