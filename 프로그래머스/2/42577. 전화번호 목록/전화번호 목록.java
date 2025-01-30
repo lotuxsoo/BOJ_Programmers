8ca0a7a2658975e4d733d0250882fcefbe6f0485
최수연 <tndus5383@gmail.com>
2025-01-30 15:58:11 +0900

    [level 2] Title: 전화번호 목록, Time: 282.36 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,15 +3,14 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        
+    
+        // 사전순 정렬
         Arrays.sort(phone_book);
-        
+
         for (int i=0; i<phone_book.length-1; i++) {
-            if (phone_book[i+1].startsWith(phone_book[i])) {
-                return false;
-            }
+            if (phone_book[i+1].startsWith(phone_book[i])) return false;
         }
-        
+    
         return answer;
     }
 }
