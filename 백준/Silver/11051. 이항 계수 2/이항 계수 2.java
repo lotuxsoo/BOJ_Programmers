67d4d616d1b870563f35424c8812bd46150f4856
최수연 <tndus5383@gmail.com>
2025-01-30 21:35:46 +0900

    [Silver II] Title: 이항 계수 2, Time: 120 ms, Memory: 18216 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -4,30 +4,28 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
+    static int MOD = 10007;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        //DP 배열 초기화
-        int[][] D = new int[N + 1][N + 1];
+        int[][] dp = new int[N + 1][N + 1];
 
-        for (int i = 0; i <= N; i++) {
-            D[i][0] = 1;
-            D[i][1] = i;
-            D[i][i] = 1;
+        // nC0 = 1, nC1 = n, nCn = 1
+        for (int i = 1; i < N + 1; i++) {
+            dp[i][0] = 1;
+            dp[i][1] = i;
+            dp[i][i] = 1;
         }
 
-        for (int i = 2; i <= N; i++) {
+        for (int i = 2; i < N + 1; i++) {
             for (int j = 1; j < i; j++) {
-                D[i][j] = D[i - 1][j - 1] + D[i - 1][j];
-                D[i][j] = D[i][j] % 10007;
+                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]) % MOD;
             }
         }
 
-        System.out.println(D[N][K]);
+        System.out.println(dp[N][K] % MOD);
     }
-
 }
