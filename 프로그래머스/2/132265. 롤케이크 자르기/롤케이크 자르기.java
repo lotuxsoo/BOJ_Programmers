19f05906e208d917803e5dd45af3f993f8693652
최수연 <tndus5383@gmail.com>
2025-02-01 01:19:07 +0900

    [level 2] Title: 롤케이크 자르기, Time: 208.69 ms, Memory: 189 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -4,21 +4,23 @@ class Solution {
     public int solution(int[] topping) {
         int answer = 0;
         
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i=0; i<topping.length; i++) {
-            map.put(topping[i], map.getOrDefault(topping[i],0)+1);
+        // topping 원소,개수
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int t : topping) {
+            map.put(t, map.getOrDefault(t,0)+1);
         }
         
         Set<Integer> set = new HashSet<>();
         for (int i=0; i<topping.length; i++) {
-            set.add(topping[i]);
             if (map.containsKey(topping[i])) {
-                map.put(topping[i], map.get(topping[i])-1);
-                if (map.get(topping[i]) < 1) {
+                if (map.getOrDefault(topping[i],0)-1 <= 0) {
                     map.remove(topping[i]);
+                } else {
+                    map.put(topping[i], map.get(topping[i])-1);
                 }
+                set.add(topping[i]);
+                if (map.size() == set.size()) answer++;
             }
-            if (set.size() == map.size()) answer++;
         }
         
         return answer;
