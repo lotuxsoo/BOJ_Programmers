8a10e37a9dab9ee60658efcb4d1f659e26ba21ec
최수연 <tndus5383@gmail.com>
2025-02-01 02:08:24 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.05 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,30 +1,42 @@
-import java.util.*;
-
 class Solution {
+    
+    static boolean isValid(int x, int y, int n, int m) {
+        return -n <= x && x <= n && -m <= y && y <= m;
+    }
+    
     public int[] solution(String[] keyinput, int[] board) {
         int[] answer = {};
         
-        HashMap<String, int[]> map = new HashMap<>();
-        map.put("up", new int[]{0,1});
-        map.put("down", new int[]{0,-1});
-        map.put("left", new int[]{-1,0});
-        map.put("right", new int[]{1,0});
+        int n = board[0] / 2;
+        int m = board[1] / 2;
         
-        int[] now = {0,0}; // 가로,세로
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,1,-1};
         
-        for (String key : keyinput) {
-            if (map.containsKey(key)) {
-                int[] dx = map.get(key);
-                int nx = now[0] + dx[0];
-                int ny = now[1] + dx[1];
-                if (Math.abs(nx) <= board[0]/2 && Math.abs(ny) <= board[1]/2) {
-                    now[0] = nx;
-                    now[1] = ny;
-                }
+        int cx = 0, cy = 0;
+        
+        for (String s : keyinput) {
+            int nx = cx, ny = cy;
+            if (s.equals("left")) {
+                nx = cx + dx[0];
+                ny = cy + dy[0];
+            } else if (s.equals("right")) {
+                nx = cx + dx[1];
+                ny = cy + dy[1];
+            } else if (s.equals("up")) {
+                nx = cx + dx[2];
+                ny = cy + dy[2];
+            } else if (s.equals("down")) {
+                nx = cx + dx[3];
+                ny = cy + dy[3];
+            }
+            if (isValid(nx, ny, n, m)) {
+                cx = nx;
+                cy = ny;
             }
         }
         
-        answer = now;
+        answer = new int[]{cx,cy};
         
         return answer;
     }
