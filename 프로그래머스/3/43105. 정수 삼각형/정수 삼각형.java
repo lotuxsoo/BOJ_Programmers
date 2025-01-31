babe12b3285d3bd8931f0edbb6acb4987f437673
최수연 <tndus5383@gmail.com>
2025-01-31 21:08:08 +0900

    [level 3] Title: 정수 삼각형, Time: 9.53 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,39 +1,29 @@
-import java.util.*;
-
 class Solution {
-    
-    static int[][] dp;
-    
     public int solution(int[][] triangle) {
         int answer = 0;
-        int n = triangle.length;
-        dp = new int[n][n];
-    
-        for (int i=0; i<n; i++) {
-            Arrays.fill(dp[i], -1);
+        
+        int n = triangle.length; // 세로 길이
+        int[][] dp = new int[n][n];
+        
+        // 초기화
+        dp[0][0] = triangle[0][0];
+        for (int i=1; i<n; i++) {
+            dp[i][0] = dp[i-1][0] + triangle[i][0];
         }
         
-        // 초깃값 설정
-        dp[0][0] = triangle[0][0];
+        for (int i=1; i<n; i++) {
+            dp[i][i] = dp[i-1][i-1] + triangle[i][i];
+        }
         
-        
-        for (int i=0; i<n; i++) {
-            // 모두 왼쪽에서만 옴
-            if (i > 0) dp[i][0] = Math.max(dp[i-1][0] + triangle[i][0], dp[i][0]);
-            // 모두 오른쪽에서만 옴
-            if (i > 0) dp[i][i] = Math.max(dp[i-1][i-1] + triangle[i][i], dp[i][i]);
-            
-            for (int j=1; j<i; j++) {
+        for (int i=2; i<n; i++) {
+            for (int j=1; j<=i; j++) {
                 dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j];
             }
         }
         
-        int max = Integer.MIN_VALUE;
         for (int i=0; i<n; i++) {
-            if (max < dp[n-1][i]) max = dp[n-1][i];
+            answer = Math.max(answer, dp[n-1][i]);
         }
-        
-        answer = max;
         
         return answer;
     }
