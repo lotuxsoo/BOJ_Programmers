b1d7790d9cd780195ea6015155719f4bd9a19273
최수연 <tndus5383@gmail.com>
2025-02-01 22:22:29 +0900

    [Silver I] Title: 포도주 시식, Time: 124 ms, Memory: 15028 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -4,26 +4,31 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    static int n;
+    static int[] drinks;
+    static int[] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] drinks = new int[N + 1];
-        for (int i = 1; i < N + 1; i++) {
+        n = Integer.parseInt(br.readLine());
+        drinks = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
             drinks[i] = Integer.parseInt(br.readLine());
         }
 
-        int[] dp = new int[N + 1];
+        // dp[i]: i번째 잔을 고려할때 마실수있는 최대양
+        dp = new int[n + 1];
         dp[1] = drinks[1];
-        if (N == 1) {
+        if (n == 1) {
             System.out.println(dp[1]);
             return;
         }
-        dp[2] = drinks[1] + drinks[2];
+        dp[2] = Math.max(dp[1], drinks[1] + drinks[2]);
 
-        for (int i = 3; i < N + 1; i++) {
-            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + +drinks[i], dp[i - 3] + drinks[i - 1] + +drinks[i]));
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 3] + drinks[i - 1] + drinks[i], dp[i - 2] + drinks[i]));
         }
 
-        System.out.println(dp[N]);
+        System.out.println(dp[n]);
     }
 }
