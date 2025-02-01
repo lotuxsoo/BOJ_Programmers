d695e594717f765228c481d04ed4ad32c2be1ce4
최수연 <tndus5383@gmail.com>
2025-02-01 15:23:52 +0900

    [Gold IV] Title: 네트워크 연결, Time: 368 ms, Memory: 48004 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,24 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
     static class Edge {
-        int a, b, c;
+        int a, b, cost;
 
-        Edge(int a, int b, int c) {
+        Edge(int a, int b, int cost) {
             this.a = a;
             this.b = b;
-            this.c = c;
+            this.cost = cost;
         }
     }
-
-    static PriorityQueue<Edge> pq;
-    static int N, M;
-    static int[] parent;
 
     static int find(int x) {
         if (x != parent[x]) {
@@ -27,20 +22,30 @@ public class Main {
         return parent[x];
     }
 
-    static void union(int a, int b) {
-        int r1 = find(a);
-        int r2 = find(b);
-        if (r1 != r2) {
+    static void union(int x, int y) {
+        int r1 = find(x);
+        int r2 = find(y);
+
+        if (rank[r1] < rank[r2]) {
             parent[r1] = r2;
+        } else if (rank[r1] > rank[r2]) {
+            parent[r2] = r1;
+        } else {
+            parent[r2] = r1;
+            rank[r1]++;
         }
     }
 
+    static int N, M;
+    static int[] parent;
+    static int[] rank;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine()); // 노드 개수
-        M = Integer.parseInt(br.readLine()); // 간선 개수
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
 
-        pq = new PriorityQueue<>(Comparator.comparingInt(e -> e.c));
+        PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
         for (int i = 0; i < M; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -49,27 +54,27 @@ public class Main {
             pq.add(new Edge(a, b, c));
         }
 
+        // 집합 초기화
         parent = new int[N + 1];
+        rank = new int[N + 1];
         for (int i = 1; i < N + 1; i++) {
             parent[i] = i;
+            rank[i] = 0;
         }
 
-        int cnt = 0;
-        int cost = 0;
+        int nodeCount = 0;
+        int totalCost = 0;
 
-        while (cnt < N - 1) {
-            Edge cur = pq.poll();
-            if (find(cur.a) != find(cur.b)) {
-                union(cur.a, cur.b);
-                cost += cur.c;
-                cnt++;
-            }
+        while (nodeCount < N - 1) {
+            Edge edge = pq.poll();
 
-            if (cnt == N - 1) {
-                break;
+            if (find(edge.a) != find(edge.b)) {
+                union(edge.a, edge.b);
+                totalCost += edge.cost;
+                nodeCount++;
             }
         }
 
-        System.out.println(cost);
+        System.out.println(totalCost);
     }
 }
