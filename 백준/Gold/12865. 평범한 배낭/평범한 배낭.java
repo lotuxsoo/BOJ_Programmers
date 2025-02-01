e4367760eadbd588122a53b52baa8113356d00d6
최수연 <tndus5383@gmail.com>
2025-02-01 09:55:07 +0900

    [Gold V] Title: 평범한 배낭, Time: 176 ms, Memory: 53712 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -8,27 +8,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // 물품의 수
-        int K = Integer.parseInt(st.nextToken()); // 버틸수있는 무게
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
         int[] weights = new int[N + 1];
         int[] values = new int[N + 1];
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i < N + 1; i++) {
             st = new StringTokenizer(br.readLine());
-            weights[i] = Integer.parseInt(st.nextToken()); // 무게
-            values[i] = Integer.parseInt(st.nextToken()); // 가치
+            weights[i] = Integer.parseInt(st.nextToken());
+            values[i] = Integer.parseInt(st.nextToken());
         }
 
-        // dp[i][j] : j만큼 무게까지 담을때, i개를 담은 최대 가치값
+        // dp[i][w]: i번째 물건까지 고려했을때, w무게까지 담을때의 최대값
         int[][] dp = new int[N + 1][K + 1];
 
-        for (int i = 1; i < N + 1; i++) { // 물건 개수만큼 반복
-            for (int w = 0; w < K + 1; w++) { // 배낭 무게별 계산
-                if (weights[i] > w) { // 현재 물건을 넣을 수 없는 경우
-                    dp[i][w] = dp[i - 1][w];
-                } else {
+        for (int i = 1; i < N + 1; i++) {
+            for (int w = 1; w < K + 1; w++) {
+                if (weights[i] <= w) {
                     dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - weights[i]] + values[i]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
                 }
             }
         }
