40ee94c8006e264909d4e4feefff3c4c9572ed97
최수연 <tndus5383@gmail.com>
2025-02-03 20:02:10 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(A);
+        int answer = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            if (K >= A[i]) {
+                answer += K / A[i];
+                K %= A[i];
+            }
+            if (K == 0) {
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
