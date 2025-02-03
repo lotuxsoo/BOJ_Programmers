3f91a3bef8964afe6815f5b08574cb5db038b7da
최수연 <tndus5383@gmail.com>
2025-02-03 20:38:06 +0900

    [Silver IV] Title: 설탕 배달, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -8,17 +8,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
+        int answer = 0;
+
         int five = N / 5;
 
         while (five >= 0) {
-            int remainder = N - (five * 5);
-            if (remainder % 3 != 0) {
-                five--;
-            } else {
-                five += remainder / 3;
+            int remain = N - five * 5;
+            if (remain % 3 == 0) {
+                five += remain / 3;
                 System.out.println(five);
                 return;
             }
+            five--;
         }
 
         System.out.println(-1);
