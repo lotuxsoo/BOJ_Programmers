bea6d553034cafb240ae162ef3e1af4270f9ed92
최수연 <tndus5383@gmail.com>
2025-02-04 10:01:29 +0900

    [Gold III] Title: 나머지 합, Time: 468 ms, Memory: 124612 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,28 +11,33 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N + 1];
+        int[] A = new int[N + 1];
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
-        long[] sum = new long[N + 1];
-        int[] dp = new int[M]; // M으로 나눈 나머지 저장
-
-        long answer = 0;
+        // 합배열 저장
+        long[] S = new long[N + 1];
         for (int i = 1; i <= N; i++) {
-            sum[i] = sum[i - 1] + arr[i];
-            int remainder = (int) (sum[i] % M);
-            if (remainder == 0) {
-                answer++;
-            }
-            dp[remainder]++;
+            S[i] = S[i - 1] + A[i];
         }
 
+        // 같은 나머지 개수 저장
+        long[] C = new long[M];
+        long answer = 0;
+
+        // 합배열을 M으로 나눈 나머지값
+        for (int i = 1; i <= N; i++) {
+            int remainder = (int) (S[i] % M);
+            C[remainder]++;
+        }
+
+        answer += C[0];
         for (int i = 0; i < M; i++) {
-            if (dp[i] > 1) {
-                answer += (long) dp[i] * (dp[i] - 1) / 2;
+            if (C[i] >= 2) {
+                // 나머지가 같은 인덱스중 2개를 뽑
+                answer += C[i] * (C[i] - 1) / 2;
             }
         }
 
