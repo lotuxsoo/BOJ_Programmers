4ea51adcffe59d99624251a0c77a680cd43bd20e
최수연 <tndus5383@gmail.com>
2025-02-04 09:14:32 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1320 ms, Memory: 127904 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -8,37 +8,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] grid = new int[N+1][N+1];
+        int N = Integer.parseInt(st.nextToken()); // 수의 개수
+        int M = Integer.parseInt(st.nextToken()); // 합을 구해야 하는 횟수
+
+        int[][] A = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
-                grid[i][j] = Integer.parseInt(st.nextToken());
+                A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int[][] D  = new int[N+1][N+1];
-
-        D[1][1] = grid[1][1];
-        for (int i = 2; i <= N; i++) {
-            D[i][1] = D[i-1][1] + grid[i][1];
-            D[1][i] = D[1][i-1] + grid[1][i];
-        }
-
-        for (int i = 2; i <= N; i++) {
-            for (int j = 2; j <= N; j++) {
-                D[i][j] = D[i - 1][j] + D[i][j - 1] - D[i - 1][j - 1] + grid[i][j];
+        // 2차원 구간합 S[x1][y1]~S[x2][y2]
+        int[][] S = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                S[i][j] = S[i - 1][j] + S[i][j - 1] - S[i - 1][j - 1] + A[i][j];
             }
         }
 
-        for (int i = 0; i < M; i++) {
+        for (int i = 1; i <= M; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            int sum = D[x2][y2] - D[x2][y1 - 1] - D[x1 - 1][y2] + D[x1 - 1][y1 - 1];
+
+            int sum = S[x2][y2] - S[x1 - 1][y2] - S[x2][y1 - 1] + S[x1 - 1][y1 - 1];
             System.out.println(sum);
         }
     }
