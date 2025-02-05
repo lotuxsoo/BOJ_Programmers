5f613dc96ec48b70941f81204d55c5537ffadd44
최수연 <tndus5383@gmail.com>
2025-02-05 19:23:18 +0900

    [Gold III] Title: 내리막 길, Time: 352 ms, Memory: 37468 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int DFS(int x, int y) {
-        if (x == M - 1 && y == N - 1) {
+        if (x == M && y == N) {
             return 1;
         }
 
@@ -15,7 +15,7 @@ public class Main {
             return dp[x][y];
         }
 
-        dp[x][y] = 0;
+        dp[x][y] = 0; // (경로가 없을수도 아닐수도, 이후 누적)
 
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
@@ -23,11 +23,11 @@ public class Main {
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (!(0 <= nx && nx < M && 0 <= ny && ny < N)) {
+            if (!(1 <= nx && nx <= M && 1 <= ny && ny <= N)) {
                 continue;
             }
 
-            if (map[nx][ny] < map[x][y]) {
+            if (map[x][y] > map[nx][ny]) {
                 dp[x][y] += DFS(nx, ny);
             }
         }
@@ -42,24 +42,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken()); // 세로
-        N = Integer.parseInt(st.nextToken()); // 가로
-
-        map = new int[M][N];
-        for (int i = 0; i < M; i++) {
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        map = new int[M + 1][N + 1];
+        for (int i = 1; i <= M; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
+            for (int j = 1; j <= N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // dp[i][j]: (i,j)에서 (M-1,N-1)까지 갈수있는 경로의 수
-        dp = new int[M][N];
-        for (int i = 0; i < M; i++) {
+        // dp[i][j]: (i,j)~(M,N)까지 갈수있는 경로의 개수
+        dp = new int[M + 1][N + 1];
+
+        // 필수 **미방문 표시**
+        for (int i = 0; i < M + 1; i++) {
             Arrays.fill(dp[i], -1);
         }
 
-        int H = DFS(0, 0);
+        int H = DFS(1, 1);
         System.out.println(H);
     }
 }
