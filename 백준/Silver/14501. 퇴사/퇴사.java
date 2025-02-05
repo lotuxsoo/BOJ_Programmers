a097197b9daa5e065d2daa4c4fe9d4d0fd0ed8b3
최수연 <tndus5383@gmail.com>
2025-02-05 14:05:56 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,45 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Day {
-        int T; // 걸리는 기간
-        int P; // 금액
-
-        Day(int T, int P) {
-            this.T = T;
-            this.P = P;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        ArrayList<Day> list = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
+        int[] T = new int[N + 1];
+        int[] P = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int T = Integer.parseInt(st.nextToken());
-            int P = Integer.parseInt(st.nextToken());
-            list.add(new Day(T, P));
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] D = new int[N + 1];
+        // dp[i]: i부터 끝까지 얻는 최대이익 (역순 dp)
+        int[] dp = new int[N + 2];
 
-        for (int i = N - 1; i >= 0; i--) {
-            Day d = list.get(i);
-            if (i + d.T - 1 < N) {
-                D[i] = Math.max(D[i + 1], D[i + d.T] + d.P);
+        for (int i = N; i > 0; i--) {
+            if (i + T[i] - 1 <= N) {
+                dp[i] = Math.max(dp[i + T[i]] + P[i], dp[i + 1]);
             } else {
-                D[i] = D[i + 1];
+                dp[i] = dp[i + 1];
             }
         }
 
-        Arrays.sort(D);
-        System.out.println(D[D.length - 1]);
+        System.out.println(dp[1]);
     }
 }
