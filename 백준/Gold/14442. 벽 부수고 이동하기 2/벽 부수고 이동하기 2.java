3bc647130e038b9571f677aba069d0e67f8fa420
최수연 <tndus5383@gmail.com>
2025-02-07 04:28:08 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1868 ms, Memory: 411500 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -7,59 +7,22 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Node {
-        int x, y, z;
+    static class State {
+        int x, y, cnt;
 
-        Node(int x, int y, int z) {
+        State(int x, int y, int cnt) {
             this.x = x;
             this.y = y;
-            this.z = z; // 벽부순 횟수
+            this.cnt = cnt;
         }
     }
 
     static int N, M, K;
-    static int[][][] dp;
     static int[][] map;
-
-    static int BFS() {
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(0, 0, 0));
-        dp[0][0][0] = 1;
-
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
-        while (!queue.isEmpty()) {
-            Node cur = queue.poll();
-            int x = cur.x, y = cur.y, z = cur.z;
-
-            if (x == N - 1 && y == M - 1) {
-                return dp[x][y][z];
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (!(0 <= nx && nx < N && 0 <= ny && ny < M)) {
-                    continue;
-                }
-
-                if (map[nx][ny] == 0) {
-                    if (dp[nx][ny][z] > dp[x][y][z] + 1) {
-                        dp[nx][ny][z] = dp[x][y][z] + 1;
-                        queue.add(new Node(nx, ny, z));
-                    }
-                } else if (map[nx][ny] == 1 && z + 1 <= K) {
-                    if (dp[nx][ny][z + 1] > dp[x][y][z] + 1) {
-                        dp[nx][ny][z + 1] = dp[x][y][z] + 1;
-                        queue.add(new Node(nx, ny, z + 1));
-                    }
-                }
-            }
-        }
-
-        return -1;
-    }
+    static int[][][] dp;
+    static final int INF = 1_000_000_000;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -67,23 +30,61 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-
-        dp = new int[N][M][K + 1]; // 최소 경로 저장
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                Arrays.fill(dp[i][j], Integer.MAX_VALUE);
+        map = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            char[] ch = br.readLine().toCharArray();
+            for (int j = 1; j <= M; j++) {
+                map[i][j] = ch[j - 1] - '0';
             }
         }
 
-        map = new int[N][M];
+        // (x,y)에서 k번까지 부쉈을때의 최단거리
+        dp = new int[N + 1][M + 1][K + 1];
 
-        for (int i = 0; i < N; i++) {
-            String[] splits = br.readLine().split("");
-            for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(splits[j]);
+        // INF로 초기화
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= M; j++) {
+                Arrays.fill(dp[i][j], INF);
             }
         }
 
-        System.out.println(BFS());
+        // 초기값
+        dp[1][1][0] = 0;
+        int MIN_DIST = INF;
+
+        Queue<State> queue = new LinkedList<>();
+        queue.add(new State(1, 1, 0));
+
+        while (!queue.isEmpty()) {
+            State cur = queue.poll();
+
+            if (cur.x == N && cur.y == M) {
+                MIN_DIST = Math.min(MIN_DIST, dp[cur.x][cur.y][cur.cnt] + 1);
+                continue;
+            }
+
+            // 4방향 탐색
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+                if (!(1 <= nx && nx <= N && 1 <= ny && ny <= M)) {
+                    continue;
+                }
+
+                if (map[nx][ny] == 0) {
+                    if (dp[nx][ny][cur.cnt] > dp[cur.x][cur.y][cur.cnt] + 1) {
+                        dp[nx][ny][cur.cnt] = dp[cur.x][cur.y][cur.cnt] + 1;
+                        queue.add(new State(nx, ny, cur.cnt));
+                    }
+                } else if ((map[nx][ny] == 1) && cur.cnt + 1 <= K) {
+                    if (dp[nx][ny][cur.cnt + 1] > dp[cur.x][cur.y][cur.cnt] + 1) {
+                        dp[nx][ny][cur.cnt + 1] = dp[cur.x][cur.y][cur.cnt] + 1;
+                        queue.add(new State(nx, ny, cur.cnt + 1));
+                    }
+                }
+            }
+        }
+
+        System.out.println(MIN_DIST == INF ? -1 : MIN_DIST);
     }
 }
