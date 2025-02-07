a9764617fea44d671d57db6e56020f48389d6c17
최수연 <tndus5383@gmail.com>
2025-02-07 13:55:01 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 2.51 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,77 +1,80 @@
 import java.util.*;
 
 class Solution {
-    static class Node {
-        int x, y, dir, cost;
-        Node(int x, int y, int dir, int cost) {
+    static class State {
+        int x, y, pos;
+        State(int x, int y, int pos) {
             this.x = x;
             this.y = y;
-            this.dir = dir;
-            this.cost = cost;
+            this.pos = pos;
         }
     }
     
+    static int[][][] dp;
     static int N;
-    static int[][][] dist;
+    static final int INF = 1_000_000_000;
     
-    static int BFS(int[][] board) { 
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(0, 0, -1, 0)); // 초기: -1(방향없음)
+    // 세로방향:0,2 가로방향:1,3
+    static int[] dx = {-1,0,1,0};
+    static int[] dy = {0,-1,0,1};
+    
+    public int solution(int[][] board) {
+        int answer = INF;
+        N = board.length;
         
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,-1,1};
+        // dp[x][y][pos]: 현재까지의 최소 비용
+        dp = new int[N][N][2]; // 0:세로, 1:가로
         
-        int minCost = Integer.MAX_VALUE;
+        // INF로 초기화
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                Arrays.fill(dp[i][j], INF);
+            }
+        }
+        
+        // 초기값 설정
+        dp[0][0][0] = 0;
+        dp[0][0][1] = 0;
+        
+        Queue<State> queue = new LinkedList<>();
+        queue.add(new State(0,0,0));
+        queue.add(new State(0,0,1));
         
         while (!queue.isEmpty()) {
-            Node cur = queue.poll();
-            int x = cur.x, y = cur.y, dir = cur.dir, cost = cur.cost;
+            State cur = queue.poll();
+            int x=cur.x, y=cur.y, pos=cur.pos;
             
-            if (x == N-1 && y == N-1) {
-                // 이 좌표에 도달한 모든 방향의 비용 중 최솟값을 선택
-                minCost = Math.min(minCost, cost);
+            if (x==N-1 && y==N-1) {
+                answer = Math.min(answer, dp[x][y][pos]);
                 continue;
             }
             
-            // 0:상 1:하 2:좌 3:우
+            if (dp[x][y][pos] == INF) continue;
+            
+            // 세로방향:0,2 가로방향:1,3
             for (int i=0; i<4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (!(0 <= nx && nx < N && 0 <= ny && ny < N)) continue;
-                if (board[nx][ny] == 1) continue;
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+                if (!(0<=nx&&nx<N&&0<=ny&&ny<N)) continue;
                 
-                if (dir == i || dir == -1) {
-                    int newCost = cost + 100;
-                    if (dist[nx][ny][i] > newCost) {
-                        dist[nx][ny][i] = newCost;
-                        queue.add(new Node(nx, ny, i, dist[nx][ny][i]));
-                    }
-                } else {
-                    int newCost = cost + 600;
-                    if (dist[nx][ny][i] > newCost) {
-                        dist[nx][ny][i] = newCost;
-                        queue.add(new Node(nx, ny, i, dist[nx][ny][i]));
-                    }
+                // 이동 가능한 경로 탐색
+                if (board[nx][ny] == 0) {
+                    if (((i%2==0) && (pos==0)) || ((i%2==1) && (pos==1))) {
+                        if (dp[nx][ny][pos] > dp[x][y][pos] + 100) {
+                            dp[nx][ny][pos] = dp[x][y][pos] + 100;
+                            queue.add(new State(nx,ny,pos));
+                        }
+                    } else {
+                        if (dp[nx][ny][Math.abs(1-pos)] > dp[x][y][pos] + 600) {
+                            dp[nx][ny][Math.abs(1-pos)] = dp[x][y][pos] + 600;
+                            queue.add(new State(nx,ny,Math.abs(1-pos)));
+                        }
+                    } 
                 }
             }
         }
         
-        return minCost;
-    }
-    
-    public int solution(int[][] board) {
-        int answer = 0;
         
-        N = board.length;
-        dist = new int[N][N][4];
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<N; j++) {
-                Arrays.fill(dist[i][j], Integer.MAX_VALUE);   
-            }
-        }
-        
-        answer = BFS(board);
-
         return answer;
     }
 }
