5916dc74994128c976e2ebb0d7321985001bae51
최수연 <tndus5383@gmail.com>
2025-02-07 10:11:32 +0900

    [level 3] Title: 양과 늑대, Time: 38.32 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,50 +1,57 @@
 import java.util.*;
 
 class Solution {
-    static class Node {
-        int idx, sheep, wolf;
+    static class State {
+        int index, sheep, wolf;
         Set<Integer> nodes;
-        Node(int idx, int sheep, int wolf, Set<Integer> nodes) {
-            this.idx = idx;
+        
+        State(int index, int sheep, int wolf, Set<Integer> nodes) {
+            this.index = index;
             this.sheep = sheep;
             this.wolf = wolf;
             this.nodes = nodes;
         }
     }
     
+    static List<List<Integer>> adjList = new ArrayList<>();
+    
     public int solution(int[] info, int[][] edges) {
         int answer = 0;
         
-        int n = info.length;
-        ArrayList<Integer>[] A = new ArrayList[n];
-        for (int i=0; i<n; i++) {
-            A[i] = new ArrayList<>();
-        }
-        for (int[] edge : edges) {
-            A[edge[0]].add(edge[1]);
+        int N = info.length;
+        
+        for (int i=0; i<N; i++) {
+            adjList.add(new ArrayList<>());
         }
         
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(0, 1, 0, new HashSet<>(Set.of(0))));
+        for (int[] edge : edges) {
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
+        }
+                
+        Queue<State> queue = new LinkedList<>();
+        queue.add(new State(0, 1, 0, new HashSet<>(Set.of(0))));
         
         while (!queue.isEmpty()) {
-            Node cur = queue.poll();
-            answer = Math.max(answer, cur.sheep);
+            State cur = queue.poll();
             
-            for (int node : cur.nodes) {
-                for (int next : A[node]) {
+            answer = Math.max(answer, cur.sheep);            
+            
+            for (int node : cur.nodes) { // 후보 노드 집합 탐색
+                for (int next : adjList.get(node)) { // 후보 노드 연결관계 탐색
                     if (cur.nodes.contains(next)) continue;
-                    
-                    int nextSheep = (info[next] == 0) ? cur.sheep + 1 : cur.sheep;
-                    int nextWolf = (info[next] == 1) ? cur.wolf + 1 : cur.wolf;
 
-                    if (nextSheep > nextWolf) {
+                    if (info[next] == 0) {
                         Set<Integer> newSet = new HashSet<>(cur.nodes);
                         newSet.add(next);
-                        queue.add(new Node(node, nextSheep, nextWolf, newSet));
+                        queue.add(new State(next, cur.sheep+1, cur.wolf, newSet));
+                    } else if ((info[next] == 1) && cur.wolf+1 < cur.sheep) {
+                        Set<Integer> newSet = new HashSet<>(cur.nodes);
+                        newSet.add(next);
+                        queue.add(new State(next, cur.sheep, cur.wolf+1, newSet));
                     }
-                }
-            } 
+                } 
+            }
         }
         
         return answer;
