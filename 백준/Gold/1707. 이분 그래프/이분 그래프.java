b75d6c1d1b8f4d5ecd1a6bd2051e8f9b41e51cb9
최수연 <tndus5383@gmail.com>
2025-02-20 15:15:18 +0900

    [Gold IV] Title: 이분 그래프, Time: 812 ms, Memory: 269076 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,54 +1,48 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static boolean BFS(int x) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(x);
-        check[x] = 1;
+    static void DFS(int x, int nowColor) {
 
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
+        color[x] = nowColor;
 
-            for (int next : graph[cur]) {
-                if (check[next] == 0) {
-                    check[next] = -check[cur];
-                    queue.add(next);
-                } else {
-                    if (check[next] == check[cur]) {
-                        return false;
-                    }
+        for (int next : graph[x]) {
+            if (color[next] == 0) {
+                DFS(next, -nowColor);
+            } else {
+                if (color[next] == color[x]) {
+                    found = true;
+                    return;
                 }
             }
         }
-        return true;
     }
 
     static ArrayList<Integer>[] graph;
-    static int[] check;
-    static int V, E;
+    static int[] color;
+    static boolean found; // 이분그래프 아닌걸 찾음
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int K = Integer.parseInt(br.readLine());
-        for (int i = 0; i < K; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            V = Integer.parseInt(st.nextToken());
 
-            // 인접리스트 초기화 (양방향)
+        int K = Integer.parseInt(br.readLine());
+        while (K-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+
             graph = new ArrayList[V + 1];
-            for (int j = 0; j < V + 1; j++) {
-                graph[j] = new ArrayList<>();
+            for (int i = 0; i < V + 1; i++) {
+                graph[i] = new ArrayList<>();
             }
 
-            E = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < E; j++) {
+            for (int i = 0; i < E; i++) {
                 st = new StringTokenizer(br.readLine());
                 int u = Integer.parseInt(st.nextToken());
                 int v = Integer.parseInt(st.nextToken());
@@ -56,19 +50,24 @@ public class Main {
                 graph[v].add(u);
             }
 
-            check = new int[V + 1]; // 0 초기화 상태
-            boolean found = true;
+            color = new int[V + 1]; // 각 정점의 색을 구분
+            found = false;
 
-            for (int j = 1; j <= V; j++) {
-                if (check[j] == 0) {
-                    if (!BFS(j)) {
-                        found = false;
-                        break;
-                    }
+            for (int i = 1; i <= V; i++) {
+                if (found) {
+                    break;
+                }
+                if (color[i] == 0) {
+                    DFS(i, 1);
                 }
             }
 
-            System.out.println(found ? "YES" : "NO");
+            if (found) {
+                sb.append("NO\n");
+            } else {
+                sb.append("YES\n");
+            }
         }
+        System.out.println(sb.toString());
     }
 }
