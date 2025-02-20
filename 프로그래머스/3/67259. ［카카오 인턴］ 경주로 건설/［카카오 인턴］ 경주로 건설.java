5cf6c08354d81196d30e9508cb2dca3d816e7f80
최수연 <tndus5383@gmail.com>
2025-02-20 14:12:30 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 6.74 ms, Memory: 94.6 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -13,7 +13,7 @@ class Solution {
 
     static int calculateCost(int prevDir, int dir, int cost) {
         int newCost = cost;
-        if (prevDir==-1 || (prevDir-dir)%2 == 0) {
+        if (prevDir == -1 || (prevDir == dir)) {
             newCost += 100;
         } else {
             newCost += 600;
@@ -23,24 +23,34 @@ class Solution {
     
     static int[] dx = {-1,0,1,0}; // 상(0),좌(1),하(2),우(3)
     static int[] dy = {0,-1,0,1};
-    static int[][][] cost;
+    static int[][][] dp;
     static int N;
-    static int bestCost = Integer.MAX_VALUE;
     
     public int solution(int[][] board) {
         int answer = 0;
         N = board.length;
-        cost = new int[N][N][4];
+        
+        // (x,y)에서의 최소비용 저장
+        dp = new int[N][N][2];
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                Arrays.fill(dp[i][j], Integer.MAX_VALUE);   
+            }
+        }
+        
+        // 시작점 초기화 필수
+        dp[0][0][0] = 0;
+        dp[0][0][1] = 0;
 
         PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.cost,b.cost));
         pq.add(new Node(0, 0, -1, 0));
+        pq.add(new Node(0, 0, -1, 1));
         
         while (!pq.isEmpty()) {
             Node now = pq.poll();
             
             if (now.x == N-1 && now.y == N-1) {
-                bestCost = Math.min(bestCost, now.cost);
-                break;
+                return dp[N-1][N-1][now.dir];
             }
             
             for (int i=0; i<4; i++) {
@@ -49,16 +59,14 @@ class Solution {
                 if (!(0<=nx && nx<N && 0<=ny && ny<N)) continue;
                 if (board[nx][ny] == 1) continue;
                 
-                int newCost = calculateCost(now.dir, i, now.cost);
-                if (cost[nx][ny][i] == 0 || cost[nx][ny][i] > newCost) {
-                    cost[nx][ny][i] = newCost;
-                    pq.add(new Node(nx, ny, i, newCost));
+                int newCost = calculateCost(now.dir, i%2, now.cost);
+                if (dp[nx][ny][i%2] == Integer.MAX_VALUE || dp[nx][ny][i%2] > newCost) {
+                    dp[nx][ny][i%2] = newCost;
+                    pq.add(new Node(nx, ny, i%2, newCost));
                 }
             }
         }
         
-        answer = bestCost;
-        
-        return answer;
+        return -1;
     }
 }
