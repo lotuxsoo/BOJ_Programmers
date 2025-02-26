812d3d39fa5b786acf4608f9b5e0e275c7aeec1e
최수연 <tndus5383@gmail.com>
2025-02-26 22:51:10 +0900

    [Gold IV] Title: 플로이드, Time: 360 ms, Memory: 42776 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,49 +8,47 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int N, M;
-    static int[][] D;
+    static int[][] dist;
     static final int INF = 1_000_000_000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        D = new int[N + 1][N + 1];
 
-        // 비용 정보 인접행렬에 초기화
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (i == j) {
-                    D[i][j] = 0;
-                } else {
-                    D[i][j] = INF;
-                }
-            }
+        dist = new int[N + 1][N + 1];
+
+        for (int i = 1; i < N + 1; i++) {
+            Arrays.fill(dist[i], INF);
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            dist[i][i] = 0;
         }
 
         for (int i = 0; i < M; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            D[s][e] = Math.min(D[s][e], w);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            dist[a][b] = Math.min(dist[a][b], c); // 효율적인 노선으로 저장
         }
 
-        // 플로이드
         for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    if (D[i][j] > D[i][k] + D[k][j]) {
-                        D[i][j] = D[i][k] + D[k][j];
+            for (int s = 1; s <= N; s++) {
+                for (int e = 1; e <= N; e++) {
+                    if (dist[s][e] > dist[s][k] + dist[k][e]) {
+                        dist[s][e] = dist[s][k] + dist[k][e];
                     }
                 }
             }
         }
 
         StringBuilder sb = new StringBuilder();
+
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                sb.append(D[i][j] == INF ? 0 : D[i][j]).append(" ");
+                sb.append(dist[i][j] == INF ? 0 : dist[i][j]).append(" ");
             }
             sb.append("\n");
         }
