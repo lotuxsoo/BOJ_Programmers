444590f443ee5200ea2b524ace51bbe07f820d03
최수연 <tndus5383@gmail.com>
2025-03-01 01:02:10 +0900

    [Gold IV] Title: 연구소, Time: 872 ms, Memory: 299456 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -2,31 +2,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M;
-    static int[][] map;
-    static int maxSafeZone = 0;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
-    static void dfs(int cnt) {
+    static void backtrack(int cnt) {
         if (cnt == 3) {
             goVirus();
             return;
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 0) {
-                    map[i][j] = 1;
-                    dfs(cnt + 1);
-                    map[i][j] = 0;
-                }
+        for (int i = 0; i < emptyList.size(); i++) {
+            if (!visited[i]) {
+                int[] cur = emptyList.get(i);
+                map[cur[0]][cur[1]] = 1;
+                visited[i] = true;
+                backtrack(cnt + 1);
+                map[cur[0]][cur[1]] = 0;
+                visited[i] = false;
             }
         }
     }
@@ -34,14 +30,9 @@ public class Main {
     static void goVirus() {
         Queue<int[]> queue = new LinkedList<>();
         boolean[][] visited = new boolean[N][M];
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 2) {
-                    queue.add(new int[]{i, j});
-                    visited[i][j] = true;
-                }
-            }
+        for (int[] cur : virusList) {
+            queue.add(cur);
+            visited[cur[0]][cur[1]] = true;
         }
 
         int[][] copied = new int[map.length][];
@@ -51,24 +42,23 @@ public class Main {
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
-            int x = cur[0], y = cur[1];
 
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
                 if (!(0 <= nx && nx < N && 0 <= ny && ny < M)) {
                     continue;
                 }
+
                 if (!visited[nx][ny] && copied[nx][ny] == 0) {
                     copied[nx][ny] = 2;
-                    queue.add(new int[]{nx, ny});
                     visited[nx][ny] = true;
+                    queue.add(new int[]{nx, ny});
                 }
             }
         }
 
-        visited = new boolean[N][M];
-        // 영역 세기
         int count = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
@@ -81,6 +71,14 @@ public class Main {
         maxSafeZone = Math.max(maxSafeZone, count);
     }
 
+    static int N, M;
+    static int[][] map;
+    static int maxSafeZone = 0;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static ArrayList<int[]> emptyList = new ArrayList<>();
+    static ArrayList<int[]> virusList = new ArrayList<>();
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -92,10 +90,16 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 0) {
+                    emptyList.add(new int[]{i, j});
+                } else if (map[i][j] == 2) {
+                    virusList.add(new int[]{i, j});
+                }
             }
         }
 
-        dfs(0); // 백트래킹으로 모든 가지수 세기
+        visited = new boolean[emptyList.size()];
+        backtrack(0);
 
         System.out.println(maxSafeZone);
     }
