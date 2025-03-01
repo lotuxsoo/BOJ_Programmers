f20e9429468f259b19b0618b8e0cc0034d134005
최수연 <tndus5383@gmail.com>
2025-03-01 09:58:59 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 112.51 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -3,47 +3,72 @@ import java.util.*;
 class Solution {
     public int[] solution(int rows, int columns, int[][] queries) {
         int[] answer = new int[queries.length];
+        
         int[][] map = new int[rows][columns];
-        for (int i=0; i<rows; i++) {
-            for (int j=0; j<columns; j++) {
-                map[i][j] = i*columns + j+1;
+        for (int r=0; r<rows; r++) {
+            for (int c=0; c<columns; c++) {
+                map[r][c] = r*columns + c+1;
             }
         }
         
         for (int q=0; q<queries.length; q++) {
             int[] query = queries[q];
             int x1 = query[0]-1, y1 = query[1]-1, x2 = query[2]-1, y2 = query[3]-1;
-        
-            // 시계방향 (오른쪽 -> 위쪽 -> 왼쪽 -> 아래쪽) 
-            int n = x2 - x1;
-            int m = y2 - y1;
+                   
+            List<Integer> ring = new ArrayList<>();
             
-            int firstNum = map[x1][y1+m]; // 오른쪽 방향 가장 끝
-            int min = firstNum;
-            
-            for (int i=m; i>0; i--) { // 오른쪽
-                map[x1][y1+i] = map[x1][y1+i-1];
-                min = Math.min(min, map[x1][y1+i]);
+            // 상단 가로줄
+            for (int c=y1; c<=y2; c++) {
+                ring.add(map[x1][c]);
             }
             
-            for (int i=1; i<=n; i++) { // 위쪽
-                map[x1+i-1][y1] = map[x1+i][y1];
-                min = Math.min(min, map[x1+i-1][y1]);
+            // 오른쪽 세로줄
+            for (int r=x1+1; r<=x2; r++) {
+                ring.add(map[r][y2]);
             }
             
-            for (int i=m; i>0; i--) { // 왼쪽
-                map[x2][y2-i] = map[x2][y2-i+1];  
-                min = Math.min(min, map[x2][y2-i]);
+            // 하단 가로줄
+            for (int c=y2-1; c>=y1; c--) {
+                ring.add(map[x2][c]);
             }
             
-            for (int i=1; i<=n; i++) { // 아래쪽
-                map[x2-i+1][y2] = map[x2-i][y2];
-                min = Math.min(min, map[x2-i+1][y2]);
+            // 왼쪽 세로줄
+            for (int r=x2-1; r>x1; r--) {
+                ring.add(map[r][y1]);
             }
             
-            map[x1+1][y1+m] = firstNum;
-
+            // 한칸 회전
+            int last = ring.get(ring.size()-1);
+            ring.remove(ring.size()-1);
+            ring.add(0, last);
+            
+            int min = Integer.MAX_VALUE;
+            for (int x : ring) {
+                min = Math.min(min, x);
+            }
             answer[q] = min;
+            
+            int idx = 0;
+            
+            // 상단 가로줄
+            for (int c=y1; c<=y2; c++) {
+                map[x1][c] = ring.get(idx++);
+            }
+            
+            // 오른쪽 세로줄
+            for (int r=x1+1; r<=x2; r++) {
+                map[r][y2] = ring.get(idx++);
+            }
+            
+            // 하단 가로줄
+            for (int c=y2-1; c>=y1; c--) {
+                map[x2][c] = ring.get(idx++);
+            }
+            
+            // 왼쪽 세로줄
+            for (int r=x2-1; r>x1; r--) {
+                map[r][y1] = ring.get(idx++);
+            }
         }
         
         return answer;
