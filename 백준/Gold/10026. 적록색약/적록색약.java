7b8b7c1f036c8dbde340f153a80d4e9cacd32831
최수연 <tndus5383@gmail.com>
2025-03-07 09:18:45 +0900

    [Gold V] Title: 적록색약, Time: 132 ms, Memory: 16116 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -5,67 +5,52 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static void dfs(int x, int y, int flag) {
-        visited[x][y] = true;
+    static void dfs(int x, int y, char[][] map) {
+        char current = map[x][y];
+        map[x][y] = 'X';
 
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i], ny = y + dy[i];
 
-            if (flag == 0) {
-                if ((0 <= nx && nx < N && 0 <= ny && ny < N) && !visited[nx][ny] && map[x][y] == map[nx][ny]) {
-                    dfs(nx, ny, flag);
-                }
-            } else {
-                if (!(0 <= nx && nx < N && 0 <= ny && ny < N) || visited[nx][ny]) {
-                    continue;
-                }
-                if (map[x][y] == 'R' || map[x][y] == 'G') {
-                    if (map[nx][ny] == 'R' || map[nx][ny] == 'G') {
-                        dfs(nx, ny, flag);
-                    }
-                } else if (map[x][y] == 'B' && map[nx][ny] == 'B') {
-                    dfs(nx, ny, flag);
-                }
+            if ((0 <= nx && nx < N && 0 <= ny && ny < N) && map[nx][ny] != 'X' && map[nx][ny] == current) {
+                dfs(nx, ny, map);
             }
         }
     }
 
     static int N;
-    static char[][] map;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static boolean[][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        map = new char[N][N];
+
+        char[][] map = new char[N][N];
+        char[][] blindMap = new char[N][N];
+
         for (int i = 0; i < N; i++) {
             map[i] = br.readLine().toCharArray();
+            for (int j = 0; j < N; j++) {
+                blindMap[i][j] = map[i][j] == 'G' ? 'R' : map[i][j];
+            }
         }
 
-        int normalCount = 0, abnormalCount = 0;
+        int count = 0, blindCount = 0;
 
-        visited = new boolean[N][N];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (!visited[i][j]) {
-                    dfs(i, j, 0);
-                    normalCount++;
+                if (map[i][j] != 'X') {
+                    count++;
+                    dfs(i, j, map);
+                }
+                if (blindMap[i][j] != 'X') {
+                    blindCount++;
+                    dfs(i, j, blindMap);
                 }
             }
         }
 
-        visited = new boolean[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!visited[i][j]) {
-                    dfs(i, j, 1);
-                    abnormalCount++;
-                }
-            }
-        }
-
-        System.out.println(normalCount + " " + abnormalCount);
+        System.out.println(count + " " + blindCount);
     }
 }
