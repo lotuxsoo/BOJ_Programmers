b4968298bfac5378d53274db84eea95356559a19
최수연 <tndus5383@gmail.com>
2025-03-08 10:56:22 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 120 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,29 +6,34 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    static int N;
+    static int[] A;
+    static int[] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N + 1];
+        N = Integer.parseInt(br.readLine());
+        A = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
         }
 
-        // dp[i]: A[i]를 **마지막 원소**로 하는 증가하는 부분수열중 최대합
-        int[] dp = new int[N + 1];
-        int MAX_VAL = Integer.MIN_VALUE;
-
-        for (int i = 1; i <= N; i++) {
+        // dp[i]: A[i]가 마지막 원소인 증가부분수열 중 합이 가장 큰것
+        dp = new int[N];
+        for (int i = 0; i < N; i++) {
             dp[i] = A[i];
-            for (int j = 1; j < i; j++) {
+            for (int j = 0; j < i; j++) {
                 if (A[i] > A[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + A[i]);
                 }
             }
-            MAX_VAL = Math.max(MAX_VAL, dp[i]);
         }
 
-        System.out.println(MAX_VAL);
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
     }
 }
