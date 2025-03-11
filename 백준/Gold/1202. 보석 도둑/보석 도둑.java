1d67cb7d5358b5e66f3f3b96c1a73f9dc23a47a1
최수연 <tndus5383@gmail.com>
2025-03-11 19:22:29 +0900

    [Gold II] Title: 보석 도둑, Time: 1432 ms, Memory: 127724 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -2,69 +2,52 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Node implements Comparable<Node> {
-        int M, V;
+    static class Jewel {
+        int weight, value;
 
-        Node(int M, int V) {
-            this.M = M; // 무게
-            this.V = V; // 가격
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            if (this.M == o.M) {
-                return o.V - this.V;
-            }
-            return this.M - o.M;
+        Jewel(int weight, int value) {
+            this.weight = weight;
+            this.value = value;
         }
     }
 
-    static int N, K; // 보석개수, 가방 개수
-    static int[] C;
+    static int N, K;
+    static ArrayList<Jewel> jewels = new ArrayList<>();
+    static ArrayList<Integer> bags = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-
-        ArrayList<Node> nodes = new ArrayList<>();
-
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int M = Integer.parseInt(st.nextToken()); // 무게
-            int V = Integer.parseInt(st.nextToken()); // 가격
-            nodes.add(new Node(M, V));
+            int m = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            jewels.add(new Jewel(m, v));
         }
-
-        C = new int[K];
         for (int i = 0; i < K; i++) {
-            st = new StringTokenizer(br.readLine());
-            C[i] = Integer.parseInt(st.nextToken());
+            bags.add(Integer.parseInt(br.readLine()));
         }
 
-        Arrays.sort(C); // 가방 무게 오름차순
-        Collections.sort(nodes); // 보석 무게 오름차순, 가격 내림차순
+        Collections.sort(jewels, (a, b) -> Integer.compare(a.weight, b.weight));
+        Collections.sort(bags);
 
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
-        long answer = 0;
+
         int idx = 0;
+        long answer = 0;
 
-        for (int i = 0; i < K; i++) {
+        for (int bag : bags) {
 
-            while (idx < nodes.size()) {
-                if (C[i] >= nodes.get(idx).M) {
-                    pq.add(nodes.get(idx).V);
-                    idx++;
-                } else {
-                    break;
-                }
+            while (idx < N && jewels.get(idx).weight <= bag) {
+                pq.add(jewels.get(idx).value);
+                idx++;
             }
 
             if (!pq.isEmpty()) {
