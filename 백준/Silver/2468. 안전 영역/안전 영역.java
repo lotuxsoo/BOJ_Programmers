45be55ce34c6a0fa6f13f524480b940c34dc1928
최수연 <tndus5383@gmail.com>
2025-03-19 20:47:44 +0900

    [Silver I] Title: 안전 영역, Time: 240 ms, Memory: 19512 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -6,39 +6,19 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static void initFlood(int height) {
-        floodMap = new int[N][N];
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (map[i][j] <= height) {
-                    floodMap[i][j] = FLOOD;
-                }
-            }
-        }
-    }
-
-    static void dfs(int x, int y) {
+    static void dfs(int x, int y, int rain) {
         visited[x][y] = true;
 
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (!(0 <= nx && nx < N && 0 <= ny && ny < N)) {
-                continue;
-            }
-            if (floodMap[nx][ny] != FLOOD && !visited[nx][ny]) {
-                dfs(nx, ny);
+            int nx = x + dx[i], ny = y + dy[i];
+            if ((0 <= nx && nx < N && 0 <= ny && ny < N) && !visited[nx][ny] && map[nx][ny] > rain) {
+                dfs(nx, ny, rain);
             }
         }
     }
 
-    static final int FLOOD = -1;
     static int N;
     static int[][] map;
-    static int[][] floodMap;
-    static int MAX_HEIGHT = 0;
-    static int ANS = 0;
     static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
@@ -47,30 +27,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
+        int maxHeight = 0;
+
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                MAX_HEIGHT = Math.max(MAX_HEIGHT, map[i][j]);
+                maxHeight = Math.max(maxHeight, map[i][j]);
             }
         }
 
-        while (MAX_HEIGHT-- > 0) {
-            int count = 0;
-            initFlood(MAX_HEIGHT);
-
+        int answer = 0;
+        for (int rain = 0; rain < maxHeight; rain++) {
+            int cnt = 0;
             visited = new boolean[N][N];
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (floodMap[i][j] != FLOOD && !visited[i][j]) {
-                        dfs(i, j);
-                        count++;
+                    if (map[i][j] > rain && !visited[i][j]) {
+                        dfs(i, j, rain);
+                        cnt++;
                     }
                 }
             }
-            ANS = Math.max(ANS, count);
+            answer = Math.max(answer, cnt);
         }
 
-        System.out.println(ANS);
+        System.out.println(answer);
     }
 }
