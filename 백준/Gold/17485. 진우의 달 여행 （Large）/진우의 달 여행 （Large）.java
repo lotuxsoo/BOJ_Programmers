4bd5bd65853684e3c506df679fab400e910ffafa
최수연 <tndus5383@gmail.com>
2025-03-21 21:01:01 +0900

    [Gold V] Title: 진우의 달 여행 (Large), Time: 604 ms, Memory: 112688 KB -BaekjoonHub

---
+++ 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.java
@@ -0,0 +1,69 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static void solve() {
+
+        // dp[i][j][k]: (i,j)위치로 k방향에서 왔을 때 필요한 연료의 최솟값
+        int[][][] dp = new int[N][M][3];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                Arrays.fill(dp[i][j], Integer.MAX_VALUE);
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < 3; j++) {
+                dp[0][i][j] = map[0][i];
+            }
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (j == 0) {
+                    dp[i][j][1] = Math.min(dp[i - 1][j][0], dp[i - 1][j][2]) + map[i][j];
+                    dp[i][j][2] = Math.min(dp[i - 1][j + 1][0], dp[i - 1][j + 1][1]) + map[i][j];
+                } else if (j == M - 1) {
+                    dp[i][j][0] = Math.min(dp[i - 1][j - 1][1], dp[i - 1][j - 1][2]) + map[i][j];
+                    dp[i][j][1] = Math.min(dp[i - 1][j][0], dp[i - 1][j][2]) + map[i][j];
+                } else {
+                    dp[i][j][0] = Math.min(dp[i - 1][j - 1][1], dp[i - 1][j - 1][2]) + map[i][j];
+                    dp[i][j][1] = Math.min(dp[i - 1][j][0], dp[i - 1][j][2]) + map[i][j];
+                    dp[i][j][2] = Math.min(dp[i - 1][j + 1][0], dp[i - 1][j + 1][1]) + map[i][j];
+                }
+            }
+        }
+
+        int minCost = Integer.MAX_VALUE;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < 3; j++) {
+                minCost = Math.min(minCost, dp[N - 1][i][j]);
+            }
+        }
+        System.out.println(minCost);
+    }
+
+    static int N, M;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        solve();
+    }
+}
