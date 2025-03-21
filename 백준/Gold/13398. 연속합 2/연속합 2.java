c54ecc237b5ddaaa02bdc7f335a5bac127cef8a0
최수연 <tndus5383@gmail.com>
2025-03-21 16:10:51 +0900

    [Gold V] Title: 연속합 2, Time: 260 ms, Memory: 25528 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.java
+++ 백준/Gold/13398. 연속합 2/연속합 2.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,31 +9,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N + 1];
+        int[] A = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] L = new int[N];
-        L[0] = A[0];
-        int result = L[0];
-        for (int i = 1; i < N; i++) {
-            L[i] = Math.max(A[i], L[i - 1] + A[i]);
-            result = Math.max(result, L[i]);
-        }
+        int[] L = new int[N]; // L[i]: 왼쪽에서부터 i를 포함한 연속 최대합
+        int[] R = new int[N]; // R[i]: 오른쪽에서부터 i를 포함한 연속 최대합
 
-        int[] R = new int[N];
+        L[0] = A[0];
         R[N - 1] = A[N - 1];
+        int maxVal = L[0];
+
+        for (int i = 1; i < N; i++) {
+            L[i] = Math.max(L[i - 1] + A[i], A[i]);
+            maxVal = Math.max(maxVal, L[i]);
+        }
         for (int i = N - 2; i >= 0; i--) {
-            R[i] = Math.max(A[i], R[i + 1] + A[i]);
+            R[i] = Math.max(R[i + 1] + A[i], A[i]);
         }
 
         for (int i = 1; i < N - 1; i++) {
-            int temp = L[i - 1] + R[i + 1];
-            result = Math.max(result, temp);
+            maxVal = Math.max(maxVal, L[i - 1] + R[i + 1]);
         }
-
-        System.out.println(result);
+        System.out.println(maxVal);
     }
 }
