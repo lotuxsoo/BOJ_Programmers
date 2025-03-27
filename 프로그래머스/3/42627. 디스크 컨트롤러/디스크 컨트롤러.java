81a0032d40f5e207f17b11e7cc68666f207bd881
최수연 <tndus5383@gmail.com>
2025-03-27 15:17:02 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 11.53 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,67 +1,63 @@
 import java.util.*;
 
 class Solution {
-    static class Job implements Comparable<Job> {
-        int index;
-        int request;
-        int duration;
-        Job(int index,int request,int duration) {
-            this.index = index;
-            this.request = request;
+    static class Task {
+        int duration, requestTime, number;
+        Task(int duration, int requestTime, int number) {
             this.duration = duration;
-        }
-        @Override
-        public int compareTo(Job o) {
-            if (this.duration == o.duration) {
-                if (this.request == o.request) {
-                    return this.index - o.index;
-                }
-                return this.request - o.request;   
-            }
-            return this.duration - o.duration;
+            this.requestTime = requestTime;
+            this.number = number;
         }
     }
     
     public int solution(int[][] jobs) {
         int answer = 0;
         
-        PriorityQueue<Job> pq = new PriorityQueue<>();
-        List<Job> list = new ArrayList<>();
-        
-        for (int i=0; i<jobs.length; i++) {
-            list.add(new Job(i, jobs[i][0], jobs[i][1]));
+        ArrayList<Task> tasks = new ArrayList<>();
+    
+        int n = 0;
+        for (int[] job : jobs) {
+            tasks.add(new Task(job[1], job[0], n++));
         }
         
-        Collections.sort(list, (o1,o2) -> o1.request - o2.request);
-       
-        int[] result = new int[jobs.length];
-        int time = 0;
-        int index = 0;
-        
-        while (index < list.size() || !pq.isEmpty()) {
-            // 현재 시간에 요청된 작업을 우선순위 큐에 추가
-            while (index < list.size() && list.get(index).request <= time) {
-                pq.add(list.get(index));
-                index++;
+        Collections.sort(tasks, (t1, t2) -> {
+            if (t1.duration == t2.duration) {
+                if (t1.requestTime == t2.requestTime) {
+                    return Integer.compare(t1.number, t2.number);
+                }
+                return Integer.compare(t1.requestTime, t2.requestTime);
             }
-            
-            if (!pq.isEmpty()) {
-                Job job = pq.poll();
-                time += job.duration;
-                result[job.index] = time - job.request;
-            } else {
-                time = list.get(index).request;
+            return Integer.compare(t1.duration, t2.duration);
+        });
+        
+        int[][] time = new int[n][2];
+        
+        int now = 0;
+        
+        boolean[] visited = new boolean[n];
+        
+        int cnt = n;
+        
+        while (cnt > 0) {
+            boolean found = false;
+            for (Task task : tasks) {
+                if ((now >= task.requestTime) && !visited[task.number]) {
+                    now += task.duration;
+                    time[task.number] = new int[]{task.requestTime,now};
+                    visited[task.number] = true;
+                    found = true;
+                    break;
+                }
             }
+            if (!found) now++;  
+            else cnt--;
         }
         
-        
-        
-        int sum = 0;
-        for (int x : result) {
-            sum += x;
+        for (int[] a : time) {
+            answer += a[1] - a[0];
         }
         
-        answer = sum / result.length;
+        answer /= n;
         
         return answer;
     }
