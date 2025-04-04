f7a535a8c4c13ac5f6f01f16e57ba340873638c5
최수연 <tndus5383@gmail.com>
2025-04-04 16:28:24 +0900

    [Silver II] Title: 이동하기, Time: 484 ms, Memory: 78784 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -6,32 +6,9 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static void solve() {
-
-        // dp[i][j]: (i,j)까지 도달할 때까지 얻는 최대 가치
-        int[][] dp = new int[N][M];
-        dp[0][0] = map[0][0];
-
-        for (int i = 1; i < N; i++) {
-            dp[i][0] = dp[i - 1][0] + map[i][0];
-        }
-
-        for (int i = 1; i < M; i++) {
-            dp[0][i] = dp[0][i - 1] + map[0][i];
-        }
-
-        for (int i = 1; i < N; i++) {
-            for (int j = 1; j < M; j++) {
-                int max = Math.max(dp[i - 1][j - 1], Math.max(dp[i - 1][j], dp[i][j - 1])) + map[i][j];
-                dp[i][j] = Math.max(max, dp[i][j]);
-            }
-        }
-
-        System.out.println(dp[N - 1][M - 1]);
-    }
-
     static int N, M;
     static int[][] map;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -46,6 +23,22 @@ public class Main {
             }
         }
 
-        solve();
+        dp = new int[N][M];
+
+        dp[0][0] = map[0][0];
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = dp[i - 1][0] + map[i][0];
+        }
+        for (int i = 1; i < M; i++) {
+            dp[0][i] = dp[0][i - 1] + map[0][i];
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < M; j++) {
+                dp[i][j] = Math.max(Math.max(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + map[i][j];
+            }
+        }
+
+        System.out.println(dp[N - 1][M - 1]);
     }
 }
