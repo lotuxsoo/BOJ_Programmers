0e1b8d15765e2952c71182b0a925c247823d306b
최수연 <tndus5383@gmail.com>
2025-04-04 20:06:44 +0900

    [Gold IV] Title: RGB거리 2, Time: 112 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -1,33 +1,40 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
+    static int N;
+    static int[][] cost;
+    static final int INF = 1_000_000_000;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] cost = new int[N][3]; // 0:R, 1:G, 2:B
-
+        N = Integer.parseInt(br.readLine());
+        cost = new int[N][3];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            cost[i][0] = Integer.parseInt(st.nextToken());
-            cost[i][1] = Integer.parseInt(st.nextToken());
-            cost[i][2] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < 3; j++) {
+                cost[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        final int INF = 1000000000;
-        int MIN_VAL = Integer.MAX_VALUE;
-
-        // dp[i][j]: i번집에서 j색깔을 칠했을때 최소비용
         int[][] dp = new int[N][3];
+        int result = INF;
 
-        for (int color = 0; color < 3; color++) {
-            dp[0][0] = cost[0][0];
-            dp[0][1] = cost[0][1];
-            dp[0][2] = cost[0][2];
-            dp[0][color] = INF;
+        for (int firstColor = 0; firstColor < 3; firstColor++) {
+            for (int i = 0; i < N; i++) {
+                Arrays.fill(dp[i], INF);
+            }
+
+            for (int k = 0; k < 3; k++) {
+                if (firstColor == k) {
+                    dp[0][k] = cost[0][k];
+                }
+            }
 
             for (int i = 1; i < N; i++) {
                 dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
@@ -35,9 +42,13 @@ public class Main {
                 dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
             }
 
-            MIN_VAL = Math.min(MIN_VAL, dp[N - 1][color]);
+            for (int k = 0; k < 3; k++) {
+                if (firstColor != k) {
+                    result = Math.min(result, dp[N - 1][k]);
+                }
+            }
         }
 
-        System.out.println(MIN_VAL);
+        System.out.println(result);
     }
 }
