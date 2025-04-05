3a27ff10b491e0f1e5bba3177e8d8a99f4169426
최수연 <tndus5383@gmail.com>
2025-04-05 16:27:26 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 156 ms, Memory: 24772 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -0,0 +1,72 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    static int N, K;
+    static int[] dp;
+    static final int INF = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        K = Integer.parseInt(input[1]);
+
+        dp = new int[100001];
+        Arrays.fill(dp, INF);
+        dp[N] = 0;
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(N);
+
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            boolean found = false;
+
+            for (int i = 0; i < size; i++) {
+                int cur = queue.poll();
+
+                if (cur == K) {
+                    found = true;
+                    count++;
+                    continue;
+                }
+
+                if (cur + 1 >= 0 && cur + 1 <= 100000) {
+                    if (dp[cur + 1] >= dp[cur] + 1) {
+                        dp[cur + 1] = dp[cur] + 1;
+                        queue.add(cur + 1);
+                    }
+                }
+                if (cur - 1 >= 0 && cur - 1 <= 100000) {
+                    if (dp[cur - 1] >= dp[cur] + 1) {
+                        dp[cur - 1] = dp[cur] + 1;
+                        queue.add(cur - 1);
+                    }
+                }
+                if (cur * 2 >= 0 && cur * 2 <= 100000) {
+                    if (dp[cur * 2] >= dp[cur] + 1) {
+                        dp[cur * 2] = dp[cur] + 1;
+                        queue.add(cur * 2);
+                    }
+                }
+            }
+
+            if (found) {
+                System.out.println(dp[K]);
+                System.out.println(count);
+                break;
+            }
+        }
+
+    }
+}
