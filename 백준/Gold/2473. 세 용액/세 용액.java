7a517d0b5936df9f505fd5e5f901b9b32d2892a3
최수연 <tndus5383@gmail.com>
2025-04-07 22:12:23 +0900

    [Gold III] Title: 세 용액, Time: 224 ms, Memory: 17320 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -2,40 +2,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
 
+    static int N;
+    static long[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        long[] value = new long[N];
+        N = Integer.parseInt(br.readLine());
+        arr = new long[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            value[i] = Long.parseLong(st.nextToken());
+            arr[i] = Long.parseLong(st.nextToken());
         }
 
-        Arrays.sort(value);
+        Arrays.sort(arr);
+        long closest = Long.MAX_VALUE;
+        long[] result = new long[3];
 
-        long minSum = Long.MAX_VALUE;
-        long[] arr = new long[3];
-
+        // 투포인터 + 이분탐색
         for (int i = 0; i < N - 2; i++) {
             int left = i + 1, right = N - 1;
 
             while (left < right) {
-                long temp = value[i] + value[left] + value[right];
-                if (Math.abs(temp) < Math.abs(minSum)) {
-                    minSum = temp;
-                    arr[0] = value[i];
-                    arr[1] = value[left];
-                    arr[2] = value[right];
+                long sum = arr[i] + arr[left] + arr[right];
+                if (Math.abs(closest) > Math.abs(sum)) {
+                    closest = sum;
+                    result[0] = arr[i];
+                    result[1] = arr[left];
+                    result[2] = arr[right];
                 }
-
-                if (value[i] + value[left] + value[right] == 0) {
-                    break;
-                } else if (value[i] + value[left] + value[right] < 0) {
+                if (sum < 0) {
                     left++;
                 } else {
                     right--;
@@ -43,6 +45,7 @@ public class Main {
             }
         }
 
-        System.out.println(arr[0] + " " + arr[1] + " " + arr[2]);
+        Arrays.sort(result);
+        System.out.println(result[0] + " " + result[1] + " " + result[2]);
     }
 }
