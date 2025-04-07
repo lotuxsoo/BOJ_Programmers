a96e3773a3a2bfc4d5efb69050c0dcf04311d13e
최수연 <tndus5383@gmail.com>
2025-04-07 12:14:56 +0900

    [Gold IV] Title: LCS 2, Time: 136 ms, Memory: 18784 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,35 +9,34 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s1 = br.readLine();
         String s2 = br.readLine();
-        int N = s1.length();
-        int M = s2.length();
-        int[][] D = new int[N + 1][M + 1];
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
+        int n = s1.length(), m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    D[i][j] = D[i - 1][j - 1] + 1;
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    D[i][j] = Math.max(D[i - 1][j], D[i][j - 1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        System.out.println(D[N][M]);
+
+        System.out.println(dp[n][m]);
 
         StringBuilder sb = new StringBuilder();
-        int i = N;
-        int j = M;
-        while (i > 0 && j > 0) {
+        int i = n, j = m;
+        while (dp[n][m] > 0) {
             if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
                 sb.append(s1.charAt(i - 1));
+                dp[n][m]--;
                 i--;
                 j--;
+            } else if (dp[i - 1][j] < dp[i][j - 1]) {
+                j--;
             } else {
-                if (D[i][j - 1] > D[i - 1][j]) {
-                    j--;
-                } else {
-                    i--;
-                }
+                i--;
             }
         }
 
