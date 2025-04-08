9884a9f3a16d536a4d721a12c997f2de56cdae41
최수연 <tndus5383@gmail.com>
2025-04-08 14:45:04 +0900

    [Gold II] Title: 친구 네트워크, Time: 804 ms, Memory: 78032 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -0,0 +1,76 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int find(int x) {
+        if (x != parent[x]) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    static void union(int x, int y) {
+        int r1 = find(x), r2 = find(y);
+        if (r1 != r2) {
+            if (rank[r1] < rank[r2]) {
+                parent[r1] = r2;
+                size[r2] += size[r1];
+            } else if (rank[r1] > rank[r2]) {
+                parent[r2] = r1;
+                size[r1] += size[r2];
+            } else {
+                parent[r2] = r1;
+                size[r1] += size[r2];
+                rank[r1]++;
+            }
+        }
+    }
+
+    static int[] parent, rank, size;
+    static Map<String, Integer> idMap;
+    static final int MAX = 200000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        while (T-- > 0) {
+            // 이름마다 id로 관리
+            int id = 0;
+            idMap = new HashMap<>();
+            parent = new int[MAX];
+            rank = new int[MAX];
+            size = new int[MAX];
+            for (int i = 0; i < MAX; i++) {
+                parent[i] = i;
+                rank[i] = 0;
+                size[i] = 1;
+            }
+
+            int F = Integer.parseInt(br.readLine());
+            for (int i = 0; i < F; i++) {
+                String[] sp = br.readLine().split(" ");
+                idMap.putIfAbsent(sp[0], id++);
+                idMap.putIfAbsent(sp[1], id++);
+                int id1 = idMap.get(sp[0]);
+                int id2 = idMap.get(sp[1]);
+                if (find(id1) != find(id2)) {
+                    union(id1, id2);
+                }
+                int root = find(id1);
+                sb.append(size[root] + "\n");
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
