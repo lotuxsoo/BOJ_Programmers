766ceffa6504bcde42625b7516988aa35b74aac5
최수연 <tndus5383@gmail.com>
2025-04-09 15:37:11 +0900

    [Gold IV] Title: 작업, Time: 744 ms, Memory: 82108 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.java
+++ 백준/Gold/2056. 작업/작업.java
@@ -3,78 +3,68 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Info {
-        int n, time;
-
-        Info(int n, int time) {
-            this.n = n;
-            this.time = time;
-        }
-    }
 
     static int N;
-    static ArrayList<Integer>[] A; // 방향 인접리스트
-    static int[] time; // 작업 시간
-    static int[] D; // 진입차수 배열
+    static int[] time;
+    static int[] indegree;
     static int[] dp;
+    static ArrayList<Integer>[] graph; // graph[i]를 선행작업으로 가지는 작업번호 저장
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-
-        A = new ArrayList[N + 1];
-        for (int i = 0; i < N + 1; i++) {
-            A[i] = new ArrayList<>();
-        }
-
         time = new int[N + 1];
-        D = new int[N + 1];
-
-        for (int i = 1; i < N + 1; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            time[i] = Integer.parseInt(st.nextToken());
-            int n = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < n; j++) {
-                int x = Integer.parseInt(st.nextToken());
-                A[x].add(i);
-                D[i]++;
-            }
+        indegree = new int[N + 1];
+        graph = new ArrayList[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            graph[i] = new ArrayList<>();
         }
 
-        // i번 작업까지 수행했을 때의 최대 시간
-        dp = new int[N + 1];
-
-        Queue<Info> queue = new LinkedList<>();
         for (int i = 1; i <= N; i++) {
-            if (D[i] == 0) {
-                dp[i] = time[i];
-                queue.add(new Info(i, time[i]));
-            }
-        }
-
-        int MAX = 0;
-
-        while (!queue.isEmpty()) {
-            Info cur = queue.poll();
-
-            MAX = Math.max(MAX, dp[cur.n]);
-
-            for (int next : A[cur.n]) {
-                dp[next] = Math.max(dp[next], dp[cur.n] + time[next]);
-
-                D[next]--;
-                if (D[next] == 0) {
-                    queue.add(new Info(next, dp[next]));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            time[i] = t;
+            int n = Integer.parseInt(st.nextToken());
+            indegree[i] = n;
+            if (n > 0) {
+                for (int j = 0; j < n; j++) {
+                    int x = Integer.parseInt(st.nextToken());
+                    graph[x].add(i);
                 }
             }
         }
 
-        System.out.println(MAX);
+        dp = new int[N + 1];
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+                dp[i] = time[i];
+            }
+        }
+
+        int result = 0;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            result = Math.max(result, dp[cur]);
+
+            for (int next : graph[cur]) {
+                indegree[next]--;
+                dp[next] = Math.max(dp[next], dp[cur] + time[next]);
+
+                if (indegree[next] == 0) {
+                    queue.add(next);
+                }
+            }
+        }
+
+        System.out.println(result);
     }
 }
