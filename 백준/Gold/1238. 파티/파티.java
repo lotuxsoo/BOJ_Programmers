393a04e75e3e2145d00840c7688b371414be76c4
최수연 <tndus5383@gmail.com>
2025-04-09 10:17:51 +0900

    [Gold III] Title: 파티, Time: 564 ms, Memory: 69688 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -9,99 +9,94 @@ import java.util.StringTokenizer;
 
 public class Main {
     static class Node {
-        int dest, time;
+        int val, cost;
 
-        Node(int dest, int time) {
-            this.dest = dest;
-            this.time = time;
+        Node(int val, int cost) {
+            this.val = val;
+            this.cost = cost;
         }
     }
 
     static int N, M, X;
     static ArrayList<Node>[] graph;
-    static ArrayList<Node>[] reverse;
+    static final int INF = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken()); // 학생 수
+        M = Integer.parseInt(st.nextToken()); // 도로 수
+        X = Integer.parseInt(st.nextToken()); // 파티 장소
 
         graph = new ArrayList[N + 1];
-        reverse = new ArrayList[N + 1];
         for (int i = 0; i < N + 1; i++) {
             graph[i] = new ArrayList<>();
-            reverse[i] = new ArrayList<>();
         }
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            int t = Integer.parseInt(st.nextToken());
-            graph[s].add(new Node(e, t));
-            reverse[s].add(new Node(e, t));
+            int w = Integer.parseInt(st.nextToken());
+            graph[s].add(new Node(e, w));
         }
 
-        // 거리 저장
-        int[] cost = new int[N + 1];
-        int maxCost = 0;
+        int[][] distFromN = new int[N + 1][N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            Arrays.fill(distFromN[i], INF);
+            distFromN[i][i] = 0;
+        }
 
+        // 각 마을마다 -> X까지 가는 최단 경로
         for (int i = 1; i <= N; i++) {
-            if (i == X) {
+            PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.cost, b.cost));
+            pq.add(new Node(i, 0));
+
+            while (!pq.isEmpty()) {
+                Node cur = pq.poll();
+
+                if (distFromN[i][cur.val] < cur.cost) {
+                    continue;
+                }
+
+                for (Node next : graph[cur.val]) {
+                    if (distFromN[i][next.val] > distFromN[i][cur.val] + next.cost) {
+                        distFromN[i][next.val] = distFromN[i][cur.val] + next.cost;
+                        pq.add(new Node(next.val, distFromN[i][next.val]));
+                    }
+                }
+            }
+        }
+
+        // X에서 -> 각 마을까지 가는 최단 경로
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.cost, b.cost));
+        pq.add(new Node(X, 0));
+
+        int[] distFromX = new int[N + 1];
+        Arrays.fill(distFromX, INF);
+        distFromX[X] = 0;
+
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+
+            if (distFromX[cur.val] < cur.cost) {
                 continue;
             }
 
-            int[] dist = new int[N + 1];
-            Arrays.fill(dist, Integer.MAX_VALUE);
-            dist[i] = 0;
-
-            PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.time, b.time));
-            pq.add(new Node(i, dist[i]));
-
-            while (!pq.isEmpty()) {
-                Node cur = pq.poll();
-                if (cur.dest == X) {
-                    cost[i] += dist[cur.dest];
-                    break;
-                }
-                if (dist[cur.dest] < cur.time) {
-                    continue;
-                }
-                for (Node next : graph[cur.dest]) {
-                    if (dist[next.dest] > dist[cur.dest] + next.time) {
-                        dist[next.dest] = dist[cur.dest] + next.time;
-                        pq.add(new Node(next.dest, dist[next.dest]));
-                    }
-                }
-            }
-
-            dist = new int[N + 1];
-            Arrays.fill(dist, Integer.MAX_VALUE);
-            dist[X] = 0;
-
-            pq = new PriorityQueue<>((a, b) -> Integer.compare(a.time, b.time));
-            pq.add(new Node(X, 0));
-
-            while (!pq.isEmpty()) {
-                Node cur = pq.poll();
-                if (cur.dest == i) {
-                    cost[i] += dist[cur.dest];
-                    maxCost = Math.max(maxCost, cost[i]);
-                    break;
-                }
-                if (dist[cur.dest] < cur.time) {
-                    continue;
-                }
-                for (Node next : reverse[cur.dest]) {
-                    if (dist[next.dest] > dist[cur.dest] + next.time) {
-                        dist[next.dest] = dist[cur.dest] + next.time;
-                        pq.add(new Node(next.dest, dist[next.dest]));
-                    }
+            for (Node next : graph[cur.val]) {
+                if (distFromX[next.val] > distFromX[cur.val] + next.cost) {
+                    distFromX[next.val] = distFromX[cur.val] + next.cost;
+                    pq.add(new Node(next.val, distFromX[next.val]));
                 }
             }
         }
 
-        System.out.println(maxCost);
+        int maxTime = 0;
+
+        for (int i = 1; i <= N; i++) {
+            maxTime = Math.max(maxTime, distFromN[i][X] + distFromX[i]);
+        }
+
+        System.out.println(maxTime);
     }
 }
