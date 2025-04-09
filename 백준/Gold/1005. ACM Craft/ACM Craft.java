223807b7a3d45bdaeee02e8ec3eb70281f7a0940
최수연 <tndus5383@gmail.com>
2025-04-09 16:51:34 +0900

    [Gold III] Title: ACM Craft, Time: 680 ms, Memory: 241456 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -9,74 +9,70 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, K, W;
-    static int[] cost;
-    static ArrayList<Integer>[] A;
-    static int[] D;
-    static int[] dp;
-    static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
         while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+
+            int[] time = new int[N + 1];
             st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
-            cost = new int[N + 1];
-            st = new StringTokenizer(br.readLine());
-            for (int i = 1; i < N + 1; i++) {
-                cost[i] = Integer.parseInt(st.nextToken());
+            for (int i = 1; i <= N; i++) {
+                time[i] = Integer.parseInt(st.nextToken());
             }
 
-            A = new ArrayList[N + 1];
+            int[] indegree = new int[N + 1]; // 진입차수 배열
+            ArrayList<Integer>[] graph = new ArrayList[N + 1]; // 인접리스트
             for (int i = 0; i < N + 1; i++) {
-                A[i] = new ArrayList<>();
+                graph[i] = new ArrayList<>();
             }
-
-            D = new int[N + 1];
 
             for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
-                int X = Integer.parseInt(st.nextToken());
-                int Y = Integer.parseInt(st.nextToken());
-                A[X].add(Y);
-                D[Y]++;
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                graph[x].add(y);
+                indegree[y]++;
             }
-            W = Integer.parseInt(br.readLine());
 
-            dp = new int[N + 1];
+            int W = Integer.parseInt(br.readLine());
 
-            // 위상 정렬
+            // dp[i]: i를 짓는데까지 걸리는 시간
+            int[] dp = new int[N + 1];
+
+            // 위상정렬 시작
             Queue<Integer> queue = new LinkedList<>();
-            for (int i = 1; i < N + 1; i++) {
-                if (D[i] == 0) {
+            for (int i = 1; i <= N; i++) {
+                if (indegree[i] == 0) {
+                    if (i == W) {
+                        sb.append(time[i]).append("\n");
+                        break;
+                    }
                     queue.add(i);
-                    dp[i] = cost[i];
+                    dp[i] = time[i];
                 }
             }
 
             while (!queue.isEmpty()) {
                 int cur = queue.poll();
-
                 if (cur == W) {
-                    sb.append(dp[W]).append("\n");
+                    sb.append(dp[cur]).append("\n");
                     break;
                 }
 
-                for (int next : A[cur]) {
-                    // next를 짓는데까지 걸리는 최장 시간
-                    dp[next] = Math.max(dp[next], dp[cur] + cost[next]);
-
-                    D[next]--;
-                    if (D[next] == 0) {
+                for (int next : graph[cur]) {
+                    indegree[next]--; // 선행 건물 하나 지음
+                    dp[next] = Math.max(dp[next], dp[cur] + time[next]);
+                    if (indegree[next] == 0) {
                         queue.add(next);
                     }
                 }
             }
         }
 
-        System.out.println(sb.toString().trim());
+        System.out.println(sb.toString());
     }
 }
