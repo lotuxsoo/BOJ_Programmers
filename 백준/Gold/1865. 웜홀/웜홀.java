2d486077a2e85f1474201cd6b00a41a24973b3ce
최수연 <tndus5383@gmail.com>
2025-04-09 12:48:56 +0900

    [Gold III] Title: 웜홀, Time: 664 ms, Memory: 33144 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -4,82 +4,77 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
     static class Edge {
-        int S, E, T;
+        int from, to, cost;
 
-        Edge(int S, int E, int T) {
-            this.S = S;
-            this.E = E;
-            this.T = T;
+        Edge(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
         }
     }
 
-    static boolean canTimeTravel(int x) {
-        long[] dist = new long[N + 1];
-        Arrays.fill(dist, INF);
-        dist[x] = 0;
-
-        for (int i = 0; i < N; i++) {
-            boolean updated = false;
-            for (Edge edge : edges) {
-                if (dist[edge.S] != INF && dist[edge.E] > dist[edge.S] + edge.T) {
-                    dist[edge.E] = dist[edge.S] + edge.T;
-                    updated = true;
-                    if (i == N - 1) {
-                        return true;
-                    }
-                }
-            }
-            if (!updated) {
-                break;
-            }
-        }
-
-        return false;
-    }
-
-    static int N, M, W;
-    static ArrayList<Edge> edges;
-    static final int INF = 1_000_000_000;
-    static StringBuilder sb = new StringBuilder();
+    static final long INF = Long.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int TC = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         while (TC-- > 0) {
+            ArrayList<Edge> edges = new ArrayList<>();
             StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            W = Integer.parseInt(st.nextToken());
-            edges = new ArrayList<>();
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
             for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
-                int S = Integer.parseInt(st.nextToken());
-                int E = Integer.parseInt(st.nextToken());
-                int T = Integer.parseInt(st.nextToken());
-                edges.add(new Edge(S, E, T));
-                edges.add(new Edge(E, S, T));
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                int w = Integer.parseInt(st.nextToken());
+                edges.add(new Edge(s, e, w)); // 방향 X
+                edges.add(new Edge(e, s, w));
             }
             for (int i = 0; i < W; i++) {
                 st = new StringTokenizer(br.readLine());
-                int S = Integer.parseInt(st.nextToken());
-                int E = Integer.parseInt(st.nextToken());
-                int T = Integer.parseInt(st.nextToken());
-                edges.add(new Edge(S, E, -T));
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                int w = Integer.parseInt(st.nextToken());
+                edges.add(new Edge(s, e, -w)); // 방향 O
+            }
+
+            // 가상 정점 0번을 추가
+            for (int i = 1; i <= N; i++) {
+                edges.add(new Edge(0, i, 0));
+            }
+
+            long[] dist = new long[N + 1];
+            Arrays.fill(dist, INF);
+            dist[0] = 0;
+
+            // 정점수 N+1 -> N번 반복해서 에지 업데이트
+            for (int j = 0; j < N; j++) {
+                for (Edge edge : edges) {
+                    if (dist[edge.from] != INF && dist[edge.to] > dist[edge.from] + edge.cost) {
+                        dist[edge.to] = dist[edge.from] + edge.cost;
+                    }
+                }
             }
 
             boolean hasCycle = false;
-            for (int i = 1; i <= N; i++) {
-                if (canTimeTravel(i)) {
-                    sb.append("YES\n");
+            // 음수 사이클 확인
+            for (Edge edge : edges) {
+                if (dist[edge.from] != INF && dist[edge.to] > dist[edge.from] + edge.cost) {
                     hasCycle = true;
                     break;
                 }
             }
-            if (!hasCycle) {
+            if (hasCycle) {
+                sb.append("YES\n");
+            } else {
                 sb.append("NO\n");
             }
         }
