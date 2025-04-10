702a8c1d209cc2031d6a83fc7fc3e1969a157787
최수연 <tndus5383@gmail.com>
2025-04-10 16:39:58 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 232 ms, Memory: 29736 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
@@ -2,44 +2,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 public class Main {
 
-    static int N;
-    static int[] parent; // 부모노드는 반드시 1개
+    static int dfs(int x) {
+        if (parent[x] == 0) {
+            return 0;
+        }
+
+        if (depth[x] != -1) {
+            return depth[x]; // 이미 계산한 경우
+        }
+
+        depth[x] = dfs(parent[x]) + 1;
+
+        return depth[x];
+    }
+
+    static int N, X, Y;
+    static int[] parent, depth;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
             N = Integer.parseInt(br.readLine());
-            // 부모 배열 저장
             parent = new int[N + 1];
+
             for (int i = 0; i < N - 1; i++) {
                 String[] sp = br.readLine().split(" ");
-                int a = Integer.parseInt(sp[0]);
-                int b = Integer.parseInt(sp[1]);
-                parent[b] = a; // 자식->부모 단방향 배열
+                int A = Integer.parseInt(sp[0]);
+                int B = Integer.parseInt(sp[1]);
+                parent[B] = A; // A가 B의 부모
             }
+
             String[] sp = br.readLine().split(" ");
-            int x = Integer.parseInt(sp[0]);
-            int y = Integer.parseInt(sp[1]);
+            X = Integer.parseInt(sp[0]);
+            Y = Integer.parseInt(sp[1]);
+            
+            depth = new int[N + 1];
+            Arrays.fill(depth, -1);
+            int d1 = dfs(X);
 
-            Set<Integer> ancestor = new HashSet<>();
-            while (x != 0) {
-                ancestor.add(x);
-                x = parent[x];
+            depth = new int[N + 1];
+            Arrays.fill(depth, -1);
+            int d2 = dfs(Y);
+
+            while (d1 < d2) {
+                Y = parent[Y];
+                d2--;
+            }
+            while (d1 > d2) {
+                X = parent[X];
+                d1--;
+            }
+            while (X != Y) {
+                X = parent[X];
+                Y = parent[Y];
             }
 
-            while (y != 0) {
-                if (ancestor.contains(y)) {
-                    System.out.println(y);
-                    break;
-                }
-                y = parent[y];
-            }
+            System.out.println(X);
         }
     }
 }
