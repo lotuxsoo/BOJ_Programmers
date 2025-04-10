7e534e6697b17c006677fb19cd0dd63b50aec27c
최수연 <tndus5383@gmail.com>
2025-04-10 16:00:10 +0900

    [Silver II] Title: 촌수계산, Time: 100 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -6,26 +6,26 @@ import java.util.ArrayList;
 
 public class Main {
 
-    static int dfs(int cur, int count) {
-        visited[cur] = true;
-
-        if (cur == y) {
-            return count;
+    static int dfs(int a, int b, int cnt) {
+        if (a == b) {
+            return cnt;
         }
 
-        for (int next : graph[cur]) {
+        visited[a] = true;
+
+        for (int next : graph[a]) {
             if (!visited[next]) {
-                int result = dfs(next, count + 1);
+                int result = dfs(next, b, cnt + 1);
                 if (result != -1) {
                     return result;
                 }
             }
         }
+
         return -1;
     }
 
-    static int n;
-    static int x, y;
+    static int n, m, a, b;
     static ArrayList<Integer>[] graph;
     static boolean[] visited;
 
@@ -33,25 +33,25 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         String[] sp = br.readLine().split(" ");
-        x = Integer.parseInt(sp[0]);
-        y = Integer.parseInt(sp[1]);
+        a = Integer.parseInt(sp[0]);
+        b = Integer.parseInt(sp[1]);
 
         graph = new ArrayList[n + 1];
         for (int i = 0; i < n + 1; i++) {
             graph[i] = new ArrayList<>();
         }
 
-        int m = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
         for (int i = 0; i < m; i++) {
             sp = br.readLine().split(" ");
-            int parent = Integer.parseInt(sp[0]);
-            int child = Integer.parseInt(sp[1]);
-            graph[parent].add(child);
-            graph[child].add(parent);
+            int x = Integer.parseInt(sp[0]);
+            int y = Integer.parseInt(sp[1]);
+            graph[x].add(y); // 부모-자식 양방향 저장
+            graph[y].add(x);
         }
 
         visited = new boolean[n + 1];
-        int count = dfs(x, 0);
-        System.out.println(count);
+        int result = dfs(a, b, 0);
+        System.out.println(result);
     }
 }
