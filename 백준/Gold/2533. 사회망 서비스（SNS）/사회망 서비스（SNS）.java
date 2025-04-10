61bce4f1b1d8ead4df8431e46669c3f4fabd4935
최수연 <tndus5383@gmail.com>
2025-04-10 12:09:11 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 2224 ms, Memory: 407552 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -3,20 +3,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
 
-    static void dfs(int pos) {
-        dp[pos][0] = 0;
-        dp[pos][1] = 1;
+    static void dfs(int cur, int parent) {
+        visited[cur] = true;
 
-        for (int child : graph[pos]) {
-            if (!visited[child]) {
-                visited[child] = true;
-                dfs(child); // 재귀호출로 자식 노드의 dp값을 미리 구한다.
-                dp[pos][0] += dp[child][1];
-                dp[pos][1] += Math.min(dp[child][0], dp[child][1]);
+        dp[cur][0] = 0; // 얼리가 아닐 때
+        dp[cur][1] = 1; // 얼리일 때 (본인 포함)
+
+        for (int next : graph[cur]) {
+            if (next != parent) {
+                dfs(next, cur);
+                dp[cur][0] += dp[next][1];
+                dp[cur][1] += Math.min(dp[next][0], dp[next][1]);
             }
         }
     }
@@ -29,28 +29,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        graph = new ArrayList[N + 1];
+
+        graph = new ArrayList[N + 1]; // 양방향 트리 표현
         for (int i = 0; i < N + 1; i++) {
             graph[i] = new ArrayList<>();
         }
+
         for (int i = 0; i < N - 1; i++) {
-            String[] s = br.readLine().split(" ");
-            int a = Integer.parseInt(s[0]), b = Integer.parseInt(s[1]);
-            graph[a].add(b);
-            graph[b].add(a);
+            String[] sp = br.readLine().split(" ");
+            int u = Integer.parseInt(sp[0]);
+            int v = Integer.parseInt(sp[1]);
+            graph[u].add(v);
+            graph[v].add(u);
         }
 
-        // dp[v][0]: v가 얼리어답터가 아닐때, 서브트리에서의 최소 얼리어답터 수
-        // dp[v][1]: v가 얼리어답터일 때, 서브트리에서의 최소 얼리어답터 수
+        // dp[i][0]: i가 얼리가 아닐 때, 서브트리의 최소 얼리 수
+        // dp[i][1]: i가 얼리일 때, 서브트리의 최소 얼리 수
         dp = new int[N + 1][2];
-        for (int i = 0; i < N + 1; i++) {
-            Arrays.fill(dp[i], -1);
-        }
 
         visited = new boolean[N + 1];
-        visited[1] = true;
-
-        dfs(1);
+        // 루트 1로 지정
+        dfs(1, -1);
 
         System.out.println(Math.min(dp[1][0], dp[1][1]));
     }
