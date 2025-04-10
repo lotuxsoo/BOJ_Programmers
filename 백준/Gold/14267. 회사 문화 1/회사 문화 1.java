3ce6d128d45cda7bfeff47c272401b98b6d76c90
최수연 <tndus5383@gmail.com>
2025-04-10 11:10:26 +0900

    [Gold IV] Title: 회사 문화 1, Time: 1320 ms, Memory: 82312 KB -BaekjoonHub

---
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static void dfs(int cur) {
+
+        for (int next : graph[cur]) {
+            compliment[next] += compliment[cur];
+            dfs(next);
+        }
+    }
+
+    static int n, m;
+    static int[] parent;
+    static int[] compliment;
+    static ArrayList<Integer>[] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        parent = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            parent[i] = Integer.parseInt(st.nextToken());
+        }
+
+        graph = new ArrayList[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        int x = n;
+        while (x > 1) { // n~2
+            int p = parent[x];
+            graph[p].add(x); // 부모 -> 자식 방향 그래프
+            x--;
+        }
+
+        compliment = new int[n + 1];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int idx = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            compliment[idx] += w;
+        }
+
+        dfs(1);
+
+        for (int i = 1; i < n + 1; i++) {
+            System.out.print(compliment[i] + " ");
+        }
+    }
+}
