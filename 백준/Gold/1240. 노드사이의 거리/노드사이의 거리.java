05ddf0ffa4cf39bf59939da1e75bfd4443ced9f9
최수연 <tndus5383@gmail.com>
2025-04-10 16:06:19 +0900

    [Gold V] Title: 노드사이의 거리, Time: 204 ms, Memory: 18984 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -15,38 +15,37 @@ public class Main {
         }
     }
 
-    static ArrayList<Node>[] graph;
-    static int N, M;
-    static boolean[] visited;
-
-    static int dfs(int s, int e, int totalDist) {
-        if (e == s) {
-            return totalDist;
+    static int dfs(int s, int e, int dist) {
+        if (s == e) {
+            return dist;
         }
 
         visited[s] = true;
 
         for (Node next : graph[s]) {
             if (!visited[next.val]) {
-                // 다음 for루프 영향 안주도록 파라미터로 거리 누적
-                int result = dfs(next.val, e, totalDist + next.dist);
+                int result = dfs(next.val, e, dist + next.dist);
                 if (result != -1) {
                     return result;
                 }
             }
         }
+
         return -1;
     }
+
+    static int N, M;
+    static ArrayList<Node>[] graph;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         graph = new ArrayList[N + 1];
         for (int i = 0; i < N + 1; i++) {
-            graph[i] = new ArrayList<>(); // null이 아닌 상태로
+            graph[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < N - 1; i++) {
@@ -59,13 +58,11 @@ public class Main {
         }
 
         for (int i = 0; i < M; i++) {
+            visited = new boolean[N + 1];
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-
-            visited = new boolean[N + 1];
-            int dist = dfs(u, v, 0);
-            System.out.println(dist);
+            System.out.println(dfs(u, v, 0));
         }
     }
 }
