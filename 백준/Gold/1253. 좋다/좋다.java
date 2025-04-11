275f910172ef55e8ef0aceb054eb888c7dbc28aa
최수연 <tndus5383@gmail.com>
2025-04-11 15:28:11 +0900

    [Gold IV] Title: 좋다, Time: 152 ms, Memory: 14644 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,46 +7,52 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    static boolean isGood(int idx) {
+        // 투포인터로 찾기
+        long target = A[idx];
+        int left = 0, right = N - 1;
+        while (left < right) {
+            if (left == idx) {
+                left++;
+                continue;
+            } else if (right == idx) {
+                right--;
+                continue;
+            }
+
+            long sum = A[left] + A[right];
+            if (sum > target) {
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static int N;
+    static long[] A;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        N = Integer.parseInt(br.readLine());
+        A = new long[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            A[i] = Long.parseLong(st.nextToken());
         }
 
-        Arrays.sort(arr); // 배열 정렬
+        Arrays.sort(A);
 
-        int answer = 0;
-
-        for (int i = 0; i < N; i++) { // i는 현재 확인 중인 "좋은 수"
-            int left = 0;
-            int right = N - 1;
-
-            while (left < right) {
-                if (left == i) { // left가 i와 같으면 스킵
-                    left++;
-                    continue;
-                }
-                if (right == i) { // right가 i와 같으면 스킵
-                    right--;
-                    continue;
-                }
-
-                int sum = arr[left] + arr[right];
-
-                if (sum == arr[i]) { // "좋은 수" 조건 만족
-                    answer++;
-                    break; // "좋은 수"를 찾았으므로 종료
-                } else if (sum < arr[i]) {
-                    left++;
-                } else {
-                    right--;
-                }
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            if (isGood(i)) {
+                count++;
             }
         }
 
-        System.out.println(answer);
+        System.out.println(count);
     }
 }
