fdc40674d0fc7a199e6db2fe008342764602f553
최수연 <tndus5383@gmail.com>
2025-04-15 19:38:29 +0900

    [Silver I] Title: 안전 영역, Time: 208 ms, Memory: 25296 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -6,20 +6,39 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static void dfs(int x, int y, int rain) {
-        visited[x][y] = true;
+    static void dfs(int x, int y, int mid, int[][] cloneMap) {
+        cloneMap[x][y] = -1;
 
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i], ny = y + dy[i];
-            if ((0 <= nx && nx < N && 0 <= ny && ny < N) && !visited[nx][ny] && map[nx][ny] > rain) {
-                dfs(nx, ny, rain);
+            if ((0 <= nx && nx < N && 0 <= ny && ny < N) && cloneMap[nx][ny] > mid) {
+                dfs(nx, ny, mid, cloneMap);
             }
         }
     }
 
+    static int findSafetyZone(int mid) {
+        int count = 0;
+
+        int[][] cloneMap = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            cloneMap[i] = map[i].clone();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (cloneMap[i][j] > mid) {
+                    dfs(i, j, mid, cloneMap);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
     static int N;
     static int[][] map;
-    static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
@@ -28,7 +47,6 @@ public class Main {
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
         int maxHeight = 0;
-
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
@@ -37,21 +55,13 @@ public class Main {
             }
         }
 
-        int answer = 0;
-        for (int rain = 0; rain < maxHeight; rain++) {
-            int cnt = 0;
-            visited = new boolean[N][N];
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (map[i][j] > rain && !visited[i][j]) {
-                        dfs(i, j, rain);
-                        cnt++;
-                    }
-                }
-            }
-            answer = Math.max(answer, cnt);
+        int result = 0;
+        // 단조 증가/감소 성질이 없음, 브루트포스 필요
+        for (int i = 0; i < maxHeight; i++) {
+            int count = findSafetyZone(i);
+            result = Math.max(result, count);
         }
 
-        System.out.println(answer);
+        System.out.println(result);
     }
 }
