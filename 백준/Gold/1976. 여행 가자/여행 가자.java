d5effa05a1453329b5aee4a8da7fa12ab8db511c
최수연 <tndus5383@gmail.com>
2025-04-24 20:51:18 +0900

    [Gold IV] Title: 여행 가자, Time: 152 ms, Memory: 16844 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -14,55 +14,61 @@ public class Main {
         return parent[x];
     }
 
-    static void union(int a, int b) {
-        int root1 = find(a);
-        int root2 = find(b);
-
+    static void union(int x, int y) {
+        int root1 = find(x);
+        int root2 = find(y);
         if (root1 != root2) {
-            parent[root1] = root2;
+            if (rank[root1] < rank[root2]) {
+                parent[root1] = root2;
+            } else if (rank[root1] > rank[root2]) {
+                parent[root2] = root1;
+            } else {
+                parent[root2] = root1;
+                rank[root1]++;
+            }
         }
     }
 
-    static ArrayList<Integer>[] A;
     static int N, M;
-    static int[] route;
-    static int[] parent;
+    static int[] rank, parent;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        StringTokenizer st;
 
-        parent = new int[N + 1];
-        for (int i = 0; i < N + 1; i++) {
+        parent = new int[N];
+        rank = new int[N];
+        for (int i = 0; i < N; i++) {
             parent[i] = i;
         }
 
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
                 int x = Integer.parseInt(st.nextToken());
                 if (x == 1) {
-                    union(i, j);
+                    if (find(i) != find(j)) {
+                        union(i, j);
+                    }
                 }
             }
         }
 
-        route = new int[M];
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int prev = -1;
         for (int i = 0; i < M; i++) {
-            route[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int p = find(route[0]);
-        for (int i = 1; i < M; i++) {
-            if (p != find(route[i])) {
-                System.out.println("NO");
-                return;
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            if (prev != -1) {
+                if (prev != find(x)) {
+                    System.out.println("NO");
+                    return;
+                }
+            } else {
+                prev = find(x);
             }
         }
-
+        
         System.out.println("YES");
     }
 }
