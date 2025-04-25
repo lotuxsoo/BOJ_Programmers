35bbb9bfbca132c080ec7c1fc74fff3acb5b02c4
최수연 <tndus5383@gmail.com>
2025-04-25 13:56:13 +0900

    [Gold IV] Title: 알고스팟, Time: 160 ms, Memory: 16500 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -2,47 +2,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Main {
 
-    static int N, M;
+    static int M, N;
     static int[][] map;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+    static final int INF = 1_000_000_000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] line = br.readLine().split(" ");
-        M = Integer.parseInt(line[0]);
-        N = Integer.parseInt(line[1]);
+        String[] sp = br.readLine().split(" ");
+        M = Integer.parseInt(sp[0]);
+        N = Integer.parseInt(sp[1]);
         map = new int[N][M];
         for (int i = 0; i < N; i++) {
-            char[] chars = br.readLine().toCharArray();
+            String[] line = br.readLine().split("");
             for (int j = 0; j < M; j++) {
-                map[i][j] = chars[j] - '0';
+                map[i][j] = Integer.parseInt(line[j]);
             }
         }
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0, 0});
-
-        int[][] visited = new int[N][M];
+        // (x,y) 위치에서 부순 최소한의 벽 개수
+        int[][] dp = new int[N][M];
         for (int i = 0; i < N; i++) {
-            Arrays.fill(visited[i], Integer.MAX_VALUE);
+            Arrays.fill(dp[i], INF);
         }
-        visited[0][0] = 0;
 
-        int result = Integer.MAX_VALUE;
+        dp[0][0] = 0;
+
+        Deque<int[]> queue = new ArrayDeque<>();
+        queue.addFirst(new int[]{0, 0});
 
         while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
+            int[] cur = queue.pollFirst();
 
             if (cur[0] == N - 1 && cur[1] == M - 1) {
-                result = Math.min(result, cur[2]);
-                continue;
+                System.out.println(dp[N - 1][M - 1]);
+                break;
             }
 
             for (int i = 0; i < 4; i++) {
@@ -51,16 +55,17 @@ public class Main {
                     continue;
                 }
 
-                if (map[nx][ny] == 0 && visited[nx][ny] > cur[2]) {
-                    visited[nx][ny] = cur[2];
-                    queue.add(new int[]{nx, ny, cur[2]});
-                } else if (cur[2] + 1 < N * M + 1 && visited[nx][ny] > cur[2] + 1) {
-                    visited[nx][ny] = cur[2] + 1;
-                    queue.add(new int[]{nx, ny, cur[2] + 1});
+                if (map[nx][ny] == 0 && dp[nx][ny] > dp[cur[0]][cur[1]]) {
+                    dp[nx][ny] = dp[cur[0]][cur[1]];
+                    queue.addFirst(new int[]{nx, ny});
+                    continue;
+                }
+
+                if (map[nx][ny] == 1 && dp[nx][ny] > dp[cur[0]][cur[1]] + 1) {
+                    dp[nx][ny] = dp[cur[0]][cur[1]] + 1;
+                    queue.addLast(new int[]{nx, ny});
                 }
             }
         }
-
-        System.out.println(result);
     }
 }
