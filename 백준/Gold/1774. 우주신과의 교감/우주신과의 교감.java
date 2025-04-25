f1cae7de543fde22404128dfb231df30dd5c0748
최수연 <tndus5383@gmail.com>
2025-04-25 12:07:32 +0900

    [Gold III] Title: 우주신과의 교감, Time: 672 ms, Memory: 45276 KB -BaekjoonHub

---
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
@@ -0,0 +1,113 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    static class Node {
+        int idx, x, y;
+
+        Node(int idx, int x, int y) {
+            this.idx = idx;
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static class Edge {
+        int a, b;
+        double cost;
+
+        Edge(int a, int b, double cost) {
+            this.a = a;
+            this.b = b;
+            this.cost = cost;
+        }
+    }
+
+    static double getDist(int idx1, int idx2) {
+        return Math.sqrt(Math.pow(nodes[idx1].x - nodes[idx2].x, 2) + Math.pow(nodes[idx1].y - nodes[idx2].y, 2));
+    }
+
+    static int find(int x) {
+        if (x != parent[x]) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    static void union(int x, int y) {
+        int root1 = find(x);
+        int root2 = find(y);
+        if (root1 != root2) {
+            if (rank[root1] < rank[root2]) {
+                parent[root1] = root2;
+            } else if (rank[root1] > rank[root2]) {
+                parent[root2] = root1;
+            } else {
+                parent[root2] = root1;
+                rank[root1]++;
+            }
+        }
+    }
+
+    static int[] rank, parent;
+    static int N, M;
+    static Node[] nodes;
+    static ArrayList<Edge> edges;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] sp = br.readLine().split(" ");
+        N = Integer.parseInt(sp[0]);
+        M = Integer.parseInt(sp[1]);
+
+        nodes = new Node[N + 1];
+        for (int i = 1; i <= N; i++) {
+            sp = br.readLine().split(" ");
+            int x = Integer.parseInt(sp[0]);
+            int y = Integer.parseInt(sp[1]);
+            nodes[i] = new Node(i, x, y);
+        }
+
+        rank = new int[N + 1];
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            sp = br.readLine().split(" ");
+            int x = Integer.parseInt(sp[0]);
+            int y = Integer.parseInt(sp[1]);
+
+            if (find(x) != find(y)) {
+                union(x, y);
+            }
+        }
+
+        edges = new ArrayList<>();
+        for (int i = 1; i < N; i++) {
+            for (int j = i + 1; j <= N; j++) {
+                double dist = getDist(i, j);
+                edges.add(new Edge(i, j, dist));
+            }
+        }
+
+        Collections.sort(edges, (a, b) -> Double.compare(a.cost, b.cost));
+        
+        double result = 0;
+
+        for (Edge edge : edges) {
+            if (find(edge.a) != find(edge.b)) {
+                result += edge.cost;
+                union(edge.a, edge.b);
+            }
+        }
+
+        System.out.println(String.format("%.2f", result));
+    }
+}
