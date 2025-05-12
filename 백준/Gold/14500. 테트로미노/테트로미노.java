d4cd97a57ae1ba834a5cc4fd4bc06500eba9d606
최수연 <tndus5383@gmail.com>
2025-05-12 20:53:28 +0900

    [Gold IV] Title: 테트로미노, Time: 628 ms, Memory: 36764 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -2,65 +2,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    static void backtrack(int x, int y, int depth, int sum) {
-        if (depth == 3) {
-            MAX_VAL = Math.max(MAX_VAL, sum);
+    static void dfs(int x, int y, int n, int sum) {
+        if (n == 4) {
+            maxSum = Math.max(maxSum, sum);
             return;
         }
 
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+            int nx = x + dx[i], ny = y + dy[i];
+            if (!(0 <= nx && nx < N && 0 <= ny && ny < M) || visited[nx][ny]) {
+                continue;
+            }
 
-            if ((0 <= nx && nx < N && 0 <= ny && ny < M) && !visited[nx][ny]) {
-                if (depth == 1) {
-                    visited[nx][ny] = true;
-                    backtrack(x, y, depth + 1, sum + map[nx][ny]);
-                    visited[nx][ny] = false;
-                }
+            if (n == 2) {
                 visited[nx][ny] = true;
-                backtrack(nx, ny, depth + 1, sum + map[nx][ny]);
+                dfs(x, y, n + 1, sum + arr[nx][ny]);
                 visited[nx][ny] = false;
             }
+
+            visited[nx][ny] = true;
+            dfs(nx, ny, n + 1, sum + arr[nx][ny]);
+            visited[nx][ny] = false;
         }
     }
 
+    static int maxSum = 0;
     static int N, M;
-    static int[][] map;
-    static boolean[][] visited;
+    static int[][] arr;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int[] tx = {0, 0, 1};
-    static int[] ty = {-1, 1, 0};
-    static int MAX_VAL = 0;
+    static boolean[][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        map = new int[N][M];
-        visited = new boolean[N][M];
+        String[] sp = br.readLine().split(" ");
+        N = Integer.parseInt(sp[0]);
+        M = Integer.parseInt(sp[1]);
+        arr = new int[N][M];
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            sp = br.readLine().split(" ");
             for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(sp[j]);
             }
         }
 
+        visited = new boolean[N][M];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 visited[i][j] = true;
-                backtrack(i, j, 0, map[i][j]);
+                dfs(i, j, 1, arr[i][j]);
                 visited[i][j] = false;
             }
         }
 
-        System.out.println(MAX_VAL);
+        System.out.println(maxSum);
     }
 }
