a4bba97e3b9c20816d8fe6ba524297d280161e7f
최수연 <tndus5383@gmail.com>
2025-05-13 15:09:08 +0900

    [Gold IV] Title: 스도쿠, Time: 456 ms, Memory: 137908 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -3,8 +3,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
 
@@ -19,63 +17,60 @@ public class Main {
         System.out.println(sb.toString());
     }
 
-    static void backtrack(int depth) {
-        if (solved) {
+    static void backtrack(int index) {
+        if (found) {
             return;
         }
-
-        if (depth == list.size()) {
+        
+        if (index == zeros.size()) {
+            found = true;
             print();
-            solved = true;  // 첫번째 해만 출력하면됨
             return;
         }
 
-        int x = list.get(depth)[0];
-        int y = list.get(depth)[1];
+        int[] zero = zeros.get(index);
+        int x = zero[0];
+        int y = zero[1];
 
-        boolean[] visited = new boolean[10]; // 1~9
+        boolean[] numbers = new boolean[10]; // 1~9
 
-        // 행에서 후보 선정
+        // 가로줄에서 후보 제거
         for (int i = 0; i < 9; i++) {
             if (map[x][i] > 0) {
-                visited[map[x][i]] = true;
+                numbers[map[x][i]] = true;
             }
         }
 
-        // 열에서 후보 선정
+        // 세로줄에서 후보 제거
         for (int i = 0; i < 9; i++) {
             if (map[i][y] > 0) {
-                visited[map[i][y]] = true;
+                numbers[map[i][y]] = true;
             }
         }
 
-        // 사각형에서 후보 선정
+        // 3x3 정사각형에서 후보 제거
         int startX = (x / 3) * 3;
         int startY = (y / 3) * 3;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                int nx = startX + i, ny = startY + j;
-                if (!(0 <= nx && nx < 9 && 0 <= ny && ny < 9)) {
-                    continue;
-                }
-                if (map[nx][ny] > 0) {
-                    visited[map[nx][ny]] = true;
+        for (int i = startX; i < startX + 3; i++) {
+            for (int j = startY; j < startY + 3; j++) {
+                if (map[i][j] > 0) {
+                    numbers[map[i][j]] = true;
                 }
             }
         }
 
         for (int i = 1; i <= 9; i++) {
-            if (!visited[i]) {
+            if (!numbers[i]) {
                 map[x][y] = i;
-                backtrack(depth + 1);
+                backtrack(index + 1);
                 map[x][y] = 0;
             }
         }
     }
 
-    static boolean solved = false;
+    static boolean found = false;
     static int[][] map = new int[9][9];
-    static ArrayList<int[]> list = new ArrayList<>();
+    static ArrayList<int[]> zeros = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -84,12 +79,11 @@ public class Main {
             for (int j = 0; j < 9; j++) {
                 map[i][j] = Integer.parseInt(sp[j]);
                 if (map[i][j] == 0) {
-                    list.add(new int[]{i, j});
+                    zeros.add(new int[]{i, j});
                 }
             }
         }
 
         backtrack(0);
-
     }
 }
