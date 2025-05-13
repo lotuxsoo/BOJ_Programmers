68ffae84890489048989b18185b495f545de357e
최수연 <tndus5383@gmail.com>
2025-05-13 09:47:48 +0900

    [Gold III] Title: 소문난 칠공주, Time: 348 ms, Memory: 81400 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -7,18 +7,20 @@ import java.util.Queue;
 
 public class Main {
 
-    static boolean check(int[] selected) {
-        boolean[] check = new boolean[7];
+    static boolean bfs(int[] selected) {
+        int count = 0;
 
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(0);
-        check[0] = true;
-        int cnt = 1;
+        queue.add(selected[0]);
+
+        boolean[] checked = new boolean[25];
+        checked[selected[0]] = true;
+        count++;
 
         while (!queue.isEmpty()) {
             int cur = queue.poll();
+            int x = cur / 5, y = cur % 5;
 
-            int x = selected[cur] / 5, y = selected[cur] % 5;
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i], ny = y + dy[i];
                 if (!(0 <= nx && nx < 5 && 0 <= ny && ny < 5)) {
@@ -26,26 +28,29 @@ public class Main {
                 }
 
                 int idx = nx * 5 + ny;
-
-                // selected에 있는지 확인
-                for (int j = 0; j < 7; j++) {
-                    if (!check[j] && selected[j] == idx) {
-                        check[j] = true;
-                        cnt++;
-                        queue.add(j);
-                        break;
+                for (int s : selected) {
+                    if (s == idx && !checked[idx]) {
+                        checked[idx] = true;
+                        count++;
+                        queue.add(idx);
                     }
                 }
             }
         }
 
-        return cnt == 7;
+        return count == 7;
     }
 
-    static void backtrack(int depth, int start, int sCnt, int[] selected) {
-        if (depth == 7) { // 25개 중에 7개 뽑고나서 연결성 확인
-            if (check(selected) && sCnt >= 4) {
-                result++;
+    static void backtrack(int depth, int sCnt, int yCnt, int start, int[] selected) {
+        if (yCnt >= 4) {
+            return;
+        }
+
+        if (depth == 7) {
+            if (sCnt >= 4) {
+                if (bfs(selected)) {
+                    result++;
+                }
             }
             return;
         }
@@ -54,21 +59,22 @@ public class Main {
             if (!visited[i]) {
                 visited[i] = true;
                 selected[depth] = i;
-                if (map[i / 5][i % 5] == 'S') {
-                    backtrack(depth + 1, i + 1, sCnt + 1, selected);
+                if (arr[i] == 'S') {
+                    backtrack(depth + 1, sCnt + 1, yCnt, i + 1, selected);
                 } else {
-                    backtrack(depth + 1, i + 1, sCnt, selected);
+                    backtrack(depth + 1, sCnt, yCnt + 1, i + 1, selected);
                 }
                 visited[i] = false;
             }
         }
     }
 
-    static int result = 0;
     static char[][] map = new char[5][5];
+    static char[] arr = new char[25];
+    static boolean[] visited = new boolean[25];
+    static int result = 0;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static boolean[] visited = new boolean[25];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -76,10 +82,11 @@ public class Main {
             char[] ch = br.readLine().toCharArray();
             for (int j = 0; j < 5; j++) {
                 map[i][j] = ch[j];
+                arr[i * 5 + j] = ch[j];
             }
         }
 
-        backtrack(0, 0,0, new int[7]);
+        backtrack(0, 0, 0, 0, new int[7]);
 
         System.out.println(result);
     }
