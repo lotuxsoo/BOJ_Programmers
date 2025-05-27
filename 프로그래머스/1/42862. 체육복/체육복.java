1d893fe653e145d1f45bae16df2c97acb4191ca2
최수연 <tndus5383@gmail.com>
2025-05-27 10:55:30 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -4,43 +4,41 @@ class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
         
-        HashSet<Integer> lostSet = new HashSet<>();
-        for (int x : lost) {
-            lostSet.add(x);
-        }
-        
-        HashSet<Integer> reserveSet = new HashSet<>();
-        for (int x : reserve) {
-            if (!lostSet.contains(x)) {
-                reserveSet.add(x);  
-            } else {
-                lostSet.remove(x);
+        int[] arr = new int[n+1];
+        Arrays.fill(arr, 1);
+        for (int i=1; i<=n; i++) {
+            for (int x : lost) {
+                if (i == x) {
+                    arr[i]--;
+                    break;
+                }
+            }
+            for (int x : reserve) {
+                if (i == x) {
+                    arr[i]++;
+                    break;
+                }
             }
         }
         
         answer = n;
         
         for (int i=1; i<=n; i++) {
-            if (lostSet.contains(i)) {
+            if (arr[i] < 1) {
                 boolean flag = false;
-                
-                if (i-1 >= 1) {
-                    if (reserveSet.contains(i-1)) {
-                        reserveSet.remove(i-1);
-                        flag = true;
-                    }
+                if ((i-1 >= 1) && arr[i-1] > 1) {
+                    arr[i-1]--;
+                    arr[i]++;
+                    flag = true;
                 }
-                if ((i+1 <= n) && !flag) {
-                    if (reserveSet.contains(i+1)) {
-                        reserveSet.remove(i+1);
-                        flag = true;
-                    }
+                if (!flag && (i+1 <= n) && arr[i+1] > 1) {
+                    arr[i+1]--;
+                    arr[i]++;
+                    flag = true;
                 }
-                
                 if (!flag) answer--;
             }
         }
-        
         
         return answer;
     }
