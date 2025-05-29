c356521fcf34b7994c2b7136beb6e325dfe80582
최수연 <tndus5383@gmail.com>
2025-05-29 17:17:07 +0900

    [Gold III] Title: 웜홀, Time: 648 ms, Memory: 35224 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -4,49 +4,56 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.List;
 
 public class Main {
-    static class Edge {
-        int from, to, cost;
 
-        Edge(int from, int to, int cost) {
+    static class Edge {
+        int from, to;
+        long cost;
+
+        Edge(int from, int to, long cost) {
             this.from = from;
             this.to = to;
             this.cost = cost;
         }
     }
 
-    static final long INF = Long.MAX_VALUE;
+    static int N, M, W;
+    static List<Edge> edges;
+    static final long INF = 1_000_000_000_000_000_000L;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int TC = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         while (TC-- > 0) {
-            ArrayList<Edge> edges = new ArrayList<>();
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-            int W = Integer.parseInt(st.nextToken());
+            String[] sp = br.readLine().split(" ");
+            N = Integer.parseInt(sp[0]);
+            M = Integer.parseInt(sp[1]);
+            W = Integer.parseInt(sp[2]);
+
+            edges = new ArrayList<>();
             for (int i = 0; i < M; i++) {
-                st = new StringTokenizer(br.readLine());
-                int s = Integer.parseInt(st.nextToken());
-                int e = Integer.parseInt(st.nextToken());
-                int w = Integer.parseInt(st.nextToken());
-                edges.add(new Edge(s, e, w)); // 방향 X
-                edges.add(new Edge(e, s, w));
-            }
-            for (int i = 0; i < W; i++) {
-                st = new StringTokenizer(br.readLine());
-                int s = Integer.parseInt(st.nextToken());
-                int e = Integer.parseInt(st.nextToken());
-                int w = Integer.parseInt(st.nextToken());
-                edges.add(new Edge(s, e, -w)); // 방향 O
+                sp = br.readLine().split(" ");
+                int from = Integer.parseInt(sp[0]);
+                int to = Integer.parseInt(sp[1]);
+                long cost = Long.parseLong(sp[2]);
+                // 도로는 방향이 없음
+                edges.add(new Edge(from, to, cost));
+                edges.add(new Edge(to, from, cost));
             }
 
-            // 가상 정점 0번을 추가
+            for (int i = 0; i < W; i++) {
+                sp = br.readLine().split(" ");
+                int from = Integer.parseInt(sp[0]);
+                int to = Integer.parseInt(sp[1]);
+                long cost = Long.parseLong(sp[2]);
+                // 웜홀은 방향이 있음
+                edges.add(new Edge(from, to, -cost));
+            }
+
+            // 정점 0번과 모두 연결
             for (int i = 1; i <= N; i++) {
                 edges.add(new Edge(0, i, 0));
             }
@@ -55,28 +62,23 @@ public class Main {
             Arrays.fill(dist, INF);
             dist[0] = 0;
 
-            // 정점수 N+1 -> N번 반복해서 에지 업데이트
-            for (int j = 0; j < N; j++) {
+            for (int i = 0; i < N; i++) {
                 for (Edge edge : edges) {
-                    if (dist[edge.from] != INF && dist[edge.to] > dist[edge.from] + edge.cost) {
+                    if (dist[edge.from] != INF && (dist[edge.to] > dist[edge.from] + edge.cost)) {
                         dist[edge.to] = dist[edge.from] + edge.cost;
                     }
                 }
             }
 
-            boolean hasCycle = false;
-            // 음수 사이클 확인
+            boolean flag = false;
             for (Edge edge : edges) {
-                if (dist[edge.from] != INF && dist[edge.to] > dist[edge.from] + edge.cost) {
-                    hasCycle = true;
+                if (dist[edge.from] != INF && (dist[edge.to] > dist[edge.from] + edge.cost)) {
+                    flag = true;
                     break;
                 }
             }
-            if (hasCycle) {
-                sb.append("YES\n");
-            } else {
-                sb.append("NO\n");
-            }
+
+            sb.append(flag ? "YES\n" : "NO\n");
         }
         System.out.println(sb.toString());
     }
