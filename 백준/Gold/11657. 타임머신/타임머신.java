c1b4159b827d5b0c434c0347546940f9d57c9930
최수연 <tndus5383@gmail.com>
2025-05-29 16:56:35 +0900

    [Gold IV] Title: 타임머신, Time: 244 ms, Memory: 20112 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,69 +1,68 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
-    static class Edge {
-        int s, e, w;
 
-        Edge(int s, int e, int w) {
-            this.s = s;
-            this.e = e;
-            this.w = w;
+    static class Edge {
+        int from, to;
+        long cost;
+
+        Edge(int from, int to, long cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
         }
     }
 
+    static ArrayList<Edge> edges = new ArrayList<>();
+    static int N, M;
+    static final long INF = 1_000_000_000_000_000_000L;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // 도시 개수
-        int M = Integer.parseInt(st.nextToken()); // 에지 개수
-
-        ArrayList<Edge> edges = new ArrayList<>();
+        String[] sp = br.readLine().split(" ");
+        N = Integer.parseInt(sp[0]);
+        M = Integer.parseInt(sp[1]);
 
         for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            edges.add(new Edge(s, e, w));
+            sp = br.readLine().split(" ");
+            int a = Integer.parseInt(sp[0]);
+            int b = Integer.parseInt(sp[1]);
+            long c = Long.parseLong(sp[2]);
+            edges.add(new Edge(a, b, c));
         }
 
         long[] dist = new long[N + 1];
-        Arrays.fill(dist, Long.MAX_VALUE);
+        Arrays.fill(dist, INF);
         dist[1] = 0;
 
+        // 최단 경로는 최대 N-1개의 간선 이용
         for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < M; j++) {
-                Edge edge = edges.get(j);
-
-                if (dist[edge.s] != Long.MAX_VALUE && dist[edge.e] > dist[edge.s] + edge.w) {
-                    dist[edge.e] = dist[edge.s] + edge.w;
+            for (Edge edge : edges) {
+                if ((dist[edge.from] != INF) && (dist[edge.to] > dist[edge.from] + edge.cost)) {
+                    dist[edge.to] = dist[edge.from] + edge.cost;
                 }
             }
         }
 
-        boolean mCycle = false;
-        for (int i = 0; i < M; i++) {
-            Edge edge = edges.get(i);
-
-            if (dist[edge.s] != Long.MAX_VALUE && dist[edge.e] > dist[edge.s] + edge.w) {
-                mCycle = true;
-                break;
+        // 음수 사이클 확인
+        for (Edge edge : edges) {
+            if ((dist[edge.from] != INF) && dist[edge.to] > dist[edge.from] + edge.cost) {
+                System.out.println(-1);
+                return;
             }
         }
 
-        if (!mCycle) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 2; i < N + 1; i++) {
-                sb.append(dist[i] == Long.MAX_VALUE ? -1 : dist[i]).append("\n");
-            }
-            System.out.print(sb.toString());
-        } else {
-            System.out.println(-1);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 2; i <= N; i++) {
+            sb.append(dist[i] == INF ? -1 : dist[i]).append("\n");
         }
+
+        System.out.println(sb.toString());
     }
 }
