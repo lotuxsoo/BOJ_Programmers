9b9403fef9cc4ba1d8fc50581fa80060e726342c
최수연 <tndus5383@gmail.com>
2025-05-29 16:14:40 +0900

    [Silver I] Title: 숨바꼭질, Time: 376 ms, Memory: 38972 KB -BaekjoonHub

---
+++ 백준/Silver/6118. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,77 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    static List<List<Integer>> list = new ArrayList<>();
+    static int N, M;
+    static final int INF = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] sp = br.readLine().split(" ");
+        N = Integer.parseInt(sp[0]);
+        M = Integer.parseInt(sp[1]);
+
+        for (int i = 0; i < N + 1; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            sp = br.readLine().split(" ");
+            int a = Integer.parseInt(sp[0]);
+            int b = Integer.parseInt(sp[1]);
+            list.get(a).add(b);
+            list.get(b).add(a); // 양방향 그래프
+        }
+
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, INF);
+        dist[1] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+        pq.add(new int[]{1, 0}); // 시작점, 거리
+        int max = 0;
+
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+
+            if (dist[cur[0]] < cur[1]) {
+                continue;
+            }
+
+            max = Math.max(max, dist[cur[0]]);
+
+            for (int next : list.get(cur[0])) {
+                if (dist[next] > dist[cur[0]] + 1) {
+                    dist[next] = dist[cur[0]] + 1;
+                    pq.add(new int[]{next, dist[next]});
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        boolean flag = false;
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            if (dist[i] == max) {
+                if (!flag) {
+                    sb.append(i).append(" ");
+                    sb.append(max).append(" ");
+                    flag = true;
+                    count++;
+                } else {
+                    count++;
+                }
+            }
+        }
+        sb.append(count);
+        System.out.println(sb.toString());
+    }
+}
