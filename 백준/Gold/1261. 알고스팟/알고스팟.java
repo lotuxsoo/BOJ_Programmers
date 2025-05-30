c92cb81cb687bb84da1c9ecfba9fc585ef2dd8ed
최수연 <tndus5383@gmail.com>
2025-05-30 16:46:49 +0900

    [Gold IV] Title: 알고스팟, Time: 160 ms, Memory: 16188 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -5,17 +5,14 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class Main {
 
-    static int M, N;
     static int[][] map;
+    static int N, M;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static final int INF = 1_000_000_000;
+    static final int INF = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,25 +21,25 @@ public class Main {
         N = Integer.parseInt(sp[1]);
         map = new int[N][M];
         for (int i = 0; i < N; i++) {
-            String[] line = br.readLine().split("");
+            sp = br.readLine().split("");
             for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(line[j]);
+                map[i][j] = Integer.parseInt(sp[j]);
             }
         }
 
-        // (x,y) 위치에서 부순 최소한의 벽 개수
+        Deque<int[]> deque = new ArrayDeque<>();
+        deque.addFirst(new int[]{0, 0});
+
         int[][] dp = new int[N][M];
         for (int i = 0; i < N; i++) {
             Arrays.fill(dp[i], INF);
         }
-
         dp[0][0] = 0;
 
-        Deque<int[]> queue = new ArrayDeque<>();
-        queue.addFirst(new int[]{0, 0});
+        int result = INF;
 
-        while (!queue.isEmpty()) {
-            int[] cur = queue.pollFirst();
+        while (!deque.isEmpty()) {
+            int[] cur = deque.pollFirst();
 
             if (cur[0] == N - 1 && cur[1] == M - 1) {
                 System.out.println(dp[N - 1][M - 1]);
@@ -54,18 +51,19 @@ public class Main {
                 if (!(0 <= nx && nx < N && 0 <= ny && ny < M)) {
                     continue;
                 }
-
-                if (map[nx][ny] == 0 && dp[nx][ny] > dp[cur[0]][cur[1]]) {
-                    dp[nx][ny] = dp[cur[0]][cur[1]];
-                    queue.addFirst(new int[]{nx, ny});
-                    continue;
-                }
-
-                if (map[nx][ny] == 1 && dp[nx][ny] > dp[cur[0]][cur[1]] + 1) {
-                    dp[nx][ny] = dp[cur[0]][cur[1]] + 1;
-                    queue.addLast(new int[]{nx, ny});
+                if (map[nx][ny] == 0) {
+                    if (dp[nx][ny] > dp[cur[0]][cur[1]]) {
+                        dp[nx][ny] = dp[cur[0]][cur[1]];
+                        deque.addFirst(new int[]{nx, ny});
+                    }
+                } else {
+                    if (dp[nx][ny] > dp[cur[0]][cur[1]] + 1) {
+                        dp[nx][ny] = dp[cur[0]][cur[1]] + 1;
+                        deque.addLast(new int[]{nx, ny});
+                    }
                 }
             }
         }
+
     }
 }
