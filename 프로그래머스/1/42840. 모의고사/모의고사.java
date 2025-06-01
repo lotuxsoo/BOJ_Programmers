0ef1e209274f221dae91887fbed308f2124dab6c
최수연 <tndus5383@gmail.com>
2025-06-01 14:04:07 +0900

    [level 1] Title: 모의고사, Time: 1.48 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,46 +1,56 @@
+import java.util.*;
+
 class Solution {
+    
+    static int[][] pattern = {
+        {1,2,3,4,5},
+        {2,1,2,3,2,4,2,5},
+        {3,3,1,1,2,2,4,4,5,5}
+    };
+    
+    static int n;
+    static int[] scores;
+    
+    static int dfs(int depth, int[] pattern, int[] answers) {
+        if (depth == n) {
+            return 0;
+        }
+        
+        int idx = depth % pattern.length;
+        
+        int score = pattern[idx] == answers[depth] ? 1 : 0;
+        
+        score += dfs(depth+1, pattern, answers);
+        
+        return score;
+    }
+    
+    
     public int[] solution(int[] answers) {
         int[] answer = {};
+        n = answers.length;
         
-        int[][] students = {
-            {1,2,3,4,5},
-            {2,1,2,3,2,4,2,5},
-            {3,3,1,1,2,2,4,4,5,5}
-        };
+        int[] scores = new int[3];
         
-        int[] result = new int[3];
+        int max = 0;
         
-        for (int i=0; i<answers.length; i++) {
-            
-            for (int j=0; j<students.length; j++) { // 3번
-                int[] student = students[j];
-            
-                if (student[i % student.length] == answers[i]) {
-                    result[j]++;
-                }   
+        for (int i=0; i<3; i++) {
+            scores[i] = dfs(0, pattern[i], answers);
+            max = Math.max(max, scores[i]);
+        }
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for (int i=0; i<3; i++) {
+            if (max == scores[i]) {
+                list.add(i+1);
             }
         }
         
-        int max = Integer.MIN_VALUE;
-        for (int x : result) {
-            if (x > max) {
-                max = x;
-            }
-        }
-        
-        int count = 0;
-        for (int x : result) {
-            if (x == max) {
-                count++;
-            }
-        }
-        
-        answer = new int[count];
-        int index = 0;
-        for (int i=0; i<result.length; i++) {
-            if (result[i] == max) {
-                answer[index++] = i+1;
-            }
+        Collections.sort(list);
+        answer = new int[list.size()];
+        for (int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
         }
         
         return answer;
