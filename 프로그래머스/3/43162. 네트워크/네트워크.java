63857cb4bb3ae1848d0bacf3a7327cb9fa833c07
최수연 <tndus5383@gmail.com>
2025-06-01 22:19:27 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,51 +2,26 @@ import java.util.*;
 
 class Solution {
     
-    static int[] parent, rank;
-    
-    static void union(int x, int y) {
-        int root1 = find(x), root2 = find(y);
-        if (root1 != root2) {
-            if (rank[root1] < rank[root2]) {
-                parent[root1] = root2;
-            } else if (rank[root1] > rank[root2]) {
-                parent[root2] = root1;
-            } else {
-                parent[root2] = root1;
-                rank[root1]++;
+    static void dfs(int x, int n, int[][] computers) {
+        visited[x] = true;
+        
+        for (int i=0; i<n; i++) {
+            if (!visited[i] && computers[x][i] == 1) {
+                dfs(i, n, computers);
             }
         }
     }
     
-    static int find(int x) {
-        if (x != parent[x]) {
-            parent[x] = find(parent[x]);
-        }
-        return parent[x];
-    }
+    static boolean[] visited;
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
         
-        parent = new int[n+1];
-        rank = new int[n+1];
-        for (int i=0; i<n+1; i++) {
-            parent[i] = i;
-            rank[i] = 0;
-        }
+        visited = new boolean[n];
         
         for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
-                if (computers[i][j] == 1) {
-                    if (find(i) != find(j)) {
-                        union(i, j);
-                    }
-                }
-            }
-        }
-        
-        for (int i=0; i<n; i++) {
-            if (i == parent[i]) {
+            if (!visited[i]) {
+                dfs(i, n, computers);
                 answer++;
             }
         }
