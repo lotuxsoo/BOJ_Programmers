d74ce433dcac11c94344129bff46b37ecac602b2
최수연 <tndus5383@gmail.com>
2025-06-03 16:41:58 +0900

    [Silver I] Title: 안전 영역, Time: 204 ms, Memory: 18316 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -2,66 +2,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    static void dfs(int x, int y, int mid, int[][] cloneMap) {
-        cloneMap[x][y] = -1;
+    static void dfs(int x, int y, int height) {
+        visited[x][y] = true;
 
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i], ny = y + dy[i];
-            if ((0 <= nx && nx < N && 0 <= ny && ny < N) && cloneMap[nx][ny] > mid) {
-                dfs(nx, ny, mid, cloneMap);
+            if ((nx >= 0 && nx < N && ny >= 0 && ny < N) && map[nx][ny] > height && !visited[nx][ny]) {
+                dfs(nx, ny, height);
             }
         }
     }
 
-    static int findSafetyZone(int mid) {
-        int count = 0;
-
-        int[][] cloneMap = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            cloneMap[i] = map[i].clone();
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (cloneMap[i][j] > mid) {
-                    dfs(i, j, mid, cloneMap);
-                    count++;
-                }
-            }
-        }
-
-        return count;
-    }
-
-    static int N;
-    static int[][] map;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+    static int N;
+    static int[][] map;
+    static int minHeight = 1_000_000_000;
+    static int maxHeight = 0;
+    static boolean[][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
-        int maxHeight = 0;
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            String[] sp = br.readLine().split(" ");
             for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = Integer.parseInt(sp[j]);
+                minHeight = Math.min(minHeight, map[i][j]);
                 maxHeight = Math.max(maxHeight, map[i][j]);
             }
         }
 
-        int result = 0;
-        // 단조 증가/감소 성질이 없음, 브루트포스 필요
-        for (int i = 0; i < maxHeight; i++) {
-            int count = findSafetyZone(i);
-            result = Math.max(result, count);
+        int answer = 1; // 아무 지역도 안잠길때
+
+        for (int height = minHeight; height < maxHeight; height++) {
+            visited = new boolean[N][N]; // height마다 초기화
+            int cnt = 0;
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (!visited[i][j] && map[i][j] > height) {
+                        dfs(i, j, height);
+                        cnt++;
+                    }
+                }
+            }
+
+            answer = Math.max(answer, cnt);
         }
 
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
