6793c5fc19729cbaa2b7a31faad2d419045f28ef
최수연 <tndus5383@gmail.com>
2025-06-03 16:20:59 +0900

    [Silver I] Title: 단지번호붙이기, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,70 +1,62 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 
-
 public class Main {
-    static int N;
-    static int[][] map;
-    static boolean[][] visited;
-    static ArrayList<Integer> answerList = new ArrayList<>();
 
-    static boolean cango(int x, int y) {
-        if (!(0 <= x && x < N && 0 <= y && y < N)) {
-            return false;
-        }
-        if (visited[x][y]) {
-            return false;
-        }
-        if (map[x][y] == 0) {
-            return false;
-        }
-        return true;
-    }
-
-    static int DFS(int x, int y) {
+    static int dfs(int x, int y) {
         int cnt = 1;
         visited[x][y] = true;
 
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (cango(nx, ny)) {
-                cnt += DFS(nx, ny);
+            int nx = x + dx[i], ny = y + dy[i];
+            if ((nx >= 0 && nx < N && ny >= 0 && ny < N) && map[nx][ny] == 1 && !visited[nx][ny]) {
+                cnt += dfs(nx, ny);
             }
         }
         return cnt;
     }
+
+    static int N;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int maxCnt = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
         for (int i = 0; i < N; i++) {
-            String[] splits = br.readLine().split("");
+            char[] ch = br.readLine().toCharArray();
             for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(splits[j]);
+                map[i][j] = ch[j] - '0';
             }
         }
+
         visited = new boolean[N][N];
+        int count = 0;
+        ArrayList<Integer> countList = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (map[i][j] == 1 && !visited[i][j]) {
-                    int cnt = DFS(i, j);
-                    answerList.add(cnt);
+                if (!visited[i][j] && map[i][j] == 1) {
+                    count++;
+                    countList.add(dfs(i, j));
                 }
             }
         }
 
-        System.out.println(answerList.size());
-        Collections.sort(answerList);
-        for (int answer : answerList) {
-            System.out.println(answer);
+        StringBuilder sb = new StringBuilder();
+        sb.append(count).append("\n");
+        Collections.sort(countList);
+        for (int x : countList) {
+            sb.append(x).append("\n");
         }
+        System.out.println(sb.toString());
     }
 }
