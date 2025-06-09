66db60778ab155e4005c8617ed63076b41721b2d
최수연 <tndus5383@gmail.com>
2025-06-09 13:54:01 +0900

    [level 3] Title: 거스름돈, Time: 11.30 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,21 +1,19 @@
+
 class Solution {
-    
-    static int[] dp; // x원을 만드는 방법의 수
     
     public int solution(int n, int[] money) {
         int answer = 0;
         
-        dp = new int[n+1]; // dp 배열은 총 0...n+1
-        
-        dp[0] = 1; // 0원을 만드는 방법
-            
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+
         for (int coin : money) {
             for (int i=coin; i<=n; i++) {
                 dp[i] += dp[i-coin];
             }
         }
         
-        answer = dp[n];
+        answer = dp[n] % 1000000007;
         
         return answer;
     }
