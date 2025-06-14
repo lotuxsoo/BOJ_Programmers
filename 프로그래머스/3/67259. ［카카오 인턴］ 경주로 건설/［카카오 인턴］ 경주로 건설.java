bfc27a0bf95eb3d7d9d27c50e88368dce9fd4b01
최수연 <tndus5383@gmail.com>
2025-06-15 01:27:05 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 4.44 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -3,70 +3,85 @@ import java.util.*;
 class Solution {
     static class Node {
         int x, y, dir, cost;
-        Node(int x, int y, int dir, int cost) {
+        Node (int x, int y, int dir, int cost) {
             this.x = x;
             this.y = y;
             this.dir = dir;
             this.cost = cost;
         }
     }
-
-    static int calculateCost(int prevDir, int dir, int cost) {
-        int newCost = cost;
-        if (prevDir == -1 || (prevDir == dir)) {
-            newCost += 100;
-        } else {
-            newCost += 600;
-        }
-        return newCost;
-    }
     
-    static int[] dx = {-1,0,1,0}; // 상(0),좌(1),하(2),우(3)
-    static int[] dy = {0,-1,0,1};
-    static int[][][] dp;
-    static int N;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static final int INF = 1_000_000_000;
     
     public int solution(int[][] board) {
         int answer = 0;
-        N = board.length;
         
-        // (x,y)에서의 최소비용 저장
-        dp = new int[N][N][2];
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.cost, b.cost));
+        // 가로방향:0, 세로방향:1
+        pq.add(new Node(0,0,0,0));
+        pq.add(new Node(0,0,1,0));
+        
+        int N = board.length;
+        int M = board[0].length;
+        int[][][] dp = new int[N][M][2];
         for (int i=0; i<N; i++) {
-            for (int j=0; j<N; j++) {
-                Arrays.fill(dp[i][j], Integer.MAX_VALUE);   
+            for (int j=0; j<M; j++) {
+                Arrays.fill(dp[i][j], INF);   
             }
         }
-        
-        // 시작점 초기화 필수
-        dp[0][0][0] = 0;
-        dp[0][0][1] = 0;
-
-        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.cost,b.cost));
-        pq.add(new Node(0, 0, -1, 0));
-        pq.add(new Node(0, 0, -1, 1));
+        dp[0][0][0] = 0;     
+        dp[0][0][1] = 0;      
         
         while (!pq.isEmpty()) {
-            Node now = pq.poll();
+            Node cur = pq.poll();
             
-            if (now.x == N-1 && now.y == N-1) {
-                return dp[N-1][N-1][now.dir];
+            if (cur.cost > dp[cur.x][cur.y][cur.dir]) continue;
+            
+            if (cur.x == N-1 && cur.y == N-1) {
+                break;
             }
             
-            for (int i=0; i<4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
-                if (!(0<=nx && nx<N && 0<=ny && ny<N)) continue;
-                if (board[nx][ny] == 1) continue;
-                
-                int newCost = calculateCost(now.dir, i%2, now.cost);
-                if (dp[nx][ny][i%2] == Integer.MAX_VALUE || dp[nx][ny][i%2] > newCost) {
-                    dp[nx][ny][i%2] = newCost;
-                    pq.add(new Node(nx, ny, i%2, newCost));
-                }
+            for (int i=0; i<2; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+                if (nx >=0 && nx < N && ny >= 0 && nx < M && board[nx][ny] == 0) {
+                    if (cur.dir == 0) {
+                        if (dp[nx][ny][1] > dp[cur.x][cur.y][0] + 100 + 500) {
+                            dp[nx][ny][1] = dp[cur.x][cur.y][0] + 100 + 500;
+                            pq.add(new Node(nx,ny,1,dp[nx][ny][1]));
+                        }
+                    } else {
+                        if (dp[nx][ny][1] > dp[cur.x][cur.y][1] + 100) {
+                            dp[nx][ny][1] = dp[cur.x][cur.y][1] + 100;
+                            pq.add(new Node(nx,ny,1,dp[nx][ny][1]));
+                        }
+                    }            
+                }       
+            }
+            
+            for (int i=2; i<4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+                if (nx >=0 && nx < N && ny >= 0 && ny < M && board[nx][ny] == 0) {
+                    if (cur.dir == 0) {
+                        if (dp[nx][ny][0] > dp[cur.x][cur.y][0] + 100) {
+                            dp[nx][ny][0] = dp[cur.x][cur.y][0] + 100;
+                            pq.add(new Node(nx,ny,0,dp[nx][ny][0]));
+                        }
+                    } else {
+                        if (dp[nx][ny][0] > dp[cur.x][cur.y][1] + 100 + 500) {
+                            dp[nx][ny][0] = dp[cur.x][cur.y][1] + 100 + 500;
+                            pq.add(new Node(nx,ny,0,dp[nx][ny][0]));
+                        }
+                    }         
+                }     
             }
         }
         
-        return -1;
+        answer = Math.min(dp[N-1][N-1][0], dp[N-1][N-1][1]);
+        
+        return answer;
     }
 }
