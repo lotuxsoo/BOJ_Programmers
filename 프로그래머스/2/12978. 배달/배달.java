bb6a8bc19a421f465a6cbb137f16089d692cfe33
최수연 <tndus5383@gmail.com>
2025-06-15 01:01:08 +0900

    [level 2] Title: 배달, Time: 5.35 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,58 +1,57 @@
 import java.util.*;
 
 class Solution {
+    
     static class Node {
-        int dest, cost;
-        Node(int dest, int cost) {
-            this.dest = dest;
+        int to;
+        long cost;
+        Node (int to, long cost) {
+            this.to = to;
             this.cost = cost;
         }
     }
-
-    static ArrayList<Node>[] graph;
-    static int[] dist;
+    
+    static final long INF = 1_000_000_000_000_000_000L;
 
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-            
-        // 인접리스트 초기화
-        graph = new ArrayList[N+1];
+        
+        ArrayList<Node>[] graph = new ArrayList[N+1];
         for (int i=0; i<N+1; i++) {
             graph[i] = new ArrayList<>();
         }
-        
-        // (A마을-B마을,걸리는시간) 양방향 연결
-        for (int[] row : road) {
-            graph[row[0]].add(new Node(row[1], row[2]));
-            graph[row[1]].add(new Node(row[0], row[2]));
+
+        for (int[] arr : road) {
+            graph[arr[0]].add(new Node(arr[1], arr[2]));
+            graph[arr[1]].add(new Node(arr[0], arr[2]));
         }
         
-        dist = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> Long.compare(a.cost, b.cost));
+        pq.add(new Node(1,0));
         
-        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.cost, b.cost));
-        pq.add(new Node(1, 0));
-        dist[1] = 0;
+        long[] dp = new long[N+1];
+        Arrays.fill(dp, INF);
+        dp[1] = 0;
         
         while (!pq.isEmpty()) {
-            Node now = pq.poll();
+            Node cur = pq.poll();
             
-            if (dist[now.dest] < now.cost) continue;
+            if (cur.cost > dp[cur.to]) continue;
             
-            for (Node next : graph[now.dest]) {
-                
-                if (dist[next.dest] > next.cost + now.cost) {
-                    dist[next.dest] = next.cost + now.cost;
-                    pq.add(new Node(next.dest, dist[next.dest]));
+            for (Node next : graph[cur.to]) {
+                if (dp[next.to] > dp[cur.to] + next.cost) {
+                    dp[next.to] = dp[cur.to] + next.cost;
+                    pq.add(new Node(next.to, dp[next.to]));
                 }
-            }   
+            }
         }
         
         for (int i=1; i<=N; i++) {
-            if (dist[i] <= K) {
+            if (dp[i] <= K) {
                 answer++;
             }
         }
+        
 
         return answer;
     }
